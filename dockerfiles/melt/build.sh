4344b0d13e9c565e24aa09f54bd5dc5aa0dcdb4b
pdiakumis <peterdiakumis@gmail.com>
2021-04-27 15:07:07 +1000

    keep MELT tarball in GCS private bucket

--- dockerfiles/melt/build.sh
+++ dockerfiles/melt/build.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
 # Build MELT and upload to Google Container Registry
 
 TOOL="melt"
 VERSION="2.2.2"
-REPO="gcr.io/cpg-common/sv"
-TAG="${REPO}/${TOOL}:${VERSION}"
+REGISTRY="gcr.io/cpg-common/sv"
+TAG="${REGISTRY}/${TOOL}:${VERSION}"
+TAR="MELTv${VERSION}.tar.gz"
+
+if [[ ! -f  "$TAR" ]]; then
+    echo "Downloading MELT tarball from Google Cloud Storage"
+    gsutil cp gs://cpg-sv-workflows/MELT/${TAR} .
+fi
 
 # Build time: 2min
 # Needs local MELT tarball to be uploaded.
