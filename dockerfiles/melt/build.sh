c00ee80686187979a461e6ebadc26c58e823c45b
pdiakumis <peterdiakumis@gmail.com>
2021-05-04 14:20:23 +1000

    MELT: add dockerfile + build script

---
+++ dockerfiles/melt/build.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# Build MELT and upload to Google Container Registry
+
+REGISTRY="gcr.io/peter-dev-302805/sv"
+TOOL="melt"
+VERSION="2.2.2"
+TAG="${REGISTRY}/${TOOL}:${VERSION}"
+TARBALL="MELTv${VERSION}.tar.gz"
+
+# Needs local MELT tarball
+if [[ ! -f  "$TARBALL" ]]; then
+    echo "Downloading MELT tarball from Google Cloud Storage"
+    gsutil cp gs://cpg-reference/sv/MELT/${TARBALL} .
+fi
+
+docker image build --tag "${TAG}" .
+docker image push "${TAG}"
