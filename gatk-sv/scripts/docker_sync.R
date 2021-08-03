b4b14cfcc3476604c5124212131e2d044de08794
pdiakumis <peterdiakumis@gmail.com>
2021-08-03 10:38:40 +1000

    fix for analysis runner

--- gatk-sv/scripts/docker_sync.R
+++ gatk-sv/scripts/docker_sync.R
@@ -1,3 +1,5 @@
+#!/usr/bin/env Rscript
+
 require(tidyverse)
 require(jsonlite, include.only = "read_json")
 require(glue, include.only = "glue")
@@ -6,7 +8,7 @@ require(glue, include.only = "glue")
 gatk_sv <- list(repo = "https://raw.githubusercontent.com/populationgenomics/gatk-sv",
                 tag = "v0.16-beta")
 gatk_sv_json <-
-  glue("{gatk_sv$repo}/{gatk_sv$tag}/input_values/dockers.json") |>
+  glue("{gatk_sv$repo}/{gatk_sv$tag}/input_values/dockers.json") %>%
   read_json()
 
 # AR repo to hold the images
@@ -16,12 +18,12 @@ au_ar <- list(loc = "australia-southeast1",
 au_artifact_registry <- glue("{au_ar$loc}-docker.pkg.dev/{au_ar$proj}/{au_ar$repo}")
 
 # dataframe linking US GCR image to AU AR copy
-d <- gatk_sv_json |>
-  enframe(name = "image_name", value = "us_gcr") |>
-  unnest(us_gcr) |>
-  filter(!image_name %in% c("name", "melt_docker")) |>
+d <- gatk_sv_json %>%
+  enframe(name = "image_name", value = "us_gcr") %>%
+  unnest(us_gcr) %>%
+  filter(!image_name %in% c("name", "melt_docker")) %>%
   mutate(bname = basename(us_gcr),
-         au_ar = glue("{au_artifact_registry}/{bname}")) |>
+         au_ar = glue("{au_artifact_registry}/{bname}")) %>%
   select(-bname)
 
 # pull from US and push to AU
