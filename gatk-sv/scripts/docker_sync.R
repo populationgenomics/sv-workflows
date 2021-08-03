1b955c841798ee8264be51078127dbdb3eb7510f
pdiakumis <peterdiakumis@gmail.com>
2021-08-03 14:29:49 +1000

    use gcloud builds submit

--- gatk-sv/scripts/docker_sync.R
+++ gatk-sv/scripts/docker_sync.R
@@ -13,24 +13,25 @@ gatk_sv_json <-
 
 # AR repo to hold the images
 au_ar <- list(loc = "australia-southeast1",
-              proj = "fewgenomes",
-              repo = "sv")
+              proj = "cpg-common",
+              repo = "images/sv")
 au_artifact_registry <- glue("{au_ar$loc}-docker.pkg.dev/{au_ar$proj}/{au_ar$repo}")
 
-# dataframe linking US GCR image to AU AR copy
+# dataframe with US GCR images
 d <- gatk_sv_json %>%
   enframe(name = "image_name", value = "us_gcr") %>%
   unnest(us_gcr) %>%
   filter(!image_name %in% c("name", "melt_docker")) %>%
-  mutate(bname = basename(us_gcr),
-         au_ar = glue("{au_artifact_registry}/{bname}")) %>%
-  select(-bname)
+  mutate(bname = basename(us_gcr)) %>%
+  select(us_gcr, bname)
 
-# pull from US and push to AU
+# copy to AU AR
+DOCKERFILE <- "Dockerfile"
 for (i in seq_len(nrow(d))) {
-  cat(glue("pulling {d$us_gcr[i]}"), "\n")
-  system(glue("docker pull {d$us_gcr[i]}"))
-  system(glue("docker tag {d$us_gcr[i]} {d$au_ar[i]}"))
-  cat(glue("pushing {d$au_ar[i]}"), "\n")
-  system(glue("docker push {d$au_ar[i]}"))
+  if (file.exists(DOCKERFILE)) {
+    file.remove(DOCKERFILE)
+  }
+  write_lines(glue("FROM {d$us_gcr[i]}"), file = DOCKERFILE)
+  system(glue("gcloud builds submit --tag {au_artifact_registry}/{d$bname[i]} ."))
+  file.remove(DOCKERFILE)
 }
