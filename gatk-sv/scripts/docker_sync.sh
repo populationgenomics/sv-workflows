f3341eff3a66a5219f9398a08b5a121f92a72b49
pdiakumis <peterdiakumis@gmail.com>
2021-08-04 16:59:13 +1000

    skopeo test - add docker auth

--- gatk-sv/scripts/docker_sync.sh
+++ gatk-sv/scripts/docker_sync.sh
@@ -3,6 +3,5 @@
 micromamba install -y --prefix $MAMBA_ROOT_PREFIX -y -c conda-forge \
     skopeo
 
-echo ${GOOGLE_APPLICATION_CREDENTIALS}
-#skopeo inspect docker://australia-southeast1-docker.pkg.dev/peter-dev-302805/tmp/ubuntu1804:foo
+gcloud auth configure-docker australia-southeast1-docker.pkg.dev
 skopeo copy docker://docker.io/library/alpine:3.12 docker://australia-southeast1-docker.pkg.dev/cpg-common/images/sv/alpine:3.12
