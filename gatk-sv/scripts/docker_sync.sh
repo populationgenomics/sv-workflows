c4c639aef11750a51a2cc2101e9de93efe5495b3
pdiakumis <peterdiakumis@gmail.com>
2021-08-04 12:46:04 +1000

    skopeo test

--- gatk-sv/scripts/docker_sync.sh
+++ gatk-sv/scripts/docker_sync.sh
@@ -3,4 +3,5 @@
 micromamba install -y --prefix $MAMBA_ROOT_PREFIX -y -c conda-forge \
     skopeo
 
+gcloud auth configure-docker australia-southeast1-docker.pkg.dev
 skopeo inspect docker://australia-southeast1-docker.pkg.dev/peter-dev-302805/tmp/ubuntu1804:foo
