3c8243d92d10f8cc9edd9c563a91c8ef9416afc4
pdiakumis <peterdiakumis@gmail.com>
2021-08-04 12:27:02 +1000

    skopeo test

--- gatk-sv/scripts/docker_sync.sh
+++ gatk-sv/scripts/docker_sync.sh
@@ -3,5 +3,4 @@
 micromamba install -y --prefix $MAMBA_ROOT_PREFIX -y -c conda-forge \
     skopeo
 
-skopeo inspect docker://registry.fedoraproject.org/fedora:latest
-skopeo inspect docker://us.gcr.io/broad-dsde-methods/vjalili/cnmops:lint-24b9cda
+skopeo inspect docker://australia-southeast1-docker.pkg.dev/peter-dev-302805/tmp/ubuntu1804:foo
