5c568c14b0cd9f9a3eefca7ebf147ce26416f7ae
Peter Diakumis <peterdiakumis@gmail.com>
2021-08-04 17:13:04 +1000

    Apply suggestions from code review
    Co-authored-by: Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>

--- gatk-sv/scripts/docker_sync.sh
+++ gatk-sv/scripts/docker_sync.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-micromamba install -y --prefix $MAMBA_ROOT_PREFIX -y -c conda-forge \
-    skopeo
+set -ex
+
+micromamba install -y --prefix $MAMBA_ROOT_PREFIX -y -c conda-forge skopeo
 
 gcloud auth configure-docker australia-southeast1-docker.pkg.dev
 skopeo copy docker://docker.io/library/alpine:3.12 docker://australia-southeast1-docker.pkg.dev/cpg-common/images/sv/alpine:3.12
