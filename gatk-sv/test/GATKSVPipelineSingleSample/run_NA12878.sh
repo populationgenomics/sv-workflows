38105e2b273dac785fdb5d01656abab155ce5f6e
pdiakumis <peterdiakumis@gmail.com>
2021-08-17 13:42:29 +1000

    fixes

--- gatk-sv/test/GATKSVPipelineSingleSample/run_NA12878.sh
+++ gatk-sv/test/GATKSVPipelineSingleSample/run_NA12878.sh
@@ -7,19 +7,18 @@ curl --location \
     --header "Authorization: Bearer $(gcloud auth print-identity-token)" \
     --header 'Content-Type: application/json' \
     --data-raw '{
-        "output": "pdiakumis/test1/gatksv_single",
+        "output": "pdiakumis/test1/gatksv_single/NA12878",
         "dataset": "fewgenomes",
         "repo": "sv-workflows",
         "accessLevel": "test",
-        "commit": "9fb01edc3bf6dfb234fc522f61ccf9167d3a2f9a",
+        "commit": "4e554f59eafc7f7b220decdce177649d9bf400d4",
         "inputs_dict": {
           "GATKSVPipelineSingleSample.sample_id" : "NA12878",
           "GATKSVPipelineSingleSample.batch" : "test_NA12878",
-          "GATKSVPipelineSingleSample.bam_or_cram_file" : "gs://cpg-fewgenomes-test/pdiakumis/data/NA12878/NA12878.final.bam",
-          "GATKSVPipelineSingleSample.use_melt" : "false"
+          "GATKSVPipelineSingleSample.bam_or_cram_file" : "gs://cpg-fewgenomes-test/pdiakumis/data/NA12878/NA12878.final.bam"
         },
         "input_json_paths": ["inputs/general.json"],
-        "workflow": "gatk-sv-git/wdl/GATKSVPipelineSingleSample.wdl",
+        "workflow": "GATKSVPipelineSingleSample.wdl",
         "dependencies": ["gatk-sv-git/wdl"],
         "cwd": "gatk-sv/test/GATKSVPipelineSingleSample",
         "description": "Single sample pipeline on NA12878"
