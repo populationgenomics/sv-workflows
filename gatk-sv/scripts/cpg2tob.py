c96af388bb36e569892f802b76c5ed3b8fdd17e8
pdiakumis <peterdiakumis@gmail.com>
2021-09-29 19:16:57 +1000

    print it

--- gatk-sv/scripts/cpg2tob.py
+++ gatk-sv/scripts/cpg2tob.py
@@ -2,4 +2,5 @@
 
 from sample_metadata.api import SampleApi
 sapi = SampleApi()
-sapi.get_sample_id_map_by_internal(["CPG3293", "CPG3301"])
+s = sapi.get_sample_id_map_by_internal(["CPG3293", "CPG3301"])
+print(s)
