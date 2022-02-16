84e0699e1c2aced109244fa0815e75fd9c2f0037
hopedisastro <40783500+hopedisastro@users.noreply.github.com>
2022-02-16 13:23:01 +1100

    Update batch.py

--- str/batch.py
+++ str/batch.py
@@ -63,7 +63,7 @@ def main(cram_path: str, region: str):  # pylint: disable=missing-function-docst
 
     # Speciying where to write the result
     out_fname = os.path.splitext(os.path.basename(cram_path))[0] + '-split.cram'
-    output_path = f'gs://cpg-fewgenomes-test-tmp/hoptan-batch'
+    output_path = f'gs://{HAIL_BUCKET}/{OUTPUT_SUFFIX}/{out_fname}'
     b.write_output(j.output_bam, output_path)
 
     # don't wait for the hail batch workflow to complete, otherwise
