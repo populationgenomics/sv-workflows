58ea41bf4cfd6dbcd543314767764bc021ccfd78
hopedisastro <40783500+hopedisastro@users.noreply.github.com>
2022-02-16 13:14:53 +1100

    Update gangstr_runner.py

--- str/gangstr_runner.py
+++ str/gangstr_runner.py
@@ -68,8 +68,8 @@ def main(cram_path: str, region: str):  # pylint: disable=missing-function-docst
 
     # Speciying where to write the result
     out_fname = os.path.splitext(os.path.basename(cram_path))[0] + '-split.cram'
-    output_path = f'gs://{HAIL_BUCKET}/{OUTPUT_SUFFIX}/{out_fname}'
-    b.write_output(regions, output_path)
+    output_path = f'gs://{HAIL_BUCKET}/{OUTPUT_SUFFIX}/potato'
+    b.write_output(j.output_bam, output_path)
 
     # don't wait for the hail batch workflow to complete, otherwise
     # the workflow might get resubmitted if this VM gets preempted.
