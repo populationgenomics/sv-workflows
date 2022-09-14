efbe717ce44877a5615b75b74955f0dae41ebf06
hopedisastro <40783500+hopedisastro@users.noreply.github.com>
2022-09-14 16:37:57 +1000

    Update str_iterative_eh_runner.py

--- str/str_iterative_eh_runner.py
+++ str/str_iterative_eh_runner.py
@@ -10,7 +10,8 @@ import click
 from sample_metadata.model.analysis_type import AnalysisType
 from sample_metadata.model.analysis_query_model import AnalysisQueryModel
 from sample_metadata.apis import AnalysisApi, SampleApi
-
+from sample_metadata.model import AnalysisStatus
+xs
 from cpg_utils.config import get_config
 
 from cpg_utils.hail_batch import (
