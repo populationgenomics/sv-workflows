30ee251c75f45fa014ecd11e6d4ea4a943d5496f
hopedisastro <40783500+hopedisastro@users.noreply.github.com>
2023-01-11 12:38:05 +1100

    Update bed_to_sequence.py

--- str/inputs/pure_repeats_catalog/bed_to_sequence.py
+++ str/inputs/pure_repeats_catalog/bed_to_sequence.py
@@ -12,6 +12,7 @@ from cpg_utils.config import get_config
 from cpg_utils.hail_batch import (
     remote_tmpdir,
 )
+
 config = get_config()
 DATASET = os.getenv('DATASET')
 HAIL_BUCKET = os.getenv('HAIL_BUCKET')
@@ -19,12 +20,16 @@ OUTPUT_SUFFIX = os.getenv('OUTPUT')
 BILLING_PROJECT = os.getenv('HAIL_BILLING_PROJECT')
 ACCESS_LEVEL = os.getenv('ACCESS_LEVEL')
 
-CATALOG_PATH = 'gs://cpg-tob-wgs-test/hoptan-str/bed_catalog_without_complex_repeats.bed.txt'
-BEDTOOLS_IMAGE = 'australia-southeast1-docker.pkg.dev/cpg-common/images/bedtools:v2.30.0'
+CATALOG_PATH = (
+    'gs://cpg-tob-wgs-test/hoptan-str/bed_catalog_without_complex_repeats.bed.txt'
+)
+BEDTOOLS_IMAGE = (
+    'australia-southeast1-docker.pkg.dev/cpg-common/images/bedtools:v2.30.0'
+)
 REF_FASTA = 'gs://cpg-common-main/references/hg38/v0/Homo_sapiens_assembly38.fasta'
 
-@click.command()
 
+@click.command()
 def main():  # pylint: disable=missing-function-docstring
 
     # Initializing Batch
@@ -32,26 +37,29 @@ def main():  # pylint: disable=missing-function-docstring
         billing_project=get_config()['hail']['billing_project'],
         remote_tmpdir=remote_tmpdir(),
     )
-    b = hb.Batch(backend=backend, default_image=os.getenv('DRIVER_IMAGE'))  
-    bedtools_job = b.new_job(name = f'Get sequence data for FASTA files')
+    b = hb.Batch(backend=backend, default_image=os.getenv('DRIVER_IMAGE'))
+    bedtools_job = b.new_job(name=f'Get sequence data for FASTA files')
     catalog = b.read_input(CATALOG_PATH)
     fasta = b.read_input(REF_FASTA)
     bedtools_job.image(BEDTOOLS_IMAGE)
     bedtools_job.storage('20G')
     bedtools_job.cpu(8)
 
-    bedtools_job.command(f"""
+    bedtools_job.command(
+        f"""
 
         bedtools getfasta -fi {fasta} -bed {catalog} > {bedtools_job.ofile}
         
-        """)
+        """
+    )
 
-    #Output writing
+    # Output writing
     out_fname = f'catalog_fasta_sequences.fasta'
     output_path = f'gs://cpg-tob-wgs-test/hoptan-str/{out_fname}'
     b.write_output(bedtools_job.ofile, output_path)
 
     b.run(wait=False)
+
 
 if __name__ == '__main__':
     main()
