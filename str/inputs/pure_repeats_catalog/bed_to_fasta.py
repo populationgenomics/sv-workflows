5a763db75bc5d6941fe515010889e21fbd062502
Hope Tanudisastro <40783500+hopedisastro@users.noreply.github.com>
2023-01-12 14:26:20 +1100

    Pure repeats catalog creation PR (#55)
    * pure repeats doc
    
    * add files
    
    * Create README.md
    
    * Update README.md
    
    * Update bed_to_sequence.py
    
    * Update bed_to_sequence.py
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update bed_to_sequence.py
    
    * Update README.md
    
    * Update README.md
    
    * Update bed_to_sequence.py
    
    * changed file name
    
    * Update README.md
    
    * Update .pre-commit-config.yaml
    
    * Update .pylintrc
    
    * Update Pure repeats catalog .ipynb
    
    * Update README.md
    
    * Update README.md
    
    * Pure repeats pr review (#57)
    
    * bed2fasta tweaks
    
    * WIP
    
    * redraft of motif searching script
    
    * redraft of motif searching script
    
    * black
    
    * linter
    
    * type annotation?
    
    * linter
    
    * Update str/inputs/pure_repeats_catalog/README.md
    
    Co-authored-by: Matt Welland <mattwellie@gmail.com>
    
    * Update str/inputs/pure_repeats_catalog/README.md
    
    Co-authored-by: Matt Welland <mattwellie@gmail.com>
    
    * Update str/inputs/pure_repeats_catalog/README.md
    
    Co-authored-by: Matt Welland <mattwellie@gmail.com>
    
    * Update pure_repeats_script.py
    
    * zipped files
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    Co-authored-by: Matt Welland <mattwellie@gmail.com>

---
+++ str/inputs/pure_repeats_catalog/bed_to_fasta.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+"""
+Converts coordinates in BED file to FASTA sequence
+"""
+from cpg_utils.config import get_config
+from cpg_utils.hail_batch import output_path
+from cpg_workflows.batch import get_batch
+
+
+CATALOG_PATH = (
+    'gs://cpg-tob-wgs-test/hoptan-str/bed_catalog_without_complex_repeats.bed.txt'
+)
+REF_FASTA = 'gs://cpg-common-main/references/hg38/v0/Homo_sapiens_assembly38.fasta'
+
+
+# Initializing Batch
+b = get_batch('FASTA conversion')
+
+# provision a new job
+bedtools_job = b.new_job(name=f'Get sequence data for FASTA files')
+bedtools_job.image(get_config()['images']['bedtools'])
+bedtools_job.storage('20G')
+bedtools_job.cpu(8)
+
+# read input files
+catalog = b.read_input(CATALOG_PATH)
+fasta = b.read_input(REF_FASTA)
+
+# set the job command
+bedtools_job.command(
+    f'bedtools getfasta -fi {fasta} -bed {catalog} > {bedtools_job.ofile}'
+)
+
+# write output to GCP bucket for this dataset
+b.write_output(
+    bedtools_job.ofile, output_path('catalog_fasta_sequences.fasta', 'analysis')
+)
+
+b.run(wait=False)
