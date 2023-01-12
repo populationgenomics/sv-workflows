93c62e0e71949abef3273a0aeaf3fffc45d57c30
MattWellie <mattwellie@gmail.com>
2023-01-12 11:19:42 +1100

    redraft of motif searching script

--- str/inputs/pure_repeats_catalog/pure_repeats_script.py
+++ str/inputs/pure_repeats_catalog/pure_repeats_script.py
@@ -18,8 +18,9 @@ def get_purest_motif(metric_list: list[str]) -> tuple[str, int]:
     """
     take the list of metrics lines, find the purest
     wasteful if there's only one row
+    allows for multiple motifs at one locus
     :param metric_list:
-    :return:
+    :return: the purest motif and assc. purity
     """
     metric_data = []
     for each_metric in metric_list:
@@ -32,29 +33,38 @@ def get_purest_motif(metric_list: list[str]) -> tuple[str, int]:
 sections = []
 
 # parse the file into sections, broken up by 'Sequence:' lines
+# each 'section' will contain the sequence, params, and metrics lines
 with open(TRF_DATA, encoding='utf-8') as filehandle:
 
+    # start a fresh section
     this_section = []
+
+    # iterate over lines in this file
     for line in filehandle:
+
+        # demarcates a new sequence, so add the old
         if line.startswith(SEQUENCE):
             # avoid storing the header
             if any(section_line.startswith(SEQUENCE) for section_line in this_section):
                 sections.append(this_section)
 
-            # reset the section
-            this_section = []
+            # reset the section - adding this line
+            this_section = [line.rstrip()]
 
-        # ignore blank lines
-        if line.rstrip():
+        # ignore blank lines, but keep adding to the existing section
+        elif line.rstrip():
             this_section.append(line.rstrip())
 
+    # after iterating over the file, catch the final section
     sections.append(this_section)
+
 
 blank_coords = []
 pure_repeats = []
 
-# process the sections:
+# process the sections
 for section in sections:
+
     # if there were no metrics - get the blank coordinates
     if len(section) == 2:
         blank_coords.append(section[0].removeprefix(SEQUENCE).rstrip())
@@ -68,6 +78,7 @@ for section in sections:
         pure_repeats.append((this_seq, motif))
 
 
+# debug
 print(len(blank_coords))  # 6409
 print(len(pure_repeats))  # 161612
 
@@ -96,15 +107,18 @@ with open('catalog_fasta_sequences.fasta.txt', encoding='utf-8') as handle:
     fasta_lines = handle.readlines()
 
 fasta_dict = {}
-# iterate over lines in pairs
-# not efficient, but whatever
+# iterate over lines in pairs. not efficient, but whatever
+# would need re-thinking if the motifs stretched to multiple lines
 for line_pair in chunks(fasta_lines, 2):
     assert line_pair[0].startswith(FASTA_START)
     fasta_dict[line_pair[0].lstrip(FASTA_START).rstrip()] = line_pair[1].rstrip()
 
+
+# debug
 print(fasta_dict["chr12:6936716-6936773"])
 print(motif_dictionary["chr12:6936716-6936773"])
 
+# check if any of the impure repeats can be recovered
 impure_repeats = []
 for blank in blank_coords:
     motif = motif_dictionary[blank]
