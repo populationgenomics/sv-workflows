428a7a1dd602865d23006dd30426811bf51e7a80
hopedisastro <40783500+hopedisastro@users.noreply.github.com>
2023-02-04 10:01:02 +1100

    Update tester.py

--- str/sensitivity-analysis/tester.py
+++ str/sensitivity-analysis/tester.py
@@ -24,12 +24,15 @@ backend = hb.ServiceBackend(
 b = hb.Batch(backend=backend, default_image=os.getenv('DRIVER_IMAGE'))
 """
 def main(): 
-    b = hb.Batch("name", default_python_image=config['workflow']['driver_image'])
+    backend = hb.ServiceBackend(
+        billing_project=get_config()['hail']['billing_project'],
+        remote_tmpdir=remote_tmpdir(),
+    )
+    b = hb.Batch(backend= backend, default_python_image=config['workflow']['driver_image'])
     j = b.new_python_job(name = "potato")
 
     text = (j.call(hello_world, 'alice')).as_str()
-    #result = j.call(upper, hello_str)
     b.write_output(text, output_path('output/hello-alice.txt'))
-    b.run()
+    b.run(wait=False)
 if __name__ == '__main__':
     main()  # pylint: disable=no-value-for-parameter
