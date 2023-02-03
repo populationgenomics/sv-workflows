47abe9e098908c38f78fabc7f6fb1f61dfb8ee92
hopedisastro <40783500+hopedisastro@users.noreply.github.com>
2023-02-04 10:32:43 +1100

    test

--- str/sensitivity-analysis/data_frame_creator.py
+++ str/sensitivity-analysis/data_frame_creator.py
@@ -15,6 +15,64 @@ from google.cloud import storage
 
 config = get_config()
 
+def eh_csv_writer(file):
+    csv = ""            
+    for line in file: 
+        if line.startswith("##"):
+            continue
+        if line.startswith("#CHROM"):
+            header = line.split()
+            sample_id = header[9]
+            continue
+        attributes = line.split()
+        chr = attributes[0]
+        start = attributes[1]
+        e_qual = attributes[6]
+        locus_characteristics = attributes[7].split(";")
+        end = locus_characteristics[0][4:]
+        repeat_units_in_ref = locus_characteristics[1][4:]
+        ref_sequence_length = locus_characteristics[2][3:]
+        motif = locus_characteristics[3][3:]
+        variant_characteristics = attributes[9].split(":")
+        e_GT = variant_characteristics[0]
+        e_SO = variant_characteristics[1]
+        e_allele_1 = variant_characteristics[2].split("/")[0]
+        e_allele_2 = variant_characteristics[2].split("/")[1]
+        e_REPCI = variant_characteristics[3]
+        e_ADSP = variant_characteristics[4]
+        e_ADFL = variant_characteristics[5]
+        e_ADIR = variant_characteristics[6]
+        e_LC = variant_characteristics[7]
+        csv = csv+(
+            ','.join(
+                [
+                    sample_id, 
+                    chr, 
+                    start, 
+                    e_qual, 
+                    end,
+                    repeat_units_in_ref,
+                    ref_sequence_length, 
+                    motif,
+                    e_GT,
+                    e_SO, 
+                    e_allele_1,
+                    e_allele_2,
+                    e_REPCI,
+                    e_ADSP, 
+                    e_ADFL,
+                    e_ADIR,
+                    e_LC
+                ]
+            )+'\n'
+        )
+        return csv
+def concatenate_csv(csv_array):
+    combo_csv = ""
+    for i in csv_array: 
+        combo_csv= combo_csv+i
+    return combo_csv
+
 def main():
 # pylint: disable=missing-function-docstring
 # Initializing Batch
@@ -35,85 +93,13 @@ def main():
     for file in files: 
         if file.endswith(".vcf"): 
                 vcf_path.append(file)
-
-    def eh_csv_writer(vcf_path: list[str]):
-        print(
-            ','.join(
-                [   
-                    'sample_id',
-                    'chr',
-                    'start',
-                    'e_qual',
-                    'end',
-                    'repeat_units_in_ref',
-                    'ref_sequence_length',
-                    'motif',
-                    'e_GT',
-                    'e_SO',
-                    'e_allele_1',
-                    'e_alelle_2',
-                    'e_REPCI',
-                    'e_ADSP',
-                    'e_ADFL',
-                    'e_ADIR',
-                    'e_LC'
-                ]
-            )
-            +'\n'
-        )
-        for vcf_file in vcf_path:
-            file= b.read_input(vcf_file)
-            for line in file: 
-                if line.startswith("##"):
-                    continue
-                if line.startswith("#CHROM"):
-                    header = line.split()
-                    sample_id = header[9]
-                    continue
-                attributes = line.split()
-                chr = attributes[0]
-                start = attributes[1]
-                e_qual = attributes[6]
-                locus_characteristics = attributes[7].split(";")
-                end = locus_characteristics[0][4:]
-                repeat_units_in_ref = locus_characteristics[1][4:]
-                ref_sequence_length = locus_characteristics[2][3:]
-                motif = locus_characteristics[3][3:]
-                variant_characteristics = attributes[9].split(":")
-                e_GT = variant_characteristics[0]
-                e_SO = variant_characteristics[1]
-                e_allele_1 = variant_characteristics[2].split("/")[0]
-                e_allele_2 = variant_characteristics[2].split("/")[1]
-                e_REPCI = variant_characteristics[3]
-                e_ADSP = variant_characteristics[4]
-                e_ADFL = variant_characteristics[5]
-                e_ADIR = variant_characteristics[6]
-                e_LC = variant_characteristics[7]
-                print(
-                    ','.join(
-                        [
-                            sample_id, 
-                            chr, 
-                            start, 
-                            e_qual, 
-                            end,
-                            repeat_units_in_ref,
-                            ref_sequence_length, 
-                            motif,
-                            e_GT,
-                            e_SO, 
-                            e_allele_1,
-                            e_allele_2,
-                            e_REPCI,
-                            e_ADSP, 
-                            e_ADFL,
-                            e_ADIR,
-                            e_LC
-                        ]
-                    )+'\n'
-                )
-    csv_output = j.call(eh_csv_writer, vcf_path)
-    b.write_output(csv_output.as_str(), output_path('eh_data_frame.csv'))
+    csv_array=[]
+    for vcf_file in vcf_path:
+        file= b.read_input(vcf_file)
+        csv_array.append(j.call(eh_csv_writer(file)).as_str()) 
+    jumbo_csv = j.call(concatenate_csv(csv_array)).as_str()
+    
+    b.write_output(jumbo_csv, output_path('eh_data_frame.csv'))
     b.run(wait=False)
 if __name__ == '__main__':
     main()  # pylint: disable=no-value-for-parameter
