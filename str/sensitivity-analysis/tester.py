cf212c476eb1645420c94e69fb0cd2d51337ab39
hopedisastro <40783500+hopedisastro@users.noreply.github.com>
2023-02-03 17:27:47 +1100

    Create tester.py

---
+++ str/sensitivity-analysis/tester.py
@@ -0,0 +1,25 @@
+
+import os
+import logging
+
+
+from cpg_utils.config import get_config
+from cpg_utils.hail_batch import output_path
+from cpg_workflows.batch import get_batch
+from google.cloud import storage
+config = get_config()
+
+def hello_world(name):
+    return f'hello {name}'
+
+
+def upper(s):
+    return s.upper()
+
+
+b = get_batch()
+j = b.new_python_job()
+hello_str = j.call(hello_world, 'alice')
+result = j.call(upper, hello_str)
+b.write_output(result.as_str(), 'output/hello-alice.txt')
+b.run()
