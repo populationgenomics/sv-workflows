c39979d70ecc353ef68c3a356af0f95fe11087f4
hopedisastro <40783500+hopedisastro@users.noreply.github.com>
2023-02-04 11:43:33 +1100

    Update data_frame_creator.py

--- str/sensitivity-analysis/data_frame_creator.py
+++ str/sensitivity-analysis/data_frame_creator.py
@@ -56,7 +56,7 @@ def main():
         return file
     tester = j.call(eh_csv_writer)
 
-    b.write_output(tester, output_path('eh_data_frame.csv'))
+    b.write_output(tester.as_str(), output_path('eh_data_frame.csv'))
     b.run(wait=False)
 
 if __name__ == '__main__':
