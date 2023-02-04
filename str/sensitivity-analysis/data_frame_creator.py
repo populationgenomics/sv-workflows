107cd1d743cdd5ebb9ed55458da639043cafba29
hopedisastro <40783500+hopedisastro@users.noreply.github.com>
2023-02-04 14:12:21 +1100

    adding gangstr

--- str/sensitivity-analysis/data_frame_creator.py
+++ str/sensitivity-analysis/data_frame_creator.py
@@ -28,13 +28,37 @@ def concatenate_csv(csv_array):
         combo_csv= combo_csv+i
     return combo_csv"""
 def eh_csv_writer():
-    input_dir = 'gs://cpg-hgdp-test/str/sensitivity-analysis/eh'
-    csv = ""
+    input_dir = 'gs://cpg-hgdp-test/str/sensitivity-analysis/eh' #can't seem to code this as an argument
     bucket_name, *components = input_dir[5:].split('/')
     client = storage.Client()
     bucket = client.bucket(bucket_name)
     blobs = client.list_blobs(bucket_name, prefix = '/'.join(components))
     files = {f'gs://{bucket_name}/{blob.name}' for blob in blobs}
+    csv = (
+            ','.join(
+                [   
+                    'sample_id',
+                    'chr',
+                    'start',
+                    'e_qual',
+                    'end',
+                    'repeat_units_in_ref',
+                    'ref_sequence_length',
+                    'motif',
+                    'e_GT',
+                    'e_SO',
+                    'e_allele_1',
+                    'e_alelle_2',
+                    'e_REPCI',
+                    'e_ADSP',
+                    'e_ADFL',
+                    'e_ADIR',
+                    'e_LC'
+                ]
+            )
+            +'\n'
+        )
+
     for file in files: 
         if file.endswith(".vcf"): 
             blob = bucket.blob(file[6+len(bucket_name):])
@@ -90,6 +114,88 @@ def eh_csv_writer():
                         )+'\n'
         )
     return csv
+
+def gangstr_csv_writer():
+    input_dir = 'gs://cpg-hgdp-test/str/sensitivity-analysis/gangstr' #can't seem to code this as an argument
+    bucket_name, *components = input_dir[5:].split('/')
+    client = storage.Client()
+    bucket = client.bucket(bucket_name)
+    blobs = client.list_blobs(bucket_name, prefix = '/'.join(components))
+    files = {f'gs://{bucket_name}/{blob.name}' for blob in blobs}
+    csv = (
+            ','.join(
+                [   
+                    'sample_id',
+                    'chr',
+                    'start',
+                    'g_GT',
+                    'g_DP',
+                    'g_Q',
+                    'g_REPCI',
+                    'g_RC',
+                    'g_ENCLREADS',
+                    'g_FLNKREADS',
+                    'g_ML',
+                    'g_INS',
+                    'g_STDERR',
+                    'g_allele_1',
+                    'g_allele_2',
+                    'g_QEXP'
+                ]
+            )
+            +'\n'
+        )
+    for file in files: 
+        if file.endswith(".vcf"): 
+            blob = bucket.blob(file[6+len(bucket_name):])
+            with blob.open("r") as f: 
+                array = f.readlines() 
+                for line in array:
+                    if line.startswith("##"):
+                        continue
+                    if line.startswith("#CHROM"):
+                        header = line.split()
+                        sample_id = header[9]
+                        continue
+                    attributes = line.split()
+                    chr = attributes[0]
+                    start = attributes[1]
+                    locus_characteristics = attributes[5].split(":")
+                    g_GT = locus_characteristics[0]
+                    g_DP = locus_characteristics[1]
+                    g_Q = locus_characteristics[2]
+                    g_REPCN = locus_characteristics[3]
+                    g_allele_1,g_allele_2 = g_REPCN.split(",")
+                    g_REPCI = locus_characteristics[4]
+                    g_RC = locus_characteristics[5]
+                    g_ENCLREADS = locus_characteristics[6]
+                    g_FLNKREADS = locus_characteristics[7]
+                    g_ML = locus_characteristics[8]
+                    g_INS = locus_characteristics[9]
+                    g_STDERR = locus_characteristics[10]
+                    g_QEXP = locus_characteristics[11]
+                    csv = csv+(
+                        ','.join(
+                            [
+                                sample_id, 
+                                chr, 
+                                start, 
+                                g_GT, 
+                                g_DP,
+                                g_Q,
+                                g_REPCI, 
+                                g_RC,
+                                g_ENCLREADS,
+                                g_FLNKREADS, 
+                                g_ML,
+                                g_INS,
+                                g_STDERR,
+                                g_allele_1, 
+                                g_allele_2
+                            ]
+                        )+'\n'
+        )
+    return csv
 @click.command()
 @click.option('--input-dir')
 def main(input_dir):
@@ -102,10 +208,12 @@ def main(input_dir):
     b = hb.Batch(backend= backend, default_python_image=config['workflow']['driver_image'])
     j = b.new_python_job(name = "EH dataframe writer")
     
-    #for vcf_file in vcf_path:
-    tester = j.call(eh_csv_writer)
+    eh_csv = j.call(eh_csv_writer)
+    gangstr_csv = j.call(gangstr_csv_writer)
 
-    b.write_output(tester.as_str(), output_path('eh_data_frame.txt'))
+    b.write_output(eh_csv.as_str(), output_path('eh.csv'))
+    b.write_output(gangstr_csv.as_str(), output_path('gangstr.csv'))
+
     b.run(wait=False)
 
 if __name__ == '__main__':
