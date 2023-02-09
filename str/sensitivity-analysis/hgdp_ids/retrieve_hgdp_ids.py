1dea4b2b76e6fbf0199f43678a64f5dc56efa9b0
hopedisastro <40783500+hopedisastro@users.noreply.github.com>
2023-02-09 13:28:44 +1100

    Update retrieve_hgdp_ids.py

--- str/sensitivity-analysis/hgdp_ids/retrieve_hgdp_ids.py
+++ str/sensitivity-analysis/hgdp_ids/retrieve_hgdp_ids.py
@@ -1,18 +1,34 @@
 """
 This script filters for HGDP ids that fit the French, Han or Yoruba population groups. 
 """
-
-from sample_metadata.apis import AnalysisApi, ParticipantApi
+import sample_metadata
+from sample_metadata.apis import SampleApi, ParticipantApi
 
 data = ParticipantApi().get_participants('hgdp')
+print(data[0])
 population_groups = ["French", "Yoruba", "Han", "Northern Han"]
-sample_ids = []
+participant_ids = []
 
 
 for i in data: 
     if i["meta"]["Population name"] in population_groups: 
-        sample_ids.append(i['external_id'])
-        print(i["meta"]["Population name"])
+        participant_ids.append(i['id'])
+
+body_get_samples = SampleApi().BodyGetSamples(
+        sample_ids=[
+            "",
+        ],
+        meta={},
+        participant_ids= participant_ids,
+        project_ids=[
+            "hgdp",
+        ],
+        active=True,
+    ) 
+
+SampleApi().get_samples(body_get_samples={'project_ids':['hgdp'], "participant_ids": participant_ids})
+
+
 
 print(len(sample_ids))
 print(sample_ids)
