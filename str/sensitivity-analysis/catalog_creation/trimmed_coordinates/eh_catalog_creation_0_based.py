3f42b53ac16e84314db3738085f6b21000b39179
hopedisastro <40783500+hopedisastro@users.noreply.github.com>
2023-02-10 11:48:17 +1100

    c

---
+++ str/sensitivity-analysis/catalog_creation/trimmed_coordinates/eh_catalog_creation_0_based.py
@@ -0,0 +1,8 @@
+from convert_gangstr_spec_to_expansion_hunter_variant_catalog_modified_0_based import process_variant_catalog
+
+process_variant_catalog("catalogs/marshfield_regions_gangstr_untrimmed_coordinates.bed", "catalogs/marshfield_regions_eh_trimmed_coordinates_0_based.json")
+
+
+
+
+
