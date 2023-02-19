79e0c9528657020fae16c881e5cdf89f40ae0e2e
hopedisastro <40783500+hopedisastro@users.noreply.github.com>
2023-02-20 09:35:51 +1100

    Update str_iterative_hipstr_runner.py

--- str/str_iterative_hipstr_runner.py
+++ str/str_iterative_hipstr_runner.py
@@ -4,7 +4,7 @@
 """
 This script uses HipSTR to call STRs on WGS cram files, using the joint calling option. 
 For example:
-analysis-runner --access-level standard --dataset hgdp --description 'hipstr run' --output-dir 'str/sensitivity-analysis/hipstr/untrimmed_coordinates_1_based' hipstr_runner_hgdp.py --variant-catalog=gs://cpg-hgdp-test/str/untrimmed_coordinates_resources/hg38_hipstr_catalog_untrimmed_coordinates.bed --input-dir=gs://cpg-hgdp-test/cram/nagim --external-wgs-ids HGDPXXXX HGDPXXXXY --output-file-name hipster_90_genomes
+analysis-runner --access-level test --dataset hgdp-test --description 'hipstr run' --output-dir 'str/sensitivity-analysis/hipstr/untrimmed_coordinates_1_based' hipstr_runner_hgdp.py --variant-catalog=gs://cpg-hgdp-test/str/untrimmed_coordinates_resources/hg38_hipstr_catalog_untrimmed_coordinates.bed --external-wgs-ids HGDP00511 --output-file-name hipster_90_genomes
 
 Required packages: sample-metadata, hail, click, os
 pip install sample-metadata hail click
@@ -40,7 +40,7 @@ def main(
     variant_catalog, dataset, external_wgs_ids, output_file_name):  # pylint: disable=missing-function-docstring
     # Initializing Batch 
     #b = get_batch()
-    b = hb.Batch(backend=None, default_image=os.getenv('DRIVER_IMAGE'))
+    b = get_batch()
 
     hipstr_job = b.new_job(name=f'HipSTR running')
     hipstr_job.image(HIPSTR_IMAGE)
@@ -54,7 +54,7 @@ def main(
             'log.txt': '{root}.log.txt'
         }
     )
-    hipstr_job.cloudfuse(f'cpg-{dataset}-main', '/cramfuse')
+    hipstr_job.cloudfuse(f'cpg-{dataset}', '/cramfuse')
     ref_fasta = ('gs://cpg-common-main/references/hg38/v0/Homo_sapiens_assembly38.fasta')
 
     external_id_to_cpg_id: dict[str, str] = SampleApi().get_sample_id_map_by_external(
