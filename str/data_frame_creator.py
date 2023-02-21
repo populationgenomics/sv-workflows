1ec98e92ab4ce81bc0afd0662ed13d6c5acda6d5
Hope Tanudisastro <40783500+hopedisastro@users.noreply.github.com>
2023-02-21 12:14:47 +1100

    Create data_frame_creator.py (#62)
    * Ensure hgdp gets the masked reference fasta
    
    * Create data_frame_creator.py
    
    * solve linting issue
    
    * Update data_frame_creator.py
    
    * Update data_frame_creator.py
    
    * ignore pylint error
    
    * Update data_frame_creator.py
    
    * Update str/data_frame_creator.py
    
    Co-authored-by: Matt Welland <mattwellie@gmail.com>
    
    * maxplit
    
    Co-authored-by: Matt Welland <mattwellie@gmail.com>
    
    * to_path()
    
    Co-authored-by: Matt Welland <mattwellie@gmail.com>
    
    * added cyvcf2 helper functions
    
    * Update data_frame_creator.py
    
    * Update data_frame_creator.py
    
    * Update data_frame_creator.py
    
    * Update data_frame_creator.py
    
    * Update data_frame_creator.py
    
    * Update data_frame_creator.py
    
    * Update data_frame_creator.py
    
    * Update data_frame_creator.py
    
    * Update data_frame_creator.py
    
    * Update data_frame_creator.py
    
    * trying out VCFReader
    
    * Update data_frame_creator.py
    
    * Update data_frame_creator.py
    
    * Update data_frame_creator.py
    
    * Update data_frame_creator.py
    
    * Update data_frame_creator.py
    
    * Update data_frame_creator.py
    
    * Update data_frame_creator.py
    
    * Update data_frame_creator.py
    
    * Update data_frame_creator.py
    
    * Update data_frame_creator.py
    
    * Update data_frame_creator.py
    
    * Update data_frame_creator.py
    
    * Update data_frame_creator.py
    
    * Update data_frame_creator.py
    
    * tsv nottation
    
    * read VCFReader()
    
    Co-authored-by: Matt Welland <mattwellie@gmail.com>
    
    * read VCFReader()
    
    Co-authored-by: Matt Welland <mattwellie@gmail.com>
    
    * Update data_frame_creator.py
    
    * Update data_frame_creator.py
    
    * updated AnalysisQueryModel for EH and GangSTR runners
    
    ---------
    
    Co-authored-by: Matt Welland <mattwellie@gmail.com>

---
+++ str/data_frame_creator.py
@@ -0,0 +1,224 @@
+#!/usr/bin/env python3
+# pylint: disable=import-error, too-many-locals, broad-exception-raised, ungrouped-imports
+
+"""
+This script merges all the VCFs from one STR caller into a .CSV (ExpansionHunter) or .TSV (GangSTR) format that can be read into R. 
+analysis-runner --access-level test --dataset tob-wgs --description 'data frame creator' --output-dir 'hoptan-str/tob_test_crams/data_frames' data_frame_creator.py  --input-dir-eh=gs://cpg-tob-wgs-test/hoptan-str/tob_test_crams/output_calls/eh_0_based --input-dir-gangstr=gs://cpg-tob-wgs-test/hoptan-str/tob_test_crams/output_calls/gangstr_0_based --output-name-eh=eh.csv --output-name-gangstr=gangstr.tsv
+
+"""
+from cyvcf2 import VCFReader
+import click
+from cpg_utils.config import get_config
+from cpg_utils.hail_batch import remote_tmpdir, output_path
+import hailtop.batch as hb
+from cpg_utils import to_path
+from cloudpathlib import GSPath
+
+config = get_config()
+
+
+def eh_csv_writer(input_dir):
+    """Creates a CSV file containing dataframe of merged EH VCFs"""
+    files = to_path(input_dir).glob('*.vcf')
+    csv = (
+        ','.join(
+            [
+                'sample_id',
+                'chr',
+                'start',
+                'e_qual',
+                'end',
+                'repeat_units_in_ref',
+                'ref_sequence_length',
+                'motif',
+                'e_gt',
+                'e_so',
+                'e_allele_1',
+                'e_allele_2',
+                'e_repci',
+                'e_adsp',
+                'e_adfl',
+                'e_adir',
+                'e_lc',
+            ]
+        )
+        + '\n'
+    )
+
+    for file in files:
+        if isinstance(file, GSPath):
+            file.copy(file.name)
+            file = file.name
+        reader = VCFReader(file)
+        sample_id = str(reader.samples[0])
+        for variant in reader:
+            chr = str(variant.CHROM)
+            start = str(variant.POS)
+            e_qual = str(variant.FILTER)
+            end = str(variant.INFO.get('END'))
+            repeat_units_in_ref = str(variant.INFO.get('REF'))
+            ref_sequence_length = str(variant.INFO.get('RL'))
+            motif = str(variant.INFO.get('RU'))
+            e_gt = f'{variant.genotypes[0][0]}/{variant.genotypes[0][1]}'
+            e_so = str(variant.format('SO')[0])
+            e_allele_1 = str(variant.format('REPCN')[0].split('/')[0])
+            e_allele_2 = str(variant.format('REPCN')[0].split('/')[1])
+            e_repci = str(variant.format('REPCI')[0])
+            e_adsp = str(variant.format('ADSP')[0])
+            e_adfl = str(variant.format('ADFL')[0])
+            e_adir = str(variant.format('ADIR')[0])
+            e_lc = str(variant.format('LC')[0][0])
+            csv = csv + (
+                ','.join(
+                    [
+                        sample_id,
+                        chr,
+                        start,
+                        e_qual,
+                        end,
+                        repeat_units_in_ref,
+                        ref_sequence_length,
+                        motif,
+                        e_gt,
+                        e_so,
+                        e_allele_1,
+                        e_allele_2,
+                        e_repci,
+                        e_adsp,
+                        e_adfl,
+                        e_adir,
+                        e_lc,
+                    ]
+                )
+                + '\n'
+            )
+    return csv
+
+
+def gangstr_tsv_writer(input_dir):
+    """Creates a TSV file containing dataframe of merged GangSTR VCFs"""
+    files = to_path(input_dir).glob('*.vcf')
+    tsv = (
+        '\t'.join(
+            [
+                'sample_id',
+                'chr',
+                'start',
+                'g_gt',
+                'g_dp',
+                'g_q',
+                'g_repci',
+                'g_rc',
+                'g_enclreads',
+                'g_flnkreads',
+                'g_ml',
+                'g_ins',
+                'g_stderr',
+                'g_allele_1',
+                'g_allele_2',
+                'g_qexp',
+            ]
+        )
+        + '\n'
+    )
+    for file in files:
+        if isinstance(file, GSPath):
+            file.copy(file.name)
+            file = file.name
+        reader = VCFReader(file)
+        sample_id = str(reader.samples[0])
+        for variant in reader:
+            chr = str(variant.CHROM)
+            start = str(variant.POS)
+            g_gt = str(variant.format('GT')[0])
+            if g_gt == '':  # ie variant is not called
+                g_gt = '.'
+                g_dp = '.'
+                g_q = '.'
+                g_repcn = '.'
+                g_allele_1 = '.'
+                g_allele_2 = '.'
+                g_repci = '.'
+                g_rc = '.'
+                g_enclreads = '.'
+                g_flnkreads = '.'
+                g_ml = '.'
+                g_ins = '.'
+                g_stderr = '.'
+                g_qexp = '.'
+            else:
+                g_gt = f'{variant.genotypes[0][0]}/{variant.genotypes[0][1]}'
+                g_dp = str(variant.format('DP')[0][0])
+                g_q = str(variant.format('Q')[0][0])
+                g_repcn = (
+                    f'{variant.format("REPCN")[0][0]}, {variant.format("REPCN")[0][1]}'
+                )
+                g_repci = str(variant.format('REPCI')[0])
+                g_rc = str(variant.format('RC')[0])
+                g_enclreads = str(variant.format('ENCLREADS')[0])
+                g_flnkreads = str(variant.format('FLNKREADS')[0])
+                g_ml = str(variant.format('ML')[0][0])
+                g_ins = f'{variant.format("INS")[0][0]},{variant.format("INS")[0][1]}'
+                g_stderr = (
+                    f'{variant.format("STDERR")[0][0]},{variant.format("STDERR")[0][1]}'
+                )
+                g_qexp = f'{variant.format("QEXP")[0][0]},{variant.format("QEXP")[0][1]},{variant.format("QEXP")[0][2]}'
+                g_allele_1 = str(g_repcn.split(',')[0].strip())
+                g_allele_2 = str(g_repcn.split(',')[1].strip())
+            tsv = tsv + (
+                '\t'.join(
+                    [
+                        sample_id,
+                        chr,
+                        start,
+                        g_gt,
+                        g_dp,
+                        g_q,
+                        g_repci,
+                        g_rc,
+                        g_enclreads,
+                        g_flnkreads,
+                        g_ml,
+                        g_ins,
+                        g_stderr,
+                        g_allele_1,
+                        g_allele_2,
+                        g_qexp,
+                    ]
+                )
+                + '\n'
+            )
+    return tsv
+
+
+@click.command()
+@click.option('--input-dir-eh', help='Input directory for ExpansionHunter VCFs')
+@click.option('--input-dir-gangstr', help='Input directory for GangSTR VCFs')
+@click.option('--output-name-eh', help='Output file name for ExpansionHunter eg eh.csv')
+@click.option(
+    '--output-name-gangstr', help='Output file name for GangSTR eg gangstr.tsv'
+)
+def main(input_dir_eh, input_dir_gangstr, output_name_eh, output_name_gangstr):
+    # pylint: disable=missing-function-docstring
+    # Initializing Batch
+    backend = hb.ServiceBackend(
+        billing_project=get_config()['hail']['billing_project'],
+        remote_tmpdir=remote_tmpdir(),
+    )
+    b = hb.Batch(
+        backend=backend, default_python_image=config['workflow']['driver_image']
+    )
+    j = b.new_python_job(name='EH dataframe writer')
+    g = b.new_python_job(name='GangSTR dataframe writer')
+
+    eh_csv = j.call(eh_csv_writer, input_dir_eh)
+    gangstr_tsv = g.call(gangstr_tsv_writer, input_dir_gangstr)
+
+    b.write_output(eh_csv.as_str(), output_path(output_name_eh, 'analysis'))
+    b.write_output(gangstr_tsv.as_str(), output_path(output_name_gangstr, 'analysis'))
+
+    b.run(wait=False)
+
+
+if __name__ == '__main__':
+    main()  # pylint: disable=no-value-for-parameter
--- str/str_iterative_eh_runner.py
+++ str/str_iterative_eh_runner.py
@@ -56,10 +56,13 @@ def main(
         cpg_id: external_wgs_id
         for external_wgs_id, cpg_id in external_id_to_cpg_id.items()
     }
-    if dataset == 'tob-wgs':
+    if dataset in ['tob-wgs', 'hgdp']:
         ref_fasta = (
             'gs://cpg-common-main/references/hg38/v0/Homo_sapiens_assembly38.fasta'
         )
+    else:
+        ref_fasta = reference_path('broad/ref_fasta')
+    if dataset == 'tob-wgs':
         analysis_query_model = AnalysisQueryModel(
             sample_ids=list(external_id_to_cpg_id.values()),
             projects=[dataset],
@@ -68,7 +71,6 @@ def main(
             meta={'sequence_type': 'genome', 'source': 'nagim'},
         )
     else:
-        ref_fasta = reference_path('broad/ref_fasta')
         analysis_query_model = AnalysisQueryModel(
             sample_ids=list(external_id_to_cpg_id.values()),
             projects=[dataset],
