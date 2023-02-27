c9fbcf0f24a44fc5014702a51d2168e268c20f79
hopedisastro <40783500+hopedisastro@users.noreply.github.com>
2023-02-27 13:42:10 +1100

    Update reviewer_runner.py

--- str/reviewer_runner.py
+++ str/reviewer_runner.py
@@ -2,7 +2,7 @@
 
 """
 This script will output REViewer svg based on inputs: one/multiple CPG IDs and one locus, as defined in the variant catalog. 
-analysis-runner --access-level test --dataset hgdp --description "reviewer" --output-dir 'str/410_sgd_loci/reviewer' reviewer_runner.py --catalog=gs://cpg-hgdp-test/str/410_sgdp_loci/catalogs/eh_catalog_hg38_backbone_trimmed_0_based.json --locus=chr3-67969584-67969611-AAC --input-dir=gs://cpg-hgdp-test/str/sensitivity-analysis/eh/trimmed_coordinates_0_based_hg38_backbone CPG265538		
+analysis-runner --access-level test --dataset hgdp --description "reviewer" --output-dir 'str/410_sgd_loci/reviewer' reviewer_runner.py --catalog=gs://cpg-hgdp-test/str/410_sgdp_loci/catalogs/eh_catalog_hg38_backbone_trimmed_0_based.json --locus=chr1-216547253-216547277-TGA --input-dir=gs://cpg-hgdp-test/str/sensitivity-analysis/eh/trimmed_coordinates_0_based_hg38_backbone CPG265538		
 
 """
 import click
@@ -23,9 +23,9 @@ REVIEWER_IMAGE = config['images']['reviewer']
 @click.option('--locus', help='Locus identifier as per catalog')
 @click.option('--catalog', help='GCP path to catalog')
 @click.option('--input-dir', help='GCP path to input-dir, includes gs://')
-@click.argument('cpg-wgs-ids', nargs=-1)
+@click.argument('cpg-sample-ids', nargs=-1)
 @click.command()
-def main(locus, catalog, input_dir, cpg_wgs_ids: list[str]):
+def main(locus, catalog, input_dir, cpg_sample_ids: list[str]):
 
     # Initializing Batch
     b = get_batch()
@@ -38,9 +38,9 @@ def main(locus, catalog, input_dir, cpg_wgs_ids: list[str]):
             + '.dict',
         )
     )
-    for cpg_id in cpg_wgs_ids:
+    for cpg_id in cpg_sample_ids:
         # read in bam file from EH output
-        bam_input = b.read_input(f'{input_dir}/{cpg_id}_eh.realigned.bam')
+        bam_input = b.read_input(f'{input_dir}/{cpg_id}_eh.realigned_bam')
 
         # read in VCF from EH output
         vcf_input = b.read_input(f'{input_dir}/{cpg_id}_eh.vcf')
@@ -48,8 +48,6 @@ def main(locus, catalog, input_dir, cpg_wgs_ids: list[str]):
         # BAM file must be sorted and indexed prior to inputting into REViewer
         samtools_job = b.new_job(name=f'Sorting and indexing {cpg_id}')
         samtools_job.image(SAMTOOLS_IMAGE)
-        samtools_job.storage('20G')
-        samtools_job.cpu(8)
         samtools_job.declare_resource_group(
             bam={
                 'sorted.bam': '{root}.sorted.bam',
@@ -72,8 +70,6 @@ def main(locus, catalog, input_dir, cpg_wgs_ids: list[str]):
         reviewer_job = b.new_job(name=f'Visualising {locus} for {cpg_id}')
         reviewer_job.image(REVIEWER_IMAGE)
         reviewer_job.depends_on(samtools_job)
-        reviewer_job.storage('20G')
-        reviewer_job.cpu(8)
 
         reviewer_job.declare_resource_group(
             ofile={
