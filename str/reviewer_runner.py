174a9362ce8f8ac19256f98cacbe7439ed181026
hopedisastro <40783500+hopedisastro@users.noreply.github.com>
2023-02-28 08:11:14 +1100

    Update reviewer_runner.py

--- str/reviewer_runner.py
+++ str/reviewer_runner.py
@@ -50,9 +50,8 @@ def main(locus, catalog, input_dir, cpg_sample_ids: list[str]):
         samtools_job.image(SAMTOOLS_IMAGE)
         file_size_bytes = AnyPath(os.path.join(input_dir, f'{cpg_id}_eh.realigned_bam')).stat().st_size
         padding = 5
-        file_size_gib = math.ceil(file_size_bytes/(1024**3))+padding
-        print(f'{file_size_gib+padding} potatoes')
-        samtools_job.storage('6G')
+        file_size_gib = str(math.ceil(file_size_bytes/(1024**3))+padding)+'G'
+        samtools_job.storage(file_size_gib)
         samtools_job.declare_resource_group(
             bam={
                 'sorted.bam': '{root}.sorted.bam',
