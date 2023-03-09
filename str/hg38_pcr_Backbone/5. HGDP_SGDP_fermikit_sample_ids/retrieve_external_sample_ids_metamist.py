3cbd9a238b993d0f141acd0efbe50ede1908571f
hopedisastro <40783500+hopedisastro@users.noreply.github.com>
2023-03-09 11:18:26 +1100

    add

--- str/hg38_pcr_Backbone/5. HGDP_SGDP_fermikit_sample_ids/retrieve_external_sample_ids_metamist.py
+++ str/hg38_pcr_Backbone/5. HGDP_SGDP_fermikit_sample_ids/retrieve_external_sample_ids_metamist.py
@@ -1,6 +1,7 @@
 import json
+import pandas as pd 
 
-from sample_metadata.apis import SeqrApi, ParticipantApi,SampleApi
+from sample_metadata.apis import ParticipantApi,SampleApi
 
 participants = ParticipantApi().get_participants('hgdp')
 
@@ -10,19 +11,19 @@ for i in participants:
 
 
 samples = SampleApi().get_samples(body_get_samples={'project_ids':['hgdp'], "participant_ids": participant_ids})
-print(samples[0])
 
 external_sample_ids=[]
 for i in samples: 
     external_sample_ids.append(i["external_id"])
 
-fermikit_mappings = open("Fermikit_ID_mapping.csv") 
+sgdp_metadata = open("sgdp_metadata_mappings.csv") 
 
-fermikit_mappings = fermikit_mappings.readlines()
+sgdp_metadata = sgdp_metadata.readlines()
 
 sgdp_id_to_external_sample_id={}
+sgdp_id_no_mapping = []
 
-for i in fermikit_mappings: 
+for i in sgdp_metadata: 
     if "SGDP_ID" in i: 
         continue # first line, no info 
     sgdp_id, illumina_id, sample_id = i.split(",")
@@ -35,15 +36,50 @@ for i in fermikit_mappings:
     elif sample_id in external_sample_ids: 
         value = sample_id
     else: 
-        value = "no mapping" 
+        sgdp_id_no_mapping.append(sgdp_id) 
         continue
     sgdp_id_to_external_sample_id[key] = value 
 
-print(len(sgdp_id_to_external_sample_id))#121
-print(sgdp_id_to_external_sample_id.values()) #sample IDs to run on GangSTR and EH 
+sgdp_id_no_mapping.append("S_Japanese-1") #SGDP id of LP6005441-DNA_C06 which is listed in metamist but has no crams stored
 
-with open('sgdp_id_to_external_sample_id.txt', 'w') as convert_file:
-     convert_file.write(json.dumps(sgdp_id_to_external_sample_id))
+print(len(sgdp_id_no_mapping)) #148
+
+#print(len(sgdp_id_to_external_sample_id)) #132
+
+sgdp_metadata_dataframe = pd.read_csv("sgdp_metadata_mappings.csv")
+#subset for SGDP-ids that have no match in metamist
+sgdp_metadata_dataframe = sgdp_metadata_dataframe.loc[sgdp_metadata_dataframe['SGDP_ID'].isin(sgdp_id_no_mapping)]
+illumina_ids_to_ingest = sgdp_metadata_dataframe[["Illumina_ID"]]
+
+PRJEB9586 = open("filereport_read_run_PRJEB9586_json.txt")
+PRJEB9586 = json.load(PRJEB9586)
+sample_accessions_to_ingest=[]
+external_ids_to_ingest=[]
+for i in PRJEB9586: 
+    try: 
+        link_1,link_2 = i['submitted_ftp'].split(";") 
+    except:
+        continue #bam file extension
+    link_1_sample_id_extension = link_1.split("/")[-1] #eg LP6005441-DNA_C06_1.fastq.gz
+    link_1_extension_length = len(link_1_sample_id_extension)
+    link_1_sample_id = link_1_sample_id_extension[:link_1_extension_length-11]
+    if link_1_sample_id in illumina_ids_to_ingest.values: 
+        external_ids_to_ingest.append(link_1_sample_id)
+        sample_accessions_to_ingest.append(i['sample_accession'])
+
+
+sample_accessions_to_ingest =list(dict.fromkeys(sample_accessions_to_ingest)) #remove duplicates
+external_ids_to_ingest = list(dict.fromkeys(external_ids_to_ingest)) #remove duplicates
+
+
+print(sample_accessions_to_ingest)
+print(len(sample_accessions_to_ingest)) #148
+print(external_ids_to_ingest)
+print(len(external_ids_to_ingest)) #148
+
+
+#with open('sgdp_id_to_external_sample_id.txt', 'w') as convert_file:
+   #  convert_file.write(json.dumps(sgdp_id_to_external_sample_id))
 
 
 
