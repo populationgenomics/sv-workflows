75bf5ad262b665b016a31833aa169d9ba395070a
hopedisastro <40783500+hopedisastro@users.noreply.github.com>
2023-04-10 10:43:15 +1000

    TOB EH Runner

--- str/tob-wgs/tob_eh_runner.py
+++ str/tob-wgs/tob_eh_runner.py
@@ -2,10 +2,10 @@
 # pylint: disable=import-error
 
 """
-This script uses ExpansionHunterv5 to call STRs on WGS cram files.
-Required input: --variant-catalog (file path to variant catalog), --dataset, and external sample IDs
+This script uses ExpansionHunter v5 to call STRs on the entire cohort of TOB-WGS (1057 samples)
+Required input: --variant-catalog (file path to variant catalog), --sample-id-file
 For example:
-analysis-runner --access-level test --dataset tob-wgs --description 'tester' --output-dir 'tester' str_iterative_eh_runner.py --variant-catalog=gs://cpg-tob-wgs-test/hoptan-str/Illuminavariant_catalog.json --dataset=tob-wgs TOB1XXXX TOB1XXXX
+analysis-runner --access-level full --dataset tob-wgs --description 'TOB Run' --output-dir 'tester' tob_eh_runner.py --variant-catalog=gs://cpg-tob-wgs-test/hoptan-str/Illuminavariant_catalog.json --sample-id-file=gs://cpg-tob-wgs-test/hoptan-str/karyotype_sex_mapping.csv
 
 Required packages: sample-metadata, hail, click, os
 pip install sample-metadata hail click
@@ -19,11 +19,11 @@ import hailtop.batch as hb
 
 from sample_metadata.model.analysis_type import AnalysisType
 from sample_metadata.model.analysis_query_model import AnalysisQueryModel
-from sample_metadata.apis import AnalysisApi, SampleApi
+from sample_metadata.apis import AnalysisApi
 from sample_metadata.models import AnalysisStatus
 
 from cpg_utils.config import get_config
-from cpg_utils.hail_batch import remote_tmpdir, output_path, reference_path
+from cpg_utils.hail_batch import remote_tmpdir, output_path
 
 config = get_config()
 
@@ -34,12 +34,12 @@ EH_IMAGE = config['images']['expansionhunter']
 # inputs:
 # variant catalog
 @click.option('--variant-catalog', help='Full path to Illumina Variants catalog')
-# input dataset
-@click.option('--dataset', help='dataset eg tob-wgs')
+# sample id and sex mapping file
+@click.option(
+    '--sample-id-file', help='Full path to mapping of CPG id, TOB id, and sex'
+)
 @click.command()
-def main(
-    variant_catalog, dataset
-):  # pylint: disable=missing-function-docstring
+def main(variant_catalog, sample_id_file):  # pylint: disable=missing-function-docstring
     # Initializing Batch
     backend = hb.ServiceBackend(
         billing_project=get_config()['hail']['billing_project'],
@@ -47,78 +47,79 @@ def main(
     )
     b = hb.Batch(backend=backend, default_image=os.getenv('DRIVER_IMAGE'))
 
-    ref_fasta = (
-            'gs://cpg-common-main/references/hg38/v0/Homo_sapiens_assembly38.fasta'
-        )
-    
-    if dataset == 'tob-wgs':
-        analysis_query_model = AnalysisQueryModel(
-            sample_ids=list(external_id_to_cpg_id.values()),
-            projects=[dataset],
-            type=AnalysisType('cram'),
-            status=AnalysisStatus('completed'),
-            meta={'sequencing_type': 'genome', 'source': 'nagim'},
-        )
-    
-    crams_path = AnalysisApi().query_analyses(analysis_query_model)
-    cpg_sids_with_crams = set(sid for sids in crams_path for sid in sids['sample_ids'])
-    cpg_sids_without_crams = set(cpg_id_to_external_id.keys()) - cpg_sids_with_crams
-    if cpg_sids_without_crams:
-        external_wgs_sids_without_crams = ', '.join(
-            cpg_id_to_external_id[sid] for sid in cpg_sids_without_crams
-        )
-        logging.warning(
-            f'There were some samples without CRAMs: {external_wgs_sids_without_crams}'
-        )
+    ref_fasta = 'gs://cpg-common-main/references/hg38/v0/Homo_sapiens_assembly38.fasta'
     eh_regions = b.read_input(variant_catalog)
 
-    # Iterate over each sample to call Expansion Hunter
-    for cram_obj in crams_path:
+    with AnyPath(sample_id_file).open() as f:
 
-        # Making sure Hail Batch would localize both CRAM and the correponding CRAI index
-        crams = b.read_input_group(
-            **{'cram': cram_obj['output'], 'cram.crai': cram_obj['output'] + '.crai'}
-        )
-        cpg_sample_id = cram_obj['sample_ids'][0]
+        # Iterate over each sample to call Expansion Hunter
+        for line in f:
+            cpg_id, sex, external_id = line.split(',')
+            if cpg_id == 's':  # header line
+                continue
+            if sex == 'XY':
+                sex_param = 'male'
+            else:
+                sex_param = 'female'
+                #'X' and 'ambiguous' karyotypic sex will be marked as female (ExpansionHunter defaults to female if no sex_parameter is provided)
 
-        # Working with CRAM files requires the reference fasta
-        ref = b.read_input_group(
-            **dict(
-                base=ref_fasta,
-                fai=ref_fasta + '.fai',
-                dict=ref_fasta.replace('.fasta', '')
-                .replace('.fna', '')
-                .replace('.fa', '')
-                + '.dict',
+            analysis_query_model = AnalysisQueryModel(
+                sample_ids=list([cpg_id]),
+                projects=['tob-wgs'],
+                type=AnalysisType('cram'),
+                status=AnalysisStatus('completed'),
+                meta={'sequencing_type': 'genome', 'source': 'nagim'},
             )
-        )
 
-        # ExpansionHunter job initialisation
-        eh_job = b.new_job(name=f'ExpansionHunter:{cpg_sample_id} running')
-        eh_job.image(EH_IMAGE)
-        eh_job.storage('50G')
-        eh_job.cpu(8)
+            cram_path = AnalysisApi().query_analyses(analysis_query_model)
 
-        eh_job.declare_resource_group(
-            eh_output={
-                'vcf': '{root}.vcf',
-                'json': '{root}.json',
-                'realigned.bam': '{root}_realigned.bam',
-            }
-        )
+            # Making sure Hail Batch would localize both CRAM and the correponding CRAI index
+            crams = b.read_input_group(
+                **{
+                    'cram': cram_path[0]['output'],
+                    'cram.crai': cram_path[0]['output'] + '.crai',
+                }
+            )
 
-        eh_job.command(
-            f"""
-        ExpansionHunter  \\
-        --reads {crams['cram']} \\
-        --reference {ref.base} --variant-catalog {eh_regions}\\
-         --threads 16 --analysis-mode streaming \\
-         --output-prefix {eh_job.eh_output}
-        """
-        )
-        # ExpansionHunter output writing
-        eh_output_path = output_path(f'{cpg_sample_id}_eh')
-        b.write_output(eh_job.eh_output, eh_output_path)
+            # Working with CRAM files requires the reference fasta
+            ref = b.read_input_group(
+                **dict(
+                    base=ref_fasta,
+                    fai=ref_fasta + '.fai',
+                    dict=ref_fasta.replace('.fasta', '')
+                    .replace('.fna', '')
+                    .replace('.fa', '')
+                    + '.dict',
+                )
+            )
+
+            # ExpansionHunter job initialisation
+            eh_job = b.new_job(name=f'ExpansionHunter:{cpg_id}')
+            eh_job.image(EH_IMAGE)
+            eh_job.storage('50G')
+            eh_job.cpu(8)
+
+            eh_job.declare_resource_group(
+                eh_output={
+                    'vcf': '{root}.vcf',
+                    'json': '{root}.json',
+                    'realigned.bam': '{root}_realigned.bam',
+                }
+            )
+
+            eh_job.command(
+                f"""
+            ExpansionHunter  \\
+            --reads {crams['cram']} \\
+            --reference {ref.base} --variant-catalog {eh_regions}\\
+            --threads 16 --analysis-mode streaming \\
+            --output-prefix {eh_job.eh_output} \\
+            --sex {sex_param}
+            """
+            )
+            # ExpansionHunter output writing
+            eh_output_path = output_path(f'{cpg_id}_eh')
+            b.write_output(eh_job.eh_output, eh_output_path)
 
     b.run(wait=False)
 
