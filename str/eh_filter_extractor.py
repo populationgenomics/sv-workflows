e0199a5ee3c213ef5a1baf45a81c2689482ccc12
hopedisastro <40783500+hopedisastro@users.noreply.github.com>
2023-05-17 09:23:52 +1000

    figured out how to fix the dictionary lint check!

--- str/eh_filter_extractor.py
+++ str/eh_filter_extractor.py
@@ -42,9 +42,9 @@ def eh_filter_extractor(input_dir):
         for variant in reader:
             locus = f'{variant.CHROM}:{variant.POS}'
             e_qual = str(variant.FILTER)
-            if e_qual == 'LowDepth' and locus in low_depth_dict.keys():
+            if e_qual == 'LowDepth' and locus in low_depth_dict:
                 low_depth_dict[locus].append(sample_id)
-            elif e_qual == 'LowDepth' and locus not in low_depth_dict.keys():
+            elif e_qual == 'LowDepth' and locus not in low_depth_dict:
                 low_depth_dict[locus] = [sample_id]
     low_depth_json = json.dumps(low_depth_dict, indent=4)
     return low_depth_json
