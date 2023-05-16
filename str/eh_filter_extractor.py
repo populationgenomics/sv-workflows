51a6f54aae419c0bff05dc495261b6b8b4569a4b
hopedisastro <40783500+hopedisastro@users.noreply.github.com>
2023-05-17 09:03:18 +1000

    switch to JSON, reduce output size

--- str/eh_filter_extractor.py
+++ str/eh_filter_extractor.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python3
-# pylint: disable=import-error, too-many-locals
 
 """
-This script extracts the EH binary filter (PASS/LowDepth) for all EH VCFs found within a directory into a .TSV format to be used as an annotation file in Hail query. 
+This script extracts sample/locus combinations with EH binary filter set to "LowDepth" for all EH VCFs found within a directory into a .JSON format to be used as an annotation file in Hail query. 
 
 analysis-runner --access-level standard --dataset tob-wgs --description \
     'EH Filter Extractor' --output-dir 'str/expansionhunter/v3-eh-filter-extractor' \
     eh_filter_extractor.py \
     --input-dir-eh=gs://cpg-tob-wgs-main/str/expansionhunter/v3 \
-    --output-name-eh=eh.tsv 
+    --output-name-eh=eh.json 
 """
 
 
 import click
+import json
+import logging
 from cloudpathlib import GSPath
 from cyvcf2 import VCFReader
 import hailtop.batch as hb
@@ -26,35 +27,42 @@ config = get_config()
 
 
 def eh_filter_extractor(input_dir):
-    """Creates a TSV file containing locus, sample_id, and binary filter status of EH VCFs"""
-    files = to_path(input_dir).glob('*.vcf')
-    tsv = ['sample_id\tlocus\te_qual\n']
+    """Creates a JSON file keyed by locus with value being a list of sample_ids with binary filter status == "LowDepth" in EH VCFs"""
 
+    files = to_path(input_dir).glob('*.vcf')
+    low_depth_dict = {}
     for file in files:
         if isinstance(file, GSPath):
             file.copy(file.name)
             file = file.name
+        else:
+            logging.warning(f'There is a file path that is not a GSPath: {file}')
+
         reader = VCFReader(file)
         sample_id = str(reader.samples[0])
         for variant in reader:
             locus = f'{variant.CHROM}:{variant.POS}'
             e_qual = str(variant.FILTER)
-            tsv.append(f'{sample_id}\t{locus}\t{e_qual}\n')
-    return tsv
+            if e_qual == "LowDepth" and locus in low_depth_dict.keys():
+                low_depth_dict[locus].append(sample_id)
+            elif e_qual == "LowDepth" and locus not in low_depth_dict.keys():
+                low_depth_dict[locus] = [sample_id]
+    low_depth_json = json.dumps(low_depth_dict, indent=4)
+    return low_depth_json
 
 
 @click.command()
 @click.option('--input-dir-eh', help='Input directory for ExpansionHunter VCFs')
 @click.option(
     '--output-name-eh',
-    help='Output file name for filter extractor output file eg eh.tsv',
+    help='Output file name for filter extractor output file eg eh.json',
 )
 def main(
     input_dir_eh,
     output_name_eh,
 ):
     """
-    some docstring
+    Hail batch job
     """
     # Initializing Batch
     backend = hb.ServiceBackend(
@@ -66,9 +74,9 @@ def main(
     )
     j = b.new_python_job(name='EH filter status extractor')
 
-    eh_tsv = j.call(eh_filter_extractor, input_dir_eh)
+    eh_json = j.call(eh_filter_extractor, input_dir_eh)
 
-    b.write_output(eh_tsv.as_str(), output_path(output_name_eh, 'analysis'))
+    b.write_output(eh_json.as_str(), output_path(output_name_eh, 'analysis'))
 
     b.run(wait=False)
 
