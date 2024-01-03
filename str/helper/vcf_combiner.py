0282db3840707728c2ff63fad9b2d11193203ee8
hopedisastro <40783500+hopedisastro@users.noreply.github.com>
2024-01-04 10:39:08 +1100

    Create vcf_combiner.py

---
+++ str/helper/vcf_combiner.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+# pylint: disable=import-error, too-many-locals
+
+"""
+This script combines sharded VCFs (from one sample and one caller) into one VCF.
+
+analysis-runner --access-level test --dataset tob-wgs --description \
+    'VCF combiner' --output-dir 'str/5M_run_combined_vcfs' \
+    vcf_combiner.py \
+    --input-dir=gs://cpg-tob-wgs-main-analysis/str/5M_run/CPG308288 \
+"""
+import click
+import sys
+
+from cpg_utils.config import get_config
+from cpg_utils import to_path
+from cpg_utils.hail_batch import get_batch, output_path
+
+config = get_config()
+
+def combine_vcf_files(input_dir, gcs_out_path):
+    input_files  = to_path(input_dir).glob('*.vcf')
+
+    # Initialize variables to store information
+    fileformat_line = ""
+    info_lines = []
+    alt_lines = set()
+    chrom_line = ""
+    gt_lines = []
+
+    # Process each input file
+    for file_index, input_file in enumerate(input_files):
+        with to_path(input_file).open() as f:
+            for line in f:
+                # Collect information from the header lines
+                if line.startswith('##fileformat') and file_index == 0:
+                    fileformat_line = line
+                elif (line.startswith('##INFO') or line.startswith('##FILTER') or line.startswith('##FORMAT')) and file_index == 0:
+                    info_lines.append(line)
+                elif line.startswith('##ALT'):
+                    # Collect ALT lines from all files into a set to remove duplicates
+                    alt_lines.add(line)
+                elif line.startswith('#CHROM') and file_index == 0:
+                    chrom_line = line
+                else:
+                    # Collect calls after #CHROM
+                    gt_lines.append(line)
+
+    # Sort ALT lines alphabetically and convert to a list
+    sorted_alt_lines = sorted(list(alt_lines))
+
+    # Write the combined information to the output file
+    with to_path(gcs_out_path).open('w') as out_file:
+        # Write fileformat line
+        out_file.write(fileformat_line)
+        # Write INFO, FILTER, and FORMAT lines
+        out_file.writelines(info_lines)
+        # Write ALT lines
+        out_file.writelines(sorted_alt_lines)
+        # Write CHROM line
+        out_file.write(chrom_line)
+        # Write GT or lines containing the calls
+        out_file.writelines(gt_lines)
+
+
+@click.command()
+@click.option('--input-dir',help='Input directory for sharded VCFs')
+
+def main(
+    input_dir
+):
+    # pylint: disable=missing-function-docstring
+    # Initializing Batch
+    b = get_batch()
+    cpg_id = input_dir.split('/')[-1]
+
+    combiner_job = b.new_python_job(name=f'VCF Combiner job: {cpg_id}')
+
+    out_path = output_path(f'{cpg_id}_combined.vcf')
+    combiner_job.call(combine_vcf_files, input_dir, out_path)
+
+    b.run(wait=False)
+
+
+if __name__ == '__main__':
+    main()  # pylint: disable=no-value-for-parameter
