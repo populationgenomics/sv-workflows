bdb2d9a1a07392e4c04e67c545e21ee3f0f1be2e
hopedisastro <40783500+hopedisastro@users.noreply.github.com>
2024-01-10 15:14:55 +1100

    Update joint_merge_str_runner.py

--- str/trtools/joint_merge_str_runner.py
+++ str/trtools/joint_merge_str_runner.py
@@ -34,7 +34,6 @@ TRTOOLS_IMAGE = config['images']['trtools']
 @click.option('--sample-list-1', help='gs://...')
 # sample list 2 (CPG sample IDs separated by \n)
 @click.option('--sample-list-2', help='gs://...')
-
 @click.option(
     '--job-storage', help='Storage of the Hail batch job eg 30G', default='50G'
 )
@@ -66,28 +65,28 @@ def main(
         for id in ids_1:
             each_vcf = os.path.join(input_dir_1, f'{id}_eh.reheader.vcf.gz')
             batch_vcfs.append(
-            b.read_input_group(
-                **{
-                    'vcf.gz': each_vcf,
-                    'vcf.gz.tbi': f'{each_vcf}.tbi',
-                }
-            )['vcf.gz']
-        )
-        num_samples = num_samples+ len(ids_1)
+                b.read_input_group(
+                    **{
+                        'vcf.gz': each_vcf,
+                        'vcf.gz.tbi': f'{each_vcf}.tbi',
+                    }
+                )['vcf.gz']
+            )
+        num_samples = num_samples + len(ids_1)
 
     with to_path(sample_list_2).open() as f_2:
         ids_2 = [line.strip() for line in f_2]
         for id in ids_2:
             each_vcf = os.path.join(input_dir_2, f'{id}_eh.reheader.vcf.gz')
             batch_vcfs.append(
-            b.read_input_group(
-                **{
-                    'vcf.gz': each_vcf,
-                    'vcf.gz.tbi': f'{each_vcf}.tbi',
-                }
-            )['vcf.gz']
-        )
-        num_samples = num_samples+ len(ids_2)
+                b.read_input_group(
+                    **{
+                        'vcf.gz': each_vcf,
+                        'vcf.gz.tbi': f'{each_vcf}.tbi',
+                    }
+                )['vcf.gz']
+            )
+        num_samples = num_samples + len(ids_2)
 
     trtools_job.command(
         f"""
@@ -97,7 +96,9 @@ def main(
     """
     )
 
-    output_path_name = output_path(f'joint_mergeSTR_{num_samples}_samples_eh', 'analysis')
+    output_path_name = output_path(
+        f'joint_mergeSTR_{num_samples}_samples_eh', 'analysis'
+    )
     b.write_output(trtools_job.vcf_output['vcf.gz'], f'{output_path_name}.vcf.gz')
     b.write_output(
         trtools_job.vcf_output['vcf.gz.tbi'], f'{output_path_name}.vcf.gz.tbi'
