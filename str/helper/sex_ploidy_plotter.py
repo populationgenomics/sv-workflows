a87e49450c6bf055b8190391213124c6d8c6e0c3
hopedisastro <40783500+hopedisastro@users.noreply.github.com>
2024-01-13 10:41:25 +1100

    black

--- str/helper/sex_ploidy_plotter.py
+++ str/helper/sex_ploidy_plotter.py
@@ -18,8 +18,7 @@ from cpg_utils.hail_batch import output_path, init_batch, get_batch
 from bokeh.plotting import output_file, save
 
 
-def sex_ploidy_plotter(file_path,gcs_path):
-
+def sex_ploidy_plotter(file_path, gcs_path):
     init_batch()
     sample_qc_table = hl.read_table(file_path)
 
@@ -30,7 +29,7 @@ def sex_ploidy_plotter(file_path,gcs_path):
         label=sample_qc_table.sex_karyotype,
         title='Scatterplot of chrY_ploidy vs chrX_ploidy',
         xlabel='chrX_ploidy',
-        ylabel='chrY_ploidy'
+        ylabel='chrY_ploidy',
     )
 
     # Save the plot to a local file, then hadoop_copy to copy to GCS bucket
@@ -46,12 +45,13 @@ def sex_ploidy_plotter(file_path,gcs_path):
 )
 @click.command()
 def main(file_path):
-
     b = get_batch()
     j = b.new_python_job(name=f'Sex ploidy plotter job')
     gcs_output_path = output_path(f'sex_ploidy_plot.html', 'analysis')
-    j.call(sex_ploidy_plotter, file_path,gcs_output_path)
+    j.call(sex_ploidy_plotter, file_path, gcs_output_path)
 
     b.run(wait=False)
+
+
 if __name__ == '__main__':
     main()  # pylint: disable=no-value-for-parameter
