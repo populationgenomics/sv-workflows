9b1daee2613d22ba591847b762d15d67760dd5e1
Hope Tanudisastro <40783500+hopedisastro@users.noreply.github.com>
2024-01-12 11:45:49 +1100

    range, not enumerate (#114)
    * range, not enumerate
    
    * black

--- str/trtools/merge_str_runner.py
+++ str/trtools/merge_str_runner.py
@@ -52,7 +52,7 @@ def main(
 ):  # pylint: disable=missing-function-docstring
     # Initializing Batch
     b = get_batch()
-    for shard_index in enumerate(num_shards, 1):
+    for shard_index in range(1, num_shards + 1):
         # Initialise TRTools job to run mergeSTR
         trtools_job = b.new_job(name=f'mergeSTR shard {shard_index}')
         trtools_job.image(TRTOOLS_IMAGE)
