2fcc17064458fd355adc97fbe395c7d8447c172f
hopedisastro <40783500+hopedisastro@users.noreply.github.com>
2024-01-13 09:56:02 +1100

    Update sex_ploidy_plotter.py

--- str/helper/sex_ploidy_plotter.py
+++ str/helper/sex_ploidy_plotter.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 # pylint: disable=missing-function-docstring,no-member
 """
-This Hail Query script outputs a CSV file containing the CPG ID, external ID, and karyotypic sex from the SampleQC Hail Table.
+This Hail Query script outputs a scatterplot plotting chrX and chrY ploidy, and labelling the points by inferred karyotypic sex from the SampleQC Hail Table.
 
  analysis-runner --dataset "bioheart" \
     --description "karyotypic sex extractor" \
     --access-level "test" \
-    --output-dir "hoptan-str/sample-sex-mapping/" \
-    karyotype_sex_extractor.py --file-path=gs://cpg-bioheart-test/large_cohort/1-0/sample_qc.ht/
+    --output-dir "hoptan-str/sex_ploidy_plot/" \
+    sex_ploidy_plotter.py --file-path=gs://cpg-bioheart-test/large_cohort/1-0/sample_qc.ht/
 
 """
 
@@ -17,17 +17,22 @@ import click
 from cpg_utils.hail_batch import output_path, init_batch
 
 
-def karyotype_sex_extractor(file_path, gcs_path):
+def sex_ploidy_plotter(file_path, gcs_path):
     init_batch()
     sample_qc_table = hl.read_table(file_path)
 
-    # select CPG ID, external ID, and sex karyotype (CPG ID is the key, so don't need to explicitly select)
-    karyotype_sex_table = sample_qc_table.select(
-        sample_qc_table.external_id, sample_qc_table.sex_karyotype
+    # Create a scatterplot matrix using Hail's plotting functions
+    p = hl.plot.scatter(
+        x=mt.chrY_ploidy,
+        y=mt.chrX_ploidy,
+        label=mt.sex_karyotype,
+        title='Scatterplot of chrY_ploidy vs chrX_ploidy',
+        xlabel='chrY_ploidy',
+        ylabel='chrX_ploidy'
     )
 
-    # output writing
-    karyotype_sex_table.export(gcs_path, delimiter=',')
+    # Save the plot to a file (replace 'output_path' with the desired file path and format)
+    p.save(gcs_path)
 
 
 @click.option(
@@ -37,8 +42,8 @@ def karyotype_sex_extractor(file_path, gcs_path):
 )
 @click.command()
 def main(file_path):
-    gcs_output_path = output_path(f'sample_karyotype_sex_mapping.csv', 'analysis')
-    karyotype_sex_extractor(file_path, gcs_output_path)
+    gcs_output_path = output_path(f'sex_ploidy_plot.png', 'analysis')
+    sex_ploidy_plotter(file_path, gcs_output_path)
 
 
 if __name__ == '__main__':
