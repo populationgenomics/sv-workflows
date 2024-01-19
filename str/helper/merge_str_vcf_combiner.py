82cd750dfcb2bad498220b2bc929570325853e5c
MattWellie <mattwellie@gmail.com>
2024-01-19 15:45:50 +1100

    de-batching and RAM-reduction attempt

--- str/helper/merge_str_vcf_combiner.py
+++ str/helper/merge_str_vcf_combiner.py
@@ -11,165 +11,141 @@ analysis-runner --access-level standard --dataset tob-wgs --description  \
 import gzip
 import click
 
-from cpg_utils.config import get_config
 from cpg_utils import to_path
-from cpg_utils.hail_batch import get_batch, output_path
+from cpg_utils.hail_batch import output_path
 
-config = get_config()
+# custom sorted order of shards - PLEASE UPDATE FOR LATER USE
+SORTED_KEY_ORDER = [
+    1,
+    12,
+    23,
+    34,
+    45,
+    47,
+    48,
+    49,
+    50,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    11,
+    13,
+    14,
+    15,
+    16,
+    17,
+    18,
+    19,
+    20,
+    21,
+    22,
+    24,
+    25,
+    26,
+    27,
+    28,
+    29,
+    30,
+    31,
+    32,
+    33,
+    35,
+    36,
+    37,
+    38,
+    39,
+    40,
+    41,
+    42,
+    43,
+    44,
+    46,
+]
 
 
-def combine_vcf_files(input_dir, gcs_out_path):
-    """Combines sharded mergeSTR output VCFs in input_dir into one combined VCF, writing it to a GCS output path"""
+@click.command()
+@click.option('--input-dir', help='Parent input directory for sharded VCFs')
+@click.option('--output', help='Name of output VCF', default='combined_eh.vcf')
+def main(input_dir, output):
+    """
+    Combines sharded mergeSTR output VCFs in input_dir into one combined VCF,
+    writing it to a GCS output path
+    Takes an input directory containing vcf shards
+    Aggregates all sharded data into a single output file
 
-    input_file_paths = to_path(input_dir).glob('*.vcf.gz')
+    Doesn't use batch any more, as mem, cpu and disk can all be analysis-runner CLI args
+    """
 
     # make input_files GSPath elements into a string type object
-    input_file_paths = {str(gs_path) for gs_path in input_file_paths}
+    input_file_paths = map(str, to_path(input_dir).glob('*.vcf.gz'))
 
     # create a dictionary where key is the shard number and value is the path to the shard
-    input_files_dict = {}
-    for file_path in input_file_paths:
-        key = int(file_path.split('eh_shard')[1].split('.')[0])
-        input_files_dict[key] = file_path
-
-    # custom sorted order of shards - PLEASE UPDATE FOR LATER USE
-    sorted_key_order = [
-        1,
-        12,
-        23,
-        34,
-        45,
-        47,
-        48,
-        49,
-        50,
-        2,
-        3,
-        4,
-        5,
-        6,
-        7,
-        8,
-        9,
-        10,
-        11,
-        13,
-        14,
-        15,
-        16,
-        17,
-        18,
-        19,
-        20,
-        21,
-        22,
-        24,
-        25,
-        26,
-        27,
-        28,
-        29,
-        30,
-        31,
-        32,
-        33,
-        35,
-        36,
-        37,
-        38,
-        39,
-        40,
-        41,
-        42,
-        43,
-        44,
-        46,
-    ]
+    input_files_dict = {
+        int(file_path.split('eh_shard')[1].split('.')[0]): file_path
+        for file_path in input_file_paths
+    }
 
     # Initialize variables to store information
     fileformat_line = ''
     info_lines = []
     alt_lines = set()
     chrom_line = ''
-    gt_lines = []
 
-    # Process each input file
-    for key in sorted_key_order:
-        input_file = to_path(input_files_dict[key])
-        print(f'Parsing {input_file}')
+    temporary_gt_file = 'temporary_gt_file.txt'
+    with open(temporary_gt_file, 'w', encoding='utf-8') as handle:
+        # Process each input file
+        for key in SORTED_KEY_ORDER:
+            input_file = to_path(input_files_dict[key])
+            print(f'Parsing {input_file}')
 
-        with gzip.open(input_file, 'rt') as f:
-            for line in f:
-                # Collect information from the header lines
-                if line.startswith('##fileformat'):
-                    if key == 1:  # first file processed is shard_1
-                        fileformat_line = line
-                elif (
-                    line.startswith('##INFO')
-                    or line.startswith('##FILTER')
-                    or line.startswith('##FORMAT')
-                    or line.startswith('##contig')
-                    or line.startswith('##command')
-                ):
-                    if key == 1:
-                        info_lines.append(line)
-                elif line.startswith('##ALT'):
-                    # Collect ALT lines from all files into a set to remove duplicates
-                    alt_lines.add(line)
-                elif line.startswith('#CHROM'):
-                    if key == 1:
-                        chrom_line = line
-                elif not line.startswith('#'):
-                    # Collect calls after #CHROM
-                    gt_lines.append(line)
+            with gzip.open(input_file, 'rt') as f:
+                for line in f:
+                    # Collect information from the header lines
+                    if line.startswith('##fileformat'):
+                        if key == 1:  # first file processed is shard_1
+                            fileformat_line = line
+                    elif (
+                        line.startswith('##INFO')
+                        or line.startswith('##FILTER')
+                        or line.startswith('##FORMAT')
+                        or line.startswith('##contig')
+                        or line.startswith('##command')
+                    ):
+                        if key == 1:
+                            info_lines.append(line)
+                    elif line.startswith('##ALT'):
+                        # Collect ALT lines from all files into a set to remove duplicates
+                        alt_lines.add(line)
+                    elif line.startswith('#CHROM'):
+                        if key == 1:
+                            chrom_line = line
+                    elif not line.startswith('#'):
+                        # Collect calls after #CHROM in a temp file
+                        handle.write(line)
 
-    print(f'Parsed {len(sorted_key_order)} sharded VCFs')
-    # Sort ALT lines alphabetically and convert to a list
-    sorted_alt_lines = sorted(alt_lines)
+    print(f'Parsed {len(SORTED_KEY_ORDER)} sharded VCFs')
 
     # Write the combined information to the output file
-    with to_path(gcs_out_path).open('w') as out_file:
+    with to_path(output_path(output, 'analysis')).open('w') as out_file:
         # Write fileformat line
         out_file.write(fileformat_line)
         # Write INFO, FILTER, and FORMAT lines
         out_file.writelines(info_lines)
-        # Write ALT lines
-        out_file.writelines(sorted_alt_lines)
+        # Write ALT lines, sorted
+        out_file.writelines(sorted(alt_lines))
         # Write CHROM line
         out_file.write(chrom_line)
-        # Write GT or lines containing the calls
-        out_file.writelines(gt_lines)
 
-
-@click.command()
-@click.option(
-    '--job-storage', help='Storage of the Hail batch job eg 30G', default='20G'
-)
-@click.option(
-    '--job-memory', help='Memory of the Hail batch job eg 64G', default='highmem'
-)
-@click.option('--job-cpu', help='CPU of the Hail batch job eg 16', default=8)
-@click.option(
-    '--input-dir',
-    help='Parent input directory for sharded VCFs',
-)
-def main(input_dir, job_memory, job_storage, job_cpu):
-    """
-    Takes an input directory containing vcf shards
-    Aggregates all sharded data into a single output file
-    """
-
-    # Initializing Batch
-    b = get_batch()
-
-    combiner_job = b.new_python_job(name=f'VCF Combiner job')
-    combiner_job.memory(job_memory)
-    combiner_job.storage(job_storage)
-    combiner_job.cpu(job_cpu)
-    out_path = output_path(f'combined_eh.vcf', 'analysis')
-    combiner_job.call(combine_vcf_files, input_dir, out_path)
-
-    b.run(wait=False)
+        # read-write all GT lines from temporary file
+        with open(temporary_gt_file, 'r', encoding='utf-8') as handle:
+            for line in handle:
+                out_file.write(line)
 
 
 if __name__ == '__main__':
