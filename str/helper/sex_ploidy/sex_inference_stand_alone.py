d288a49cce3b1f6cb40ea3e584225cb66b205821
hopedisastro <40783500+hopedisastro@users.noreply.github.com>
2024-01-29 17:56:30 +1100

    add coverage()

--- str/helper/sex_ploidy/sex_inference_stand_alone.py
+++ str/helper/sex_ploidy/sex_inference_stand_alone.py
@@ -18,7 +18,101 @@ import hail as hl
 from cpg_utils.hail_batch import reference_path, genome_build, output_path, init_batch
 from gnomad.sample_qc.pipeline import annotate_sex
 from hail.vds.variant_dataset import VariantDataset
+
+
 import click
+
+
+def generate_sex_coverage_mt(
+    vds: hl.vds.VariantDataset,
+    calling_intervals: hl.Table,
+):
+    """
+    generate the pre-computed MT of coverage intervals, to supply to annotate_sex
+    Args:
+        vds ():
+        calling_intervals ():
+
+    Returns:
+
+    """
+    key_dtype = calling_intervals.key.dtype
+    if (
+        len(key_dtype) != 1
+        or not isinstance(calling_intervals.key[0].dtype, hl.tinterval)
+        or calling_intervals.key[0].dtype.point_type != vds.reference_data.locus.dtype
+    ):
+        raise ValueError(
+            f'"impute_sex_chromosome_ploidy": expect calling_intervals to be list of intervals or '
+            f'table with single key of type interval<locus>, found table with key: {key_dtype}'
+        )
+
+    rg = vds.reference_data.locus.dtype.reference_genome
+    par_boundaries = []
+    for par_interval in rg.par:
+        par_boundaries.append(par_interval.start)
+        par_boundaries.append(par_interval.end)
+
+    # segment on PAR interval boundaries
+    calling_intervals = hl.segment_intervals(calling_intervals, par_boundaries)
+
+    # remove intervals overlapping PAR
+    calling_intervals = calling_intervals.filter(
+        hl.all(lambda x: ~x.overlaps(calling_intervals.interval), hl.literal(rg.par))
+    )
+
+    # checkpoint for efficient multiple downstream usages
+    checkpoint_path = output_path(f'sample_qc/calling_intervals_partial.ht', 'tmp')
+    if can_reuse(checkpoint_path, overwrite=True):
+        calling_intervals = hl.read_matrix_table(str(checkpoint_path))
+    else:
+        calling_intervals = calling_intervals.checkpoint(str(checkpoint_path))
+
+    interval = calling_intervals.key[0]
+    (any_bad_intervals, chrs_represented) = calling_intervals.aggregate(
+        (
+            hl.agg.any(interval.start.contig != interval.end.contig),
+            hl.agg.collect_as_set(interval.start.contig),
+        )
+    )
+    if any_bad_intervals:
+        raise ValueError(
+            "'impute_sex_chromosome_ploidy' does not support calling intervals that span chromosome boundaries"
+        )
+
+    if len(rg.x_contigs) != 1:
+        raise NotImplementedError(
+            f"reference genome {rg.name!r} has multiple X contigs, this is not supported in 'impute_sex_chromosome_ploidy'"
+        )
+    if len(rg.y_contigs) != 1:
+        raise NotImplementedError(
+            f"reference genome {rg.name!r} has multiple Y contigs, this is not supported in 'impute_sex_chromosome_ploidy'"
+        )
+
+    kept_contig_filter = hl.array(chrs_represented).map(
+        lambda x: hl.parse_locus_interval(x, reference_genome=rg)
+    )
+    vds = VariantDataset(
+        hl.filter_intervals(vds.reference_data, kept_contig_filter),
+        hl.filter_intervals(vds.variant_data, kept_contig_filter),
+    )
+
+    mt = vds.variant_data
+    calling_intervals = calling_intervals.annotate(interval_dup=interval)
+    mt = mt.annotate_rows(interval=calling_intervals[mt.locus].interval_dup)
+    mt = mt.filter_rows(hl.is_defined(mt.interval))
+    coverage = mt.select_entries(sum_dp=mt.DP, interval_size=hl.is_defined(mt.DP))
+    coverage = coverage.key_rows_by('locus')
+
+    # checkpoint the coverage mt prior to returning
+    checkpoint_path = output_path('sample_qc/sex_coverage_precomputed.ht', 'tmp')
+
+    if can_reuse(checkpoint_path, overwrite=True):
+        coverage = hl.read_matrix_table(str(checkpoint_path))
+    else:
+        coverage = coverage.checkpoint(str(checkpoint_path))
+
+    return coverage
 
 
 def impute_sex(
@@ -32,6 +126,9 @@ def impute_sex(
 
     # run()
     vds = hl.vds.read_vds(str(vds_path))
+
+    # generate sex coverage mt, checkpointed
+    coverage_mt = generate_sex_coverage_mt(vds, calling_intervals_ht)
 
     # Remove centromeres and telomeres:
     tel_cent_ht = hl.read_table(str(reference_path('gnomad/tel_and_cent_ht')))
@@ -57,7 +154,7 @@ def impute_sex(
             )
             vds = VariantDataset(
                 reference_data=vds.reference_data, variant_data=tmp_variant_data
-            )
+            ).checkpoint(output_path(f'{name}.vds', 'tmp'))
             logging.info(f'count post {name} filter:{vds.variant_data.count()}')
 
     # Infer sex (adds row fields: is_female, var_data_chr20_mean_dp, sex_karyotype)
@@ -67,6 +164,7 @@ def impute_sex(
         gt_expr='LGT',
         variants_only_x_ploidy=True,
         variants_only_y_ploidy=False,
+        coverage_mt=coverage_mt,
         variants_filter_lcr=False,  # already filtered above
         variants_filter_segdup=False,  # already filtered above
         variants_filter_decoy=False,
