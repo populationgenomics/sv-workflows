b525db02aef55ae6f4622ce585a500c645a850ed
hopedisastro <40783500+hopedisastro@users.noreply.github.com>
2024-02-05 15:28:22 +1100

    black

--- str/helper/mt_joiner.py
+++ str/helper/mt_joiner.py
@@ -34,17 +34,17 @@ def main(mt_path_1, mt_path_2):
     init_batch()
     mt_1 = hl.read_matrix_table(mt_path_1)
 
-    #rekey by REPID
-    mt_1 = mt_1.annotate_rows(REPID = mt_1.info.REPID)
-    mt_1 = mt_1.key_rows_by(REPID = mt_1['REPID'])
+    # rekey by REPID
+    mt_1 = mt_1.annotate_rows(REPID=mt_1.info.REPID)
+    mt_1 = mt_1.key_rows_by(REPID=mt_1['REPID'])
 
     print(f'{mt_path_1} dimensions: {mt_1.count()}')
 
     mt_2 = hl.read_matrix_table(mt_path_2)
 
-    #rekey by REPID
-    mt_2 = mt_2.annotate_rows(REPID = mt_2.info.REPID)
-    mt_2 = mt_2.key_rows_by(REPID = mt_2['REPID'])
+    # rekey by REPID
+    mt_2 = mt_2.annotate_rows(REPID=mt_2.info.REPID)
+    mt_2 = mt_2.key_rows_by(REPID=mt_2['REPID'])
 
     print(f'{mt_path_2} dimensions: {mt_2.count()}')
 
