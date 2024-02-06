9934ec7c654ffcfdf15ec73a77663905e605152d
Hope Tanudisastro <40783500+hopedisastro@users.noreply.github.com>
2024-02-06 12:55:41 +1100

    BGZIP and MT writer (#142)
    * Create mt_exporter.py
    
    * lint
    
    * rename
    
    * forgot basic stuff
    
    * update docu string
    
    * black
    
    * create a python job
    
    * lower memory resource
    
    * Update mt_exporter.py
    
    * black (
    
    * debug
    
    * revert
    
    * try batch tmpdir
    
    * batch tmpdir
    
    * revert
    
    * add in read_input()
    
    * trying something
    
    * Update mt_exporter.py
    
    * Update mt_exporter.py
    
    * Update mt_exporter.py
    
    * Update mt_exporter.py
    
    * Update mt_exporter.py
    
    * rename file
    
    * resurrecting mt_exporter
    
    * mt writer as standa lone script
    
    * lint
    
    * Update mt_extractor.py
    
    * black
    
    * rm mt_extractor - separate PR
    
    * Create mt_joiner.py (#144)
    
    * Create mt_joiner.py
    
    * add rekeying
    
    * rekey
    
    * black
    
    * key by
    
    Co-authored-by: Matt Welland <mattwellie@gmail.com>
    
    * key by for mt_2
    
    ---------
    
    Co-authored-by: Matt Welland <mattwellie@gmail.com>
    
    * add cpu param
    
    * Exports a MT to GCP bucket (#143)
    
    * Create mt_extractor.py
    
    * update docu
    
    * remove comment
    
    * remove type
    
    * remove analysis from o path
    
    * remove tabix
    
    * remove tabix
    
    ---------
    
    Co-authored-by: Matt Welland <mattwellie@gmail.com>

---
+++ str/helper/mt_extractor.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+This Hail Query script outputs a Hail matrix table  from a BGZIPPED VCF file.
+ analysis-runner --dataset "bioheart" \
+    --description "mt_extractor" \
+    --access-level "test" \
+    --output-dir "str/polymorphic_run/mt/v1" \
+    mt_extractor.py --file-path=gs://cpg-bioheart-test/str/polymorphic_run/bzip/v1/tester_file.vcf.bgz
+
+"""
+
+import hail as hl
+import click
+
+from cpg_utils.hail_batch import output_path, init_batch
+
+
+@click.option(
+    '--file-path',
+    help='GCS file path to BGZIP VCF file.',
+)
+@click.command()
+def main(file_path):
+    """writes a BGZIP VCF as a Hail Matrix Table to a GCS bucket"""
+
+    init_batch()
+    gcs_path = output_path('str.mt')
+    hl.import_vcf(file_path, force_bgz=True).write(gcs_path, overwrite=True)
+
+
+if __name__ == '__main__':
+    main()  # pylint: disable=no-value-for-parameter
