145cce55b9c46afb3c4660b60ab2eb3325f50032
Hope Tanudisastro <40783500+hopedisastro@users.noreply.github.com>
2024-02-21 16:36:56 +1100

    Cp Hail Matrix Table containing STR genotypes into `test` bucket  (#153)
    * Create cp_mt.sh
    
    * docu

---
+++ str/helper/cp_mt.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Copies Hail Matrix Table containing STR genotypes into test bucket
+
+set -ex
+
+gsutil -m cp -r gs://cpg-bioheart-main/str/polymorphic_run_n2045/mt/v1/str.mt gs://cpg-bioheart-test/str/polymorphic_run_n2045/mt/v1/str.mt
