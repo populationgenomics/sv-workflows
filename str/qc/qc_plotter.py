af15c82e01713379f0b9a70c8d6f3011b8f3cfab
hopedisastro <40783500+hopedisastro@users.noreply.github.com>
2024-02-22 13:48:25 +1100

    plots

--- str/qc/qc_plotter.py
+++ str/qc/qc_plotter.py
@@ -24,58 +24,79 @@ def main(mt_path):
 
     mt = hl.read_matrix_table(mt_path)
     #print(f'MT dimensions: {mt.count()}')
-    print(f'MT rows example: {mt.rows().show(5)}')
+
 
     # Filter out monomorphic loci
     mt = mt.filter_rows(mt.num_alleles>1)
     #print(f'MT dimensions after subsetting to loci with more than 1 allele: {mt.count()}')
 
 
-    # Locus level call rate
-    """
-    locus_call_rate = hl.plot.histogram(mt.variant_qc.call_rate,legend= "Locus level call rate")
-    output_file('locus_call_rate.html')
-    save(locus_call_rate)
-    gcs_path = output_path('locus_call_rate/locus_call_rate.html', 'analysis')
-    hl.hadoop_copy('locus_call_rate.html', gcs_path)
-    """
-
-
-    locus_call_rate = hl.plot.histogram(mt.filter_rows(mt.variant_qc.call_rate<1).variant_qc.call_rate,legend= "Locus level call rate<1")
-    output_file('locus_call_rate.html')
-    save(locus_call_rate)
-    gcs_path = output_path('locus_call_rate/locus_call_rate_lt_1.html', 'analysis')
-    hl.hadoop_copy('locus_call_rate.html', gcs_path)
-
-
-
-
-    """
-    non_mode_alleles_mt = mt.select_rows(
+    # Alleles minus mode histogram
+    alleles_minus_mode_ht = mt.select_rows(
     allele_minus_mode = hl.agg.collect(mt.allele_1_minus_mode)
         .extend(hl.agg.collect(mt.allele_2_minus_mode))
     ).rows()
-    non_mode_alleles_mt = non_mode_alleles_mt.explode('allele_minus_mode', name='alleles_minus_mode')
-    p = hl.plot.histogram(non_mode_alleles_mt.alleles_minus_mode,legend= "Allele sizes minus mode", range=(-50, 100))
+    alleles_minus_mode_ht = alleles_minus_mode_ht.explode('allele_minus_mode', name='alleles_minus_mode')
+    p = hl.plot.histogram(alleles_minus_mode_ht.alleles_minus_mode,legend= "Allele sizes minus mode", range=(-50, 100))
 
     output_file('local_plot.html')
     save(p)
-    gcs_path = output_path('allele_size_range/allele_size_range_50_to_100.html', 'analysis')
+    gcs_path = output_path('alleles_minus_mode/allele_size_range_50_to_100.html', 'analysis')
     hl.hadoop_copy('local_plot.html', gcs_path)
 
-    q = hl.plot.histogram(non_mode_alleles_mt.alleles_minus_mode,legend= "Allele sizes minus mode")
+    q = hl.plot.histogram(alleles_minus_mode_ht.alleles_minus_mode,legend= "Allele sizes minus mode")
     output_file('local_plot_1.html')
     save(q)
-    gcs_path_1 = output_path('allele_size_range/allele_size_range.html', 'analysis')
+    gcs_path_1 = output_path('alleles_minus_mode/allele_size_range.html', 'analysis')
     hl.hadoop_copy('local_plot_1.html', gcs_path_1)
 
-    r = hl.plot.histogram(non_mode_alleles_mt.alleles_minus_mode,legend= "Allele sizes minus mode", range=(-20, 20))
+    r = hl.plot.histogram(alleles_minus_mode_ht.alleles_minus_mode,legend= "Allele sizes minus mode", range=(-20, 20))
     output_file('local_plot_2.html')
     save(r)
-    gcs_path_2 = output_path('allele_size_range/allele_size_range_20_20.html', 'analysis')
+    gcs_path_2 = output_path('alleles_minus_mode/allele_size_range_20_20.html', 'analysis')
     hl.hadoop_copy('local_plot_2.html', gcs_path_2)
 
+    # Alleles histogram (num. repeats)
+    alleles_ht = mt.select_rows(
+    alleles = hl.agg.collect(mt.allele_1_rep_length)
+        .extend(hl.agg.collect(mt.allele_2_rep_length))
+    ).rows()
+    alleles_ht = alleles_ht.explode('alleles', name='alleles')
+    s = hl.plot.histogram(alleles_ht.alleles,legend= "Allele sizes (num. repeats)")
+    output_file('local_plot_3.html')
+    save(s)
+    gcs_path_3 = output_path('alleles_num_repeats/alleles_num_repeats.html', 'analysis')
+    hl.hadoop_copy('local_plot_3.html', gcs_path_3)
 
+    t = hl.plot.histogram(alleles_ht.alleles,legend= "Allele sizes (num. repeats)", range=(0, 50))
+    output_file('local_plot_4.html')
+    save(t)
+    gcs_path_4 = output_path('alleles_num_repeats/alleles_num_repeats_0_50.html', 'analysis')
+    hl.hadoop_copy('local_plot_4.html', gcs_path_4)
+
+    # Alleles histogram (bp length)
+    alleles_bp_ht = mt.select_rows(
+    alleles_bp = hl.agg.collect(mt.allele_1_bp_length)
+        .extend(hl.agg.collect(mt.allele_2_bp_length)
+    )
+    ).rows()
+    alleles_bp_ht = alleles_bp_ht.explode('alleles_bp', name='alleles_bp')
+    u = hl.plot.histogram(alleles_bp_ht.alleles_bp,legend= "Allele sizes (bp length)")
+    output_file('local_plot_5.html')
+    save(u)
+    gcs_path_5 = output_path('alleles_bp_length/alleles_bp_length.html', 'analysis')
+    hl.hadoop_copy('local_plot_5.html', gcs_path_5)
+
+    v = hl.plot.histogram(alleles_bp_ht.alleles_bp,legend= "Allele sizes (bp length)", range=(0, 500))
+    output_file('local_plot_6.html')
+    save(v)
+    gcs_path_6 = output_path('alleles_bp_length/alleles_bp_length_0_500.html', 'analysis')
+    hl.hadoop_copy('local_plot_6.html', gcs_path_6)
+
+
+
+
+    """
     #MT dimensions
     print(f'Original MT dimensions: {mt.count()}')
     mt = mt.filter_entries(mt.num_alleles > 1)
