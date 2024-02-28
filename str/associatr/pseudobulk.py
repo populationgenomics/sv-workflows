af7dd22230dda3c812f1fb2640ff37312ca8c92f
hopedisastro <40783500+hopedisastro@users.noreply.github.com>
2024-02-29 10:15:57 +1100

    adding analysis to output path

--- str/associatr/pseudobulk.py
+++ str/associatr/pseudobulk.py
@@ -69,7 +69,8 @@ def pseudobulk(input_file_path, target_sum):
 
     data_df.to_csv(
         output_path(
-            f'pseudobulk/{cell_type}/{cell_type}_chr{chrom_num}_pseudobulk.csv'
+            f'pseudobulk/{cell_type}/{cell_type}_chr{chrom_num}_pseudobulk.csv',
+            'analysis'
         ),
         index=False,
     )
