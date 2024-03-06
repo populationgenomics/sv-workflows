6b8368ea5b11dc7177335be96d5e4eee17f8b972
hopedisastro <40783500+hopedisastro@users.noreply.github.com>
2024-03-07 09:51:36 +1100

    Update pseudobulk.py

--- str/associatr/pseudobulk.py
+++ str/associatr/pseudobulk.py
@@ -2,24 +2,59 @@
 """
 This script performs pseudobulk (mean aggregation) of an input AnnData object
 Prior to pseudobulking, the following steps are performed:
-- Remove cells with no counts
 - Normalisation
 - Log transformation (ln(1+x))
-- Batch correction using ComBat
+- Batch correction
 
-Output is a TSV file by cell-type. Each row is a sample and each column is a gene.
+Output is a TSV file by cell-type and chromosome-specific. Each row is a sample and each column is a gene.
 
-analysis-runner --access-level test --dataset bioheart --image australia-southeast1-docker.pkg.dev/cpg-common/images/scanpy:1.9.3 --description "pseudobulk" --storage=200G --cpu=16 --memory=highmem --output-dir "str/associatr/input_files" pseudobulk.py --input-file=gs://cpg-bioheart-test/str/anndata/test.h5ad
+analysis-runner --access-level test --dataset bioheart --image australia-southeast1-docker.pkg.dev/cpg-common/images-dev/scanpy_sctransform --description "pseudobulk" --output-dir "str/associatr/input_files" pseudobulk.py \
+--input-file=gs://cpg-bioheart-test/str/anndata/saige-qtl/anndata_objects_from_HPC/ASDC_chr21.h5ad
 
 """
+import logging
 import click
 import numpy as np
 import pandas as pd
 import scanpy as sc
+from scipy.sparse import issparse
 
-from cpg_utils.hail_batch import output_path
+from cpg_utils.hail_batch import get_batch, output_path
 from cpg_utils import to_path
 
+def pyScTransform(adata, output_file=None):
+    """
+    Function to call scTransform from Python
+    """
+    import rpy2.robjects as ro
+    import anndata2ri
+
+    ro.r('library(Seurat)')
+    ro.r('library(scater)')
+    anndata2ri.activate()
+
+    sc.pp.filter_genes(adata, min_cells=5)
+
+    if issparse(adata.X):
+        if not adata.X.has_sorted_indices:
+            adata.X.sort_indices()
+
+    for key in adata.layers:
+        if issparse(adata.layers[key]):
+            if not adata.layers[key].has_sorted_indices:
+                adata.layers[key].sort_indices()
+
+    ro.globalenv['adata'] = adata
+
+    ro.r('seurat_obj = as.Seurat(adata, counts="X", data = NULL)')
+
+    ro.r('res <- SCTransform(object=seurat_obj, vars.to.regress = c("pct_counts_mt","batch:),return.only.var.genes = FALSE, do.correct.umi = FALSE)')
+
+    corrected_counts = ro.r('res@assays$SCT@counts').T
+
+    adata.X = corrected_counts
+    if output_file:
+        adata.write(output_file)
 
 # inputs:
 @click.option('--input-file', help='GCS Path to the input AnnData object')
@@ -34,43 +69,8 @@ def main(input_file):
     expression_h5ad_path = to_path(input_file).copy('here.h5ad')
     adata = sc.read_h5ad(expression_h5ad_path)
 
-    # remove cells with no counts
-    sc.pp.filter_cells(adata, min_counts=1)
+    pyScTransform(adata)
 
-    # normalisation
-    sc.pp.normalize_total(adata, target_sum=1e6)
-    # log transformation
-    sc.pp.log1p(adata)
-    adata.raw = adata
-
-    # batch correction
-    sc.pp.combat(adata, key='sequencing_library')
-
-    for cell_type in adata.obs.wg2_scpred_prediction.unique():
-        cell_type_adata = adata[adata.obs['wg2_scpred_prediction'] == cell_type]
-        # each cell type will have its own pseudobulk object
-        pbs = []
-        for chr in cell_type_adata.var.chr.unique():
-            chr_adata = cell_type_adata[:, cell_type_adata.var['chr'] == chr]
-            for sample in chr_adata.obs.individual.unique():
-                samp_cell_subset = adata[adata.obs['individual'] == sample]
-                # pseudobulk mean aggregation
-                rep_adata = sc.AnnData(
-                    X=np.matrix(samp_cell_subset.X.mean(axis=0)),
-                    var=samp_cell_subset.var[[]],
-                )
-                rep_adata.obs_names = [sample]
-                pbs.append(rep_adata)
-        # concatenate individual-specific pseudobulk objects
-        pb = sc.concat(pbs)
-
-        # convert pb into dataframe
-        data_df = pd.DataFrame(pb.X, index=pb.obs.index, columns=pb.var.index)
-        data_df = data_df.rename_axis('individual').reset_index()
-        cell_type_name = cell_type.replace(' ', '_')
-        data_df.to_csv(
-            output_path(f'pseudobulk/{cell_type_name}_pseudobulk.csv'), index=False
-        )
 
 
 if __name__ == '__main__':
