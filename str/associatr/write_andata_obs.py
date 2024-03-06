fcaac2a89557d0c1c61ead153cb9e89fcf608252
hopedisastro <40783500+hopedisastro@users.noreply.github.com>
2024-03-06 11:46:05 +1100

    Update write_andata_obs.py

--- str/associatr/write_andata_obs.py
+++ str/associatr/write_andata_obs.py
@@ -4,13 +4,14 @@
 This script writes the andata obs to a gcs bucket
 
 analysis-runner --dataset "bioheart" --access-level "test" --description "get cell counts per individual" --output-dir "str/associatr/helper" --image australia-southeast1-docker.pkg.dev/cpg-common/images/scanpy:1.9.3 \
---storage=200G --memory=standard --cpu=16 \
-write_andata_obs.py --input-h5ad-file-path=gs://cpg-bioheart-test/str/anndata/concatenated_gene_info_donor_info.h5ad
+--storage=20G --memory=standard --cpu=16 \
+write_andata_obs.py --input-h5ad-file-path=gs://cpg-bioheart-test/str/anndata/concatenated_gene_info_donor_info_obs.csv
 """
 
 import click
 import scanpy as sc
 from cpg_utils.hail_batch import output_path
+import pandas as pd
 from cpg_utils import to_path
 
 
@@ -21,13 +22,18 @@ def main(input_h5ad_file_path: str, version: str):
     """
     Extracts the number of cells per individual per cell type from the anndata object.
     """
-    # read in anndata object because anndata.obs has the CPG ID and cell type
-    expression_h5ad_path = to_path(input_h5ad_file_path).copy('here.h5ad')
-    adata = sc.read_h5ad(expression_h5ad_path, backed = 'r')
+    df = pd.read_csv(input_h5ad_file_path)
+    #create a dataframe with CPG ID, cell type, and counts of cells
+    counts = df.groupby(['cpg_id', 'wg2_scpred_prediction']).size()
 
-    output_gcs = 'gs://cpg-bioheart-test/str/anndata/concatenated_gene_info_donor_info_obs.csv'
+    input_file_name = input_h5ad_file_path.split('/')[-1].split('.')[0]
+
+    output_gcs = output_path(
+        f'{version}/{input_file_name}_cell_counts_per_individual.csv', 'analysis'
+    )
+
     # write to CSV
-    adata.obs.to_csv(output_gcs)
+    counts.to_csv(output_gcs, header=['count'])
 
 
 if __name__ == '__main__':
