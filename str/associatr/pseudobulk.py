d0f379df168e2ab1fbf31d62a81c948b25b0506c
hopedisastro <40783500+hopedisastro@users.noreply.github.com>
2024-03-07 09:47:14 +1100

    adding pyScTransform

--- str/associatr/pseudobulk.py
+++ str/associatr/pseudobulk.py
@@ -8,7 +8,7 @@ Prior to pseudobulking, the following steps are performed:
 
 Output is a TSV file by cell-type and chromosome-specific. Each row is a sample and each column is a gene.
 
-analysis-runner --access-level test --dataset bioheart --image australia-southeast1-docker.pkg.dev/cpg-common/images/scanpy:1.9.3 --description "pseudobulk" --output-dir "str/associatr/input_files" pseudobulk.py \
+analysis-runner --access-level test --dataset bioheart --image australia-southeast1-docker.pkg.dev/cpg-common/images-dev/scanpy_sctransform --description "pseudobulk" --output-dir "str/associatr/input_files" pseudobulk.py \
 --input-dir=gs://cpg-bioheart-test/str/anndata --cell-types=B_naive --chromosomes=10,22
 
 """
@@ -17,9 +17,44 @@ import click
 import numpy as np
 import pandas as pd
 import scanpy as sc
+from scipy.sparse import issparse
 
 from cpg_utils.hail_batch import get_batch, output_path
 from cpg_utils import to_path
+
+def pyScTransform(adata, output_file=None):
+    """
+    Function to call scTransform from Python
+    """
+    import rpy2.robjects as ro
+    import anndata2ri
+
+    ro.r('library(Seurat)')
+    ro.r('library(scater)')
+    anndata2ri.activate()
+
+    sc.pp.filter_genes(adata, min_cells=5)
+
+    if issparse(adata.X):
+        if not adata.X.has_sorted_indices:
+            adata.X.sort_indices()
+
+    for key in adata.layers:
+        if issparse(adata.layers[key]):
+            if not adata.layers[key].has_sorted_indices:
+                adata.layers[key].sort_indices()
+
+    ro.globalenv['adata'] = adata
+
+    ro.r('seurat_obj = as.Seurat(adata, counts="X", data = NULL)')
+
+    ro.r('res <- SCTransform(object=seurat_obj, vars.to.regress = c("pct_counts_mt","batch:),return.only.var.genes = FALSE, do.correct.umi = FALSE)')
+
+    corrected_counts = ro.r('res@assays$SCT@counts').T
+
+    adata.X = corrected_counts
+    if output_file:
+        adata.write(output_file)
 
 
 def pseudobulk(input_file_path, target_sum):
