0cb6dac01826c89c53fa4b8b8c19b4f1f1b1f57f
hopedisastro <40783500+hopedisastro@users.noreply.github.com>
2024-03-08 15:22:30 +1100

    removing pyScTransform

--- str/associatr/pseudobulk.py
+++ str/associatr/pseudobulk.py
@@ -22,40 +22,6 @@ from scipy.sparse import issparse
 from cpg_utils.hail_batch import get_batch, output_path
 from cpg_utils import to_path
 
-def pyScTransform(adata, output_file=None):
-    """
-    Function to call scTransform from Python
-    """
-    import rpy2.robjects as ro
-    import anndata2ri
-
-    ro.r('library(Seurat)')
-    ro.r('library(scater)')
-    anndata2ri.activate()
-
-    sc.pp.filter_genes(adata, min_cells=5)
-
-    if issparse(adata.X):
-        if not adata.X.has_sorted_indices:
-            adata.X.sort_indices()
-
-    for key in adata.layers:
-        if issparse(adata.layers[key]):
-            if not adata.layers[key].has_sorted_indices:
-                adata.layers[key].sort_indices()
-
-    ro.globalenv['adata'] = adata
-
-    ro.r('seurat_obj = as.Seurat(adata, counts="X", data = NULL)')
-
-    ro.r('res <- SCTransform(object=seurat_obj, vars.to.regress = c("pct_counts_mt","batch:),return.only.var.genes = FALSE, do.correct.umi = FALSE)')
-
-    corrected_counts = ro.r('res@assays$SCT@counts').T
-
-    adata.X = corrected_counts
-    if output_file:
-        adata.write(output_file)
-
 
 def pseudobulk(input_file_path, target_sum):
     """
