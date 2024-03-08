7b07473026e13198fbe2d9633ddd9ee25afd7b8a
hopedisastro <40783500+hopedisastro@users.noreply.github.com>
2024-03-08 16:15:54 +1100

    fix warning in hail batch

--- str/associatr/get_cis_numpy_files.py
+++ str/associatr/get_cis_numpy_files.py
@@ -96,14 +96,14 @@ def cis_window_numpy_extractor(
 
         # rank-based inverse normal transformation based on R's orderNorm()
         # Rank the values
-        gene_pheno.loc[:, 'gene_rank'] = gene_pheno[gene].rank()
+        gene_pheno.loc[:, 'gene_rank'] = gene_pheno.loc[:, gene].rank()
         # Calculate the percentile of each rank
-        gene_pheno.loc[:, 'gene_percentile'] = (gene_pheno['gene_rank'] - 0.5) / (
-            len(gene_pheno)
-        )
+        gene_pheno.loc[:, 'gene_percentile'] = (
+            gene_pheno.loc[:, 'gene_rank'] - 0.5
+        ) / (len(gene_pheno))
         # Use the inverse normal cumulative distribution function (quantile function) to transform percentiles to normal distribution values
         gene_pheno.loc[:, 'gene_inverse_normal'] = norm.ppf(
-            gene_pheno['gene_percentile']
+            gene_pheno.loc[:, 'gene_percentile']
         )
         gene_pheno = gene_pheno[['sample_id', 'gene_inverse_normal']]
 
