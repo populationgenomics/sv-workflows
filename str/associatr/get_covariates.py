44ae7e9ff208a5c24d7a2f3037bb887ea3ea3ce9
hopedisastro <40783500+hopedisastro@users.noreply.github.com>
2024-03-11 10:05:38 +1100

    add num pcs as an arg

--- str/associatr/get_covariates.py
+++ str/associatr/get_covariates.py
@@ -5,7 +5,7 @@ merges them with other pre-calculated covariates, and writes the file to GCP.
 
 analysis-runner --access-level test --dataset bioheart --image australia-southeast1-docker.pkg.dev/cpg-common/images/scanpy:1.9.3 \
 --description "Get covariates" --output-dir "str/associatr/input_files" get_covariates.py --input-dir=gs://cpg-bioheart-test/str/associatr/input_files/pseudobulk \
---cell-types=B_naive --chromosomes=10,22 --covariate-file-path=gs://cpg-bioheart-test/str/anndata/saige-qtl/input_files/covariates/sex_age_geno_pcs_tob_bioheart.csv
+--cell-types=ASDC --chromosomes=1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22 --covariate-file-path=gs://cpg-bioheart-test/str/anndata/saige-qtl/input_files/covariates/sex_age_geno_pcs_tob_bioheart.csv
 
 """
 
@@ -18,7 +18,14 @@ from cpg_utils.hail_batch import get_batch, output_path
 from cpg_utils import to_path
 
 
-def get_covariates(pseudobulk_input_dir, cell_type, chromosomes, covariate_file_path):
+def get_covariates(
+    pseudobulk_input_dir,
+    cell_type,
+    chromosomes,
+    covariate_file_path,
+    num_pcs,
+    ofile_path,
+):
     """
     Calculates cell-type specific PCs from the pseudobulk data (genome-wide),
     merges them with other pre-calculated covariates, and writes file to GCP
@@ -47,15 +54,18 @@ def get_covariates(pseudobulk_input_dir, cell_type, chromosomes, covariate_file_
     # PCA
     sc.tl.pca(adata_genome, svd_solver='arpack')
 
+    # Variance ratio plot
+    sc.pl.pca_variance_ratio(adata_genome, save=str(ofile_path))
+
     # extract PCs
     df_pcs = pd.DataFrame(adata_genome.obsm['X_pca'])
     df_pcs.index = adata_genome.obs.index
     df_pcs = df_pcs.rename_axis(
         'sample_id'
     ).reset_index()  # index (CPG ids) are not stored in 'sample_id' column
-    df_pcs = df_pcs[['sample_id'] + list(range(20))]  # only keep first 20 PCs
+    df_pcs = df_pcs[['sample_id'] + list(range(num_pcs))]  # only keep first 20 PCs
     df_pcs = df_pcs.rename(
-        columns={i: f'rna_PC{i+1}' for i in range(20)}
+        columns={i: f'rna_PC{i+1}' for i in range(num_pcs)}
     )  # rename PC columns: rna_PC{num}
 
     # read in covariates
@@ -81,6 +91,7 @@ def get_covariates(pseudobulk_input_dir, cell_type, chromosomes, covariate_file_
 @click.option('--job-memory', help='Memory of the batch job', default='standard')
 @click.option('--job-cpu', help='Number of CPUs of Hail batch job', default=8)
 @click.option('--covariate-file-path', help='GCS Path to the existing covariate file')
+@click.option('--num-pcs', help='Number of PCs to calculate', default=20)
 @click.command()
 def main(
     input_dir,
@@ -90,6 +101,7 @@ def main(
     job_memory,
     job_cpu,
     covariate_file_path,
+    num_pcs,
 ):
     """
     Obtain cell-type specific covariates for pseudobulk associaTR model
@@ -108,7 +120,22 @@ def main(
         j.cpu(job_cpu)
         j.memory(job_memory)
         j.storage(job_storage)
-        j.call(get_covariates, input_dir, cell_type, chromosomes, covariate_file_path)
+        j.call(
+            get_covariates,
+            input_dir,
+            cell_type,
+            chromosomes,
+            covariate_file_path,
+            num_pcs,
+            j.ofile,
+        )
+        j.ofile.add_extension('.png')
+        b.write_output(
+            j.ofile,
+            output_path(
+                f'covariates/scree_plots/{cell_type}_scree_plot.png', 'analysis'
+            ),
+        )
 
     b.run(wait=False)
 
