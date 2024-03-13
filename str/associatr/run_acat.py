b36ad04727625b72228309543816ee874db042ad
hopedisastro <40783500+hopedisastro@users.noreply.github.com>
2024-03-13 15:13:15 +1100

    increase chunk size

--- str/associatr/run_acat.py
+++ str/associatr/run_acat.py
@@ -134,7 +134,7 @@ def main(input_dir, cell_types, chromosomes, max_parallel_jobs):
             job.depends_on(_dependent_jobs[-max_parallel_jobs])
         _dependent_jobs.append(job)
 
-    genes_per_job = 15
+    genes_per_job = 30
 
     for cell_type in cell_types.split(','):
         for chromosome in chromosomes.split(','):
