7e6b9003820ab2f983b9fe9ff348acb205abf47d
Hope Tanudisastro <40783500+hopedisastro@users.noreply.github.com>
2024-03-21 09:28:32 +1100

    Replace `normalize_total` method (#157)
    * DIY normalize method
    
    * lint
    
    * more descriptive name for pytjon job
    
    * remove filter for empty cells (not needed)
    
    * adding analysis to output path
    
    * black again
    
    * try with backed mode
    
    * undo backed mode
    
    * Changing batch correction method - comBat produces NaN values in some contexts
    
    * edit o-path prefix
    
    * adding pyScTransform
    
    * removing pyScTransform
    
    * lint
    
    * filter out lowly expressed genes (get the anndata obj size down!)
    
    * lint
    
    * black
    
    * update docu
    
    * use image_path
    
    * black
    
    * outputs to main not main analysis
    
    * update min_pct = 1 default
    
    * Create get_covariates.py for associaTR pseudobulk pipeline (#158)
    
    * Create get_covariates.py
    
    * black, and analysis runner docu
    
    * trying to fix .copy() error
    
    * do not trust CoPilot to autocomplete your input file path dirs - it will give you an imaginary file path XD
    
    * black, analysis o path
    
    * black again
    
    * debug w logging
    
    * rint
    
    * finished debugging - remove print statements
    
    * add num pcs as an arg
    
    * update default params
    
    * save png to local file and hadoop cp
    
    * file path notation
    
    * Update get_covariates.py
    
    * update file directory
    
    * use the first 6 geno pcs only
    
    * change o path
    
    * docu
    
    * lint
    
    * remove comment
    
    * black
    
    * Get cis-window and numpy object files - inputs for associaTR (#159)
    
    * Create get_cis_numpy_files.py
    
    * black
    
    * lint
    
    * add documentation
    
    * lint
    
    * add docu
    
    * int() type casting
    
    * init_batch
    
    * import init_batch
    
    * try with to_path
    
    * ofile writing, add max concurrency
    
    * o path
    
    * Update get_cis_numpy_files.py
    
    * Update get_cis_numpy_files.py
    
    * Update get_cis_numpy_files.py
    
    * lint
    
    * disable too-many-arguments
    
    * disable too many arguments
    
    * Update get_cis_numpy_files.py
    
    * update for loop
    
    * filter lowly expressed genes; rank based inverse normalisation
    
    * improve docu
    
    * disable too-many-locals
    
    * black
    
    * allow_pickle=False to np.save()
    
    * undo allow_pickle
    
    * done debugging
    
    * fix key error
    
    * logic
    
    * Update get_cis_numpy_files.py
    
    * lint
    
    * lint
    
    * fix warning in hail batch
    
    * Update get_cis_numpy_files.py
    
    * add in cohort as a var
    
    * add option to remove samples
    
    * black
    
    * lint
    
    * lint
    
    * add image_path
    
    Co-authored-by: Matt Welland <mattwellie@gmail.com>
    
    * use image path
    
    Co-authored-by: Matt Welland <mattwellie@gmail.com>
    
    * replace params with config
    
    * lint
    
    * lint
    
    * fix image path key
    
    * update toml
    
    * black
    
    * removing filter lowly expressed genes because now in pseudobulk.py
    
    * add back in gene_names - extract from pseudbulk
    
    * adding back lowly exp genes filter for calibration exp
    
    * remove lowly expressed genes
    
    * lint
    
    * Update get_cis_numpy_files.py
    
    * update toml
    
    ---------
    
    Co-authored-by: Matt Welland <mattwellie@gmail.com>
    
    ---------
    
    Co-authored-by: Matt Welland <mattwellie@gmail.com>
    
    ---------
    
    Co-authored-by: Matt Welland <mattwellie@gmail.com>

--- str/associatr/pseudobulk.py
+++ str/associatr/pseudobulk.py
@@ -2,75 +2,134 @@
 """
 This script performs pseudobulk (mean aggregation) of an input AnnData object
 Prior to pseudobulking, the following steps are performed:
-- Remove cells with no counts
+- Filtering out lowly expressed genes
 - Normalisation
 - Log transformation (ln(1+x))
-- Batch correction using ComBat
+- Batch correction
 
-Output is a TSV file by cell-type. Each row is a sample and each column is a gene.
+Output is a TSV file by cell-type and chromosome-specific. Each row is a sample and each column is a gene.
 
-analysis-runner --access-level test --dataset bioheart --image australia-southeast1-docker.pkg.dev/cpg-common/images/scanpy:1.9.3 --description "pseudobulk" --storage=200G --cpu=16 --memory=highmem --output-dir "str/associatr/input_files" pseudobulk.py --input-file=gs://cpg-bioheart-test/str/anndata/test.h5ad
+analysis-runner --access-level test --dataset bioheart --image australia-southeast1-docker.pkg.dev/cpg-common/images/scanpy:1.9.3 --description "pseudobulk" --output-dir "str/associatr/input_files" pseudobulk.py \
+--input-dir=gs://cpg-bioheart-test/str/anndata/saige-qtl/anndata_objects_from_HPC --cell-types=CD4_TCM --chromosomes=1 --job-memory=highmem --job-cpu=16
 
 """
+import math
+
+import logging
 import click
 import numpy as np
 import pandas as pd
 import scanpy as sc
 
-from cpg_utils.hail_batch import output_path
+from cpg_utils.hail_batch import get_batch, output_path, image_path
 from cpg_utils import to_path
 
 
-# inputs:
-@click.option('--input-file', help='GCS Path to the input AnnData object')
-@click.command()
-def main(input_file):
+def pseudobulk(input_file_path, target_sum, min_pct):
     """
-    Perform pseudobulk (mean aggregation) of an input AnnData object
-
-    Args
-        input_file (str): GCS Path to the input AnnData object
+    Performs pseudobulking in a cell-type chromosome-specific manner
     """
-    expression_h5ad_path = to_path(input_file).copy('here.h5ad')
+    expression_h5ad_path = to_path(input_file_path).copy('here.h5ad')
     adata = sc.read_h5ad(expression_h5ad_path)
 
-    # remove cells with no counts
-    sc.pp.filter_cells(adata, min_counts=1)
+    # filter out lowly expressed genes
+    n_all_cells = len(adata.obs.index)
+    min_cells = math.ceil((n_all_cells * min_pct) / 100)
+    sc.pp.filter_genes(adata, min_cells=min_cells)
 
     # normalisation
-    sc.pp.normalize_total(adata, target_sum=1e6)
+    # we can't use pp.normalize_total because the expected input is a chr-specific anndata file, while
+    # pp.normalize_total expects a whole genome-wide anndata file
+    # obs.total_counts is the sum of counts (genome-wide) for each cell, determined in prior QC processing steps
+    total_counts = adata.obs['total_counts'].values
+    scaled_x = (adata.X / total_counts[:, np.newaxis]) * target_sum
+    adata.X = scaled_x
+
     # log transformation
     sc.pp.log1p(adata)
     adata.raw = adata
 
     # batch correction
-    sc.pp.combat(adata, key='sequencing_library')
+    sc.pp.regress_out(adata, keys='sequencing_library')
 
-    for cell_type in adata.obs.wg2_scpred_prediction.unique():
-        cell_type_adata = adata[adata.obs['wg2_scpred_prediction'] == cell_type]
-        # each cell type will have its own pseudobulk object
-        pbs = []
-        for chr in cell_type_adata.var.chr.unique():
-            chr_adata = cell_type_adata[:, cell_type_adata.var['chr'] == chr]
-            for sample in chr_adata.obs.individual.unique():
-                samp_cell_subset = adata[adata.obs['individual'] == sample]
-                # pseudobulk mean aggregation
-                rep_adata = sc.AnnData(
-                    X=np.matrix(samp_cell_subset.X.mean(axis=0)),
-                    var=samp_cell_subset.var[[]],
-                )
-                rep_adata.obs_names = [sample]
-                pbs.append(rep_adata)
-        # concatenate individual-specific pseudobulk objects
-        pb = sc.concat(pbs)
+    # initialise array to hold pseudobulk anndata objects
+    pbs = []
 
-        # convert pb into dataframe
-        data_df = pd.DataFrame(pb.X, index=pb.obs.index, columns=pb.var.index)
-        data_df = data_df.rename_axis('individual').reset_index()
-        cell_type_name = cell_type.replace(' ', '_')
-        data_df.to_csv(
-            output_path(f'pseudobulk/{cell_type_name}_pseudobulk.csv'), index=False
+    for sample in adata.obs.individual.unique():
+        samp_cell_subset = adata[adata.obs['individual'] == sample]
+        # pseudobulk mean aggregation
+        rep_adata = sc.AnnData(
+            X=np.matrix(samp_cell_subset.X.mean(axis=0)),
+            var=samp_cell_subset.var[[]],
         )
+        rep_adata.obs_names = [sample]
+        pbs.append(rep_adata)
+
+    # concatenate individual-specific pseudobulk objects
+    pb = sc.concat(pbs)
+
+    # convert pb into dataframe
+    data_df = pd.DataFrame(pb.X, index=pb.obs.index, columns=pb.var.index)
+    data_df = data_df.rename_axis('individual').reset_index()
+
+    cell_type = input_file_path.split('/')[-1].split('_chr')[0]
+    chrom_num = input_file_path.split('/')[-1].split('_chr')[1].split('.')[0]
+
+    data_df.to_csv(
+        output_path(f'{cell_type}/{cell_type}_chr{chrom_num}_pseudobulk.csv'),
+        index=False,
+    )
+
+
+# inputs:
+@click.option('--input-dir', help='GCS Path to the input AnnData object')
+@click.option('--cell-types', help='Name of the cell type, comma separated if multiple')
+@click.option(
+    '--chromosomes', help='Chromosome number eg 1, comma separated if multiple'
+)
+@click.option('--job-storage', help='Storage of the batch job eg 30G', default='8G')
+@click.option('--job-memory', help='Memory of the batch job', default='standard')
+@click.option('--job-cpu', help='Number of CPUs of Hail batch job', default=8)
+@click.option(
+    '--target-sum',
+    help='Target sum of counts per cell (for normalization purposes)',
+    default=1e6,
+)
+@click.option(
+    '--min-pct',
+    help='Minimum percentage of cells expressing a gene to be included in the pseudobulk',
+    default=1,
+)
+@click.command()
+def main(
+    input_dir,
+    cell_types,
+    chromosomes,
+    job_storage,
+    job_memory,
+    job_cpu,
+    target_sum,
+    min_pct,
+):
+    """
+    Perform pseudobulk (mean aggregation) of an input AnnData object
+
+    """
+    b = get_batch('Run pseudobulk')
+
+    logging.info(f'Cell types to run: {cell_types}')
+    logging.info(f'Chromosomes to run: {chromosomes}')
+
+    for cell_type in cell_types.split(','):
+        for chromosome in chromosomes.split(','):
+            input_file = f'{input_dir}/{cell_type}_chr{chromosome}.h5ad'
+            j = b.new_python_job(name=f'Pseudobulk for {cell_type}: chr{chromosome}')
+            j.image(image_path('scanpy'))
+            j.cpu(job_cpu)
+            j.memory(job_memory)
+            j.storage(job_storage)
+            j.call(pseudobulk, input_file, target_sum, min_pct)
+    b.run(wait=False)
 
 
 if __name__ == '__main__':
