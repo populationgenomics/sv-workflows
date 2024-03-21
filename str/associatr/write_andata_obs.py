cf7a0f3932534303573bf4a4816211b2c0ac7f46
hopedisastro <40783500+hopedisastro@users.noreply.github.com>
2024-03-21 15:34:16 +1100

    Update write_andata_obs.py

--- str/associatr/write_andata_obs.py
+++ str/associatr/write_andata_obs.py
@@ -4,8 +4,8 @@
 This script writes the andata obs to a gcs bucket
 
 analysis-runner --dataset "bioheart" --access-level "test" --description "get cell counts per individual" --output-dir "str/associatr/helper/anndata-240" --image australia-southeast1-docker.pkg.dev/cpg-common/images/scanpy:1.9.3 \
---storage=200G --memory=highmem --cpu=16 \
-write_andata_obs.py --input-h5ad-dir=gs://cpg-bioheart-test/str/anndata-240/240_libraries_concatenated_gene_info.h5ad
+--storage=200G --memory=highmem --cpu=8 \
+write_andata_obs.py --input-h5ad-file-path=gs://cpg-bioheart-test/str/240_libraries_tenk10kp1_v2/240_libraries_concatenated_gene_info.h5ad
 """
 
 import click
@@ -25,7 +25,7 @@ def main(input_h5ad_file_path: str, version: str):
     expression_h5ad_path = to_path(input_h5ad_file_path).copy('here.h5ad')
     adata = sc.read_h5ad(expression_h5ad_path, backed = 'r')
 
-    output_gcs = 'gs://cpg-bioheart-test/str/anndata-240/concatenated_gene_info_donor_info_obs.csv'
+    output_gcs = 'gs://cpg-bioheart-test/str/240_libraries_tenk10kp1_v2/concatenated_gene_info_donor_info_obs.csv'
     # write to CSV
     adata.obs.to_csv(output_gcs)
 
