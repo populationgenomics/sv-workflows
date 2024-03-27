624847e90ef55b8e9612dba5c6c73eeb56779986
hopedisastro <40783500+hopedisastro@users.noreply.github.com>
2024-03-27 19:07:24 +1100

    try

--- str/helper/str_pca_dataproc_runner.py
+++ str/helper/str_pca_dataproc_runner.py
@@ -15,19 +15,12 @@ def main():
         f'dataproc_helper/str_pca_dataproc_hail_script.py '
         f'--file-path=gs://cpg-bioheart-test/str/associatr/mt_filtered/v1/str.mt'
     )
-    j = dataproc.hail_dataproc_job(
+    j = dataproc_job(
                 get_batch(),
                 function = pca_runner,
-                max_age='48h',
-                packages=[
-                    'cpg_workflows',
-                    'google',
-                    'gcloud',
-                ],
-                num_workers=2,
-                num_secondary_workers=0,
-                job_name='STR-PCA',
-            )
+                function_path_args=dict(file_path='gs://cpg-bioheart-test/str/associatr/mt_filtered/v1/str.mt'),
+                job_name='STR-PCA')
+
     j._preemptible = False
     j.attributes = (j.attributes or {}) | {'tool': 'hailctl dataproc'}
     get_batch.run(wait=False)
