8dea920155254ee3731e9b3547c4d9f9ff647061
hopedisastro <40783500+hopedisastro@users.noreply.github.com>
2024-04-08 10:57:56 +1000

    Update qc_plotter.py

--- str/qc/qc_plotter.py
+++ str/qc/qc_plotter.py
@@ -32,8 +32,8 @@ def main(mt_path):
     #potato = mt.filter_entries((mt.allele_1_minus_mode> -21) & (mt.allele_1_minus_mode<21) & (mt.allele_2_minus_mode>-21) & (mt.allele_2_minus_mode<21))
     #print(f' MT cap [-20,20] rel. to mode: {potato.entries().count()}')
 
-    chr = 'chr14'
-    position = 42532368
+    chr = 'chr16'
+    position = 34086134
 
     mt = mt.filter_rows((mt.locus.contig == chr) & (mt.locus.position == position))
 
