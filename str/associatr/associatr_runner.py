08def7dd1b27610480ffa002363a8b61bb67994c
hopedisastro <40783500+hopedisastro@users.noreply.github.com>
2024-04-09 11:04:24 +1000

    genome wide run

--- str/associatr/associatr_runner.py
+++ str/associatr/associatr_runner.py
@@ -11,7 +11,7 @@ Ensure prior scripts have been run to generate dependent files, particularly:
  analysis-runner --dataset "bioheart" --config associatr_runner.toml \
     --description "run associatr" \
     --access-level "test" \
-    --output-dir "str/associatr/rna_pc_calibration/2_pcs" \
+    --output-dir "str/associatr/240_libraries_tenk10kp1_v2_run/inflation-debug/cohort_2" \
      python3 associatr_runner.py
 
 
@@ -52,53 +52,50 @@ def main():
         _dependent_jobs.append(job)
 
     for celltype in get_config()['associatr']['celltypes'].split(','):
-        for chromosome in get_config()['associatr']['chromosomes'].split(','):
-            input_dir = get_config()['associatr']['vcf_file_dir']
-            vcf_file_path = f'{input_dir}/hail_filtered_{chromosome}.vcf.bgz'
-            variant_vcf = b.read_input_group(
-                **dict(
-                    base=vcf_file_path,
-                    tbi=vcf_file_path + '.tbi',
-                )
+        vcf_file_path = f'gs://cpg-bioheart-test/str/associatr/input_files/vcf/v1/hail_filtered.vcf.bgz'
+        variant_vcf = b.read_input_group(
+            **dict(
+                base=vcf_file_path,
+                tbi=vcf_file_path + '.tbi',
             )
-            gene_list_dir = get_config()['associatr']['gene_list_dir']
-            with to_path(
-                f'{gene_list_dir}/{celltype}/{chromosome}_{celltype}_gene_list.json'
-            ).open('r') as file:
-                pseudobulk_gene_names = json.load(file)
-            for gene in pseudobulk_gene_names:
-                cis_window_dir = get_config()['associatr']['cis_window_dir']
-                cis_window_size = get_config()['associatr']['cis_window_size']
-                gene_cis_window_file = f'{cis_window_dir}/{celltype}/{chromosome}/{gene}_{cis_window_size}bp.bed'
-                # need to extract the gene start and end from the cis window file for input into 'region'
-                cis_window_region = gene_cis_window_file_reader(gene_cis_window_file)
-                pheno_cov_numpy_dir = get_config()['associatr']['pheno_cov_numpy_dir']
-                gene_pheno_cov = b.read_input(
-                    f'{pheno_cov_numpy_dir}/{celltype}/{chromosome}/{gene}_pheno_cov.npy'
-                )
+        )
 
-                # run associaTR job on the gene
-                associatr_job = b.new_job(
-                    name=f'Run associatr on {gene} [{celltype};{chromosome}]'
-                )
-                associatr_job.image(get_config()['images']['trtools'])
-                associatr_job.storage(get_config()['associatr']['job_storage'])
-                associatr_job.cpu(get_config()['associatr']['job_cpu'])
-                associatr_job.declare_resource_group(
-                    association_results={'tsv': '{root}.tsv'}
-                )
-                associatr_job.command(
-                    f" associaTR {associatr_job.association_results['tsv']} {variant_vcf.base} {celltype}_{chromosome}_{gene} {gene_pheno_cov} --region={cis_window_region} --vcftype=eh"
-                )
-                version = get_config()['associatr']['version']
-                b.write_output(
-                    associatr_job.association_results,
-                    output_path(
-                        f'results/{version}/{celltype}/{chromosome}/{gene}_{cis_window_size}bp',
-                        'analysis',
-                    ),
-                )
-                manage_concurrency_for_job(associatr_job)
+        for gene in ['ENSG00000269893','ENSG00000111666','ENSG00000164308','ENSG00000135968']:
+            if gene == 'ENSG00000269893':
+                chromosome = 'chr4'
+            elif gene == 'ENSG00000111666':
+                chromosome = 'chr12'
+            elif gene == 'ENSG00000164308':
+                chromosome = 'chr5'
+            else:
+                chromosome = 'chr2'
+            pheno_cov_numpy_dir = get_config()['associatr']['pheno_cov_numpy_dir']
+            gene_pheno_cov = b.read_input(
+                f'{pheno_cov_numpy_dir}/{celltype}/{chromosome}/{gene}_pheno_cov.npy'
+            )
+
+            # run associaTR job on the gene
+            associatr_job = b.new_job(
+                name=f'Run associatr on {gene} [{celltype};{chromosome}]'
+            )
+            associatr_job.image(get_config()['images']['trtools'])
+            associatr_job.storage(get_config()['associatr']['job_storage'])
+            associatr_job.cpu(get_config()['associatr']['job_cpu'])
+            associatr_job.declare_resource_group(
+                association_results={'tsv': '{root}.tsv'}
+            )
+            associatr_job.command(
+                f" associaTR {associatr_job.association_results['tsv']} {variant_vcf.base} {celltype}_{chromosome}_{gene} {gene_pheno_cov} --vcftype=eh"
+            )
+            version = get_config()['associatr']['version']
+            b.write_output(
+                associatr_job.association_results,
+                output_path(
+                    f'results/{version}/{celltype}/{chromosome}/{gene}_genome_wide_bp',
+                    'analysis',
+                ),
+            )
+            manage_concurrency_for_job(associatr_job)
     b.run(wait=False)
 
 
