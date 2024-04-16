60498b2086104a76aba0f10c697281c61234f608
Hope Tanudisastro <40783500+hopedisastro@users.noreply.github.com>
2024-04-17 09:51:52 +1000

    Copy cohort specific MTs into test bucket
    New strategy to analyse the cohorts separately due to complex batch effects

--- str/helper/cp_mt.sh
+++ str/helper/cp_mt.sh
@@ -4,4 +4,9 @@
 
 set -ex
 
-gsutil -m cp -r gs://cpg-bioheart-main/str/polymorphic_run_n2045/mt/v1/str.mt gs://cpg-bioheart-test/str/polymorphic_run_n2045/mt/v1/str.mt
+#gsutil -m cp -r gs://cpg-bioheart-main/str/polymorphic_run_n2045/mt/v1/str.mt gs://cpg-bioheart-test/str/polymorphic_run_n2045/mt/v1/str.mt
+
+# copy cohort specific mts (changing approach to analyse each cohort separately because of distinct batch effects) 
+
+gsutil -m cp -r gs://cpg-bioheart-main/str/polymorphic_run_n990_bioheart_only/mt/v1/str.mt gs://cpg-bioheart-test/str/polymorphic_run_n990_bioheart_only/mt/v1/str.mt
+gsutil -m cp -r gs://cpg-bioheart-main/str/polymorphic_run_n1055_tob_only/mt/v1/str.mt gs://cpg-bioheart-test/str/polymorphic_run_n1055_tob_only/mt/v1/str.mt
