d609a54c3c3c64c3f1ea2f9889f9b53354a8146e
hopedisastro <40783500+hopedisastro@users.noreply.github.com>
2024-04-20 11:40:15 +1000

    remove cohort covariate

--- str/associatr/get_cis_numpy_files.py
+++ str/associatr/get_cis_numpy_files.py
@@ -109,14 +109,6 @@ def cis_window_numpy_extractor(
         # filter for samples that were assigned a CPG ID; unassigned samples after demultiplexing will not have a CPG ID
         gene_pheno_cov = gene_pheno_cov[gene_pheno_cov['sample_id'].str.startswith('CPG')]
 
-        # add in cohort as a covariate
-        cohort_cpg_id = adata.obs[['cpg_id', 'cohort']]
-        mapping = {'TOB': 1, 'BioHEART': 2}
-        cohort_cpg_id['cohort'] = cohort_cpg_id['cohort'].map(mapping)
-        cohort_cpg_id.rename(columns={'cpg_id': 'sample_id'}, inplace=True)  # noqa: PD002
-        cohort_cpg_id = cohort_cpg_id.drop_duplicates()
-
-        gene_pheno_cov = gene_pheno_cov.merge(cohort_cpg_id, on='sample_id', how='inner')
 
         gene_pheno_cov['sample_id'] = gene_pheno_cov['sample_id'].str[
             3:
