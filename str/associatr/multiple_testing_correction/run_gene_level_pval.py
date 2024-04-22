9de570386fd753f904f78e356967641aed681ab2
MattWellie <mattwellie@gmail.com>
2024-04-22 16:38:12 +1000

    rearrangement

--- str/associatr/multiple_testing_correction/run_gene_level_pval.py
+++ str/associatr/multiple_testing_correction/run_gene_level_pval.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# pylint: disable=no-value-for-parameter,too-many-arguments,too-many-locals,too-many-nested-blocks
+# pylint: disable=no-value-for-parameter
 """
 
 This script computes gene-level p-values using ACAT (the first step of multiple testing correction).
@@ -15,7 +15,6 @@ analysis-runner --dataset "bioheart" --description "compute gene level pvals" --
 """
 import click
 import numpy as np
-import pandas as pd
 
 import hailtop.batch as hb
 
@@ -37,14 +36,20 @@ VALUES_TO_INDEXES = [
 ]
 
 
-def cct(
-    gene_name,
-    pvals,
-    cell_type,
-    chromosome,
-    row_dict,
-    weights=None,
-):
+def cct(gene_files: list[str], cell_type, chromosome, weights=None):
+    """
+    takes a list of gene files to process in this job
+    moves processing out of the driver job
+
+    Args:
+        gene_files ():
+        cell_type ():
+        chromosome ():
+        weights ():
+
+    Returns:
+
+    """
     """
     Code adapted from the STAR package https://github.com/xihaoli/STAAR/blob/dc4f7e509f4fa2fb8594de48662bbd06a163108c/R/CCT.R wtih a modifitcaiton: when indiviudal p-value = 1, use minimum p-value
     #' An analytical p-value combination method using the Cauchy distribution
@@ -66,90 +71,131 @@ def cct(
     R code is implemented in python
     """
     # Import here as a PythonJob's function must stand alone
+    import pandas as pd
     from scipy.stats import cauchy  # noqa: PLC0415
 
     from cpg_utils import to_path
     from cpg_utils.hail_batch import output_path
 
-    # remove NA values - associaTR reports pval as NA if locus was thrown out (not tested)
-    pvals = pvals[~np.isnan(pvals)]
+    for gene_file in gene_files:
+        print(f'processing {gene_file}')
 
-    # check if all p-values are between 0 and 1
-    if ((pvals < 0).sum() + (pvals > 1).sum()) > 0:
-        raise ValueError('All p-values must be between 0 and 1!')
+        # read the raw results
+        gene_results = pd.read_csv(gene_file, sep='\t')
+        pvals = gene_results.iloc[:, 5]  # stored in the 6th column
+        # Find and store the attributes of the locus with lowest raw pval
+        # Find the minimum value in column 6
+        min_value = gene_results.iloc[:, 5].min()
+        # Find the rows with the minimum value in column 6
+        min_rows = gene_results[gene_results.iloc[:, 5] == min_value]
 
-    # check if there are p-values that are either exactly 0 or 1.
-    is_zero = (pvals == 0).sum() >= 1
-    is_one = (pvals == 1).sum() >= 1
+        # create a dictionary of {key: list}
+        row_dict: dict[str, list] = {key: [] for key, value in VALUES_TO_INDEXES}
 
-    # check the validity of weights (default: equal weights) and standardize them.
-    if weights is None:
-        weights = np.repeat(1 / len(pvals), len(pvals))
-    elif len(weights) != len(pvals):
-        raise ValueError('The length of weights should be the same as that of the p-values!')
-    elif (weights < 0).sum() > 0:
-        raise ValueError('All the weights must be positive!')
-    else:
-        weights = weights / np.sum(weights)
+        # populate the dict
+        for _index, row in min_rows.iterrows():
+            for key, value in VALUES_TO_INDEXES:
+                row_dict[key].append(row.iloc[value])
 
-    # check if there are very small non-zero p-values
-    is_small = pvals < 1e-16
-    if is_small.sum() == 0:
-        cct_stat = np.sum(weights * np.tan((0.5 - pvals) * np.pi))
-    else:
-        cct_stat = np.sum((weights[is_small] / pvals[is_small]) / np.pi)
-        cct_stat += np.sum(weights[~is_small] * np.tan((0.5 - pvals[~is_small]) * np.pi))
+        pvals = np.array(pvals)
+        gene_name = gene_results.columns[5].split('_')[-1]
 
-    if is_zero:
-        pval = 0
-    elif is_one:
-        print('There are p-values exactly equal to 1!')
-        pval = min(1, min(pvals) * len(pvals))
+        # remove NA values - associaTR reports pval as NA if locus was thrown out (not tested)
+        pvals = pvals[~np.isnan(pvals)]
 
-    # check if the test statistic is very large.
-    elif cct_stat > 1e15:
-        pval = (1 / cct_stat) / np.pi
-    else:
-        pval = 1 - cauchy.cdf(cct_stat)
+        # check if all p-values are between 0 and 1
+        if ((pvals < 0).sum() + (pvals > 1).sum()) > 0:
+            raise ValueError('All p-values must be between 0 and 1!')
 
-    # write to output
-    gcs_output = output_path(
-        f'gene_level_pvals/acat/{cell_type}/chr{chromosome}/{gene_name}_gene_level_pval.tsv',
-        'analysis',
-    )
-    with to_path(gcs_output).open('w') as f:
-        f.write(
-            'gene_name\tgene_level_pval\tchr\tpos\tn_samples_tested\tlowest_raw_pval\tcoeff\tse\tr2\tmotif\tref_len\tallele_freq\n',
+        # check if there are p-values that are either exactly 0 or 1.
+        is_zero = (pvals == 0).sum() >= 1
+        is_one = (pvals == 1).sum() >= 1
+
+        # check the validity of weights (default: equal weights) and standardize them.
+        if weights is None:
+            weights = np.repeat(1 / len(pvals), len(pvals))
+        elif len(weights) != len(pvals):
+            raise ValueError('The length of weights should be the same as that of the p-values!')
+        elif (weights < 0).sum() > 0:
+            raise ValueError('All the weights must be positive!')
+        else:
+            weights = weights / np.sum(weights)
+
+        # check if there are very small non-zero p-values
+        is_small = pvals < 1e-16
+        if is_small.sum() == 0:
+            cct_stat = np.sum(weights * np.tan((0.5 - pvals) * np.pi))
+        else:
+            cct_stat = np.sum((weights[is_small] / pvals[is_small]) / np.pi)
+            cct_stat += np.sum(weights[~is_small] * np.tan((0.5 - pvals[~is_small]) * np.pi))
+
+        if is_zero:
+            pval = 0
+        elif is_one:
+            print('There are p-values exactly equal to 1!')
+            pval = min(1, min(pvals) * len(pvals))
+
+        # check if the test statistic is very large.
+        elif cct_stat > 1e15:
+            pval = (1 / cct_stat) / np.pi
+        else:
+            pval = 1 - cauchy.cdf(cct_stat)
+
+        # write to output
+        gcs_output = output_path(
+            f'gene_level_pvals/acat/{cell_type}/chr{chromosome}/{gene_name}_gene_level_pval.tsv',
+            'analysis',
         )
-        f.write(f'{gene_name}\t{pval}\t')
-        f.write('\t'.join([str(row_dict[key]) for key, _value in VALUES_TO_INDEXES]) + '\n')
+        with to_path(gcs_output).open('w') as f:
+            f.write(
+                'gene_name\tgene_level_pval\tchr\tpos\tn_samples_tested\tlowest_raw_pval\tcoeff\tse\tr2\tmotif\tref_len\tallele_freq\n',
+            )
+            f.write(f'{gene_name}\t{pval}\t')
+            f.write('\t'.join([str(row_dict[key]) for key, _value in VALUES_TO_INDEXES]) + '\n')
 
 
-def bonferroni_compute(
-    gene_name,
-    pvals,
-    cell_type,
-    chromosome,
-    row_dict,
-):
+def bonferroni_compute(gene_files, cell_type, chromosome):
     """
     Computes Bonferroni adjusted p-value of the lowest raw p-value for a gene
     """
+    import pandas as pd
+
     from cpg_utils import to_path
     from cpg_utils.hail_batch import output_path
 
-    pval = min(pvals) * len(pvals)
-    # write to output
-    gcs_output = output_path(
-        f'gene_level_pvals/bonferroni/{cell_type}/chr{chromosome}/{gene_name}_gene_level_pval.tsv',
-        'analysis',
-    )
-    with to_path(gcs_output).open('w') as f:
-        f.write(
-            'gene_name\tgene_level_pval\tchr\tpos\tn_samples_tested\tlowest_raw_pval\tcoeff\tse\tr2\tmotif\tref_len\tallele_freq\n',
+    for gene_file in gene_files:
+        # read the raw results
+        gene_results = pd.read_csv(gene_file, sep='\t')
+        pvals = gene_results.iloc[:, 5]  # stored in the 6th column
+        # Find and store the attributes of the locus with lowest raw pval
+        # Find the minimum value in column 6
+        min_value = gene_results.iloc[:, 5].min()
+        # Find the rows with the minimum value in column 6
+        min_rows = gene_results[gene_results.iloc[:, 5] == min_value]
+
+        # create a dictionary of {key: list}
+        row_dict = {key: [] for key, value in VALUES_TO_INDEXES}
+
+        # populate the dict
+        for _index, row in min_rows.iterrows():
+            for key, value in VALUES_TO_INDEXES:
+                row_dict[key].append(row.iloc[value])
+
+        pvals = np.array(pvals)
+        gene_name = gene_results.columns[5].split('_')[-1]
+
+        pval = min(pvals) * len(pvals)
+        # write to output
+        gcs_output = output_path(
+            f'gene_level_pvals/bonferroni/{cell_type}/chr{chromosome}/{gene_name}_gene_level_pval.tsv',
+            'analysis',
         )
-        f.write(f'{gene_name}\t{pval}\t')
-        f.write('\t'.join([str(row_dict[key]) for key, _value in VALUES_TO_INDEXES]) + '\n')
+        with to_path(gcs_output).open('w') as f:
+            f.write(
+                'gene_name\tgene_level_pval\tchr\tpos\tn_samples_tested\tlowest_raw_pval\tcoeff\tse\tr2\tmotif\tref_len\tallele_freq\n',
+            )
+            f.write(f'{gene_name}\t{pval}\t')
+            f.write('\t'.join([str(row_dict[key]) for key, _value in VALUES_TO_INDEXES]) + '\n')
 
 
 @click.option('--input-dir', help='GCS path to the raw results of associaTR')
@@ -193,46 +239,29 @@ def main(input_dir, cell_types, chromosomes, max_parallel_jobs, acat, bonferroni
         for chromosome in chromosomes.split(','):
 
             gene_files = list(to_path(f'{input_dir}/{cell_type}/chr{chromosome}').glob('*.tsv'))
+
+            # split the list of files into chunks
             for i in range(0, len(gene_files), genes_per_job):
                 batch_gene_files = gene_files[i : i + genes_per_job]
-                j = b.new_python_job(
-                    name=f'Compute gene-level p-values for genes {i+1}-{i+genes_per_job}',
-                )
 
-                j.cpu(0.25).memory('lowmem')
-                f = b.new_python_job(
-                    name=f'Compute gene-level Bonferroni p-values for genes {i+1}-{i+genes_per_job}',
-                )
+                if acat:
+                    j = b.new_python_job(
+                        name=f'Compute gene-level p-values for genes {i+1}-{i+genes_per_job}',
+                    )
+                    j.cpu(0.25).memory('lowmem')
+                    j.call(cct, batch_gene_files, cell_type, chromosome)
+                    manage_concurrency_for_job(j)
 
-                f.cpu(0.25).memory('lowmem')
-                for gene_file in batch_gene_files:
-                    # read the raw results
-                    gene_results = pd.read_csv(gene_file, sep='\t')
-                    pvals = gene_results.iloc[:, 5]  # stored in the 6th column
-                    # Find and store the attributes of the locus with lowest raw pval
-                    # Find the minimum value in column 6
-                    min_value = gene_results.iloc[:, 5].min()
-                    # Find the rows with the minimum value in column 6
-                    min_rows = gene_results[gene_results.iloc[:, 5] == min_value]
-
-                    # create a dictionary of {key: list}
-                    row_dict = {key: [] for key, value in VALUES_TO_INDEXES}
-
-                    # populate the dict
-                    for _index, row in min_rows.iterrows():
-                        for key, value in VALUES_TO_INDEXES:
-                            row_dict[key].append(row.iloc[value])
-
-                    pvals = np.array(pvals)
-                    gene_name = gene_results.columns[5].split('_')[-1]
-                    if acat:
-                        j.call(cct, gene_name, pvals, cell_type, chromosome, row_dict)
-                    if bonferroni:
-                        f.call(bonferroni_compute, gene_name, pvals, cell_type, chromosome, row_dict)
-                manage_concurrency_for_job(j)
+                if bonferroni:
+                    f.call(bonferroni_compute, batch_gene_files, cell_type, chromosome)
+                    f = b.new_python_job(
+                        name=f'Compute gene-level Bonferroni p-values for genes {i+1}-{i+genes_per_job}',
+                    )
+                    f.cpu(0.25).memory('lowmem')
+                    manage_concurrency_for_job(f)
 
         b.run(wait=False)
 
 
 if __name__ == '__main__':
-    main()  # pylint: disable=no-value-for-parameter,too-many-arguments,too-many-locals,too-many-nested-blocks
+    main()  # pylint: disable=no-value-for-parameter
