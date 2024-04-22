da9d9b8c9fe4e6fceabcdcfb879e6611268657a9
hopedisastro <40783500+hopedisastro@users.noreply.github.com>
2024-04-22 10:12:58 +1000

    remove image path (not needed)

--- str/associatr/multiple_testing_correction/run_gene_level_pval.py
+++ str/associatr/multiple_testing_correction/run_gene_level_pval.py
@@ -10,7 +10,7 @@ The attributes of the locus with the lowest raw p-value are also stored in the T
 analysis-runner --dataset "bioheart" --description "compute gene level pvals" --access-level "test" \
     --output-dir "str/associatr/tob_n1055/results" \
     run_gene_level_pval.py --input-dir=gs://cpg-bioheart-test/str/associatr/tob_n1055/results/v1 \
-    --cell-types=CD4_TCM --chromosomes=1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21 --acat
+    --cell-types=CD4_TCM --chromosomes=1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22 --acat
 """
 import click
 import numpy as np
@@ -200,14 +200,10 @@ def main(input_dir, cell_types, chromosomes, max_parallel_jobs, acat, bonferroni
                 j = b.new_python_job(
                     name=f'Compute gene-level p-values for genes {i+1}-{i+genes_per_job}',
                 )
-                j.image(image_path('scanpy'))
-
                 j.cpu(0.25).memory('lowmem')
                 f = b.new_python_job(
                     name=f'Compute gene-level Bonferroni p-values for genes {i+1}-{i+genes_per_job}',
                 )
-                f.image(image_path('scanpy'))
-
                 f.cpu(0.25).memory('lowmem')
                 for gene_file in batch_gene_files:
                     # read the raw results
