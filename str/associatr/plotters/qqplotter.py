e52dcbe78662e84d46d62c191134867d117994da
hopedisastro <40783500+hopedisastro@users.noreply.github.com>
2024-04-23 14:13:15 +1000

    Update qqplotter.py

--- str/associatr/plotters/qqplotter.py
+++ str/associatr/plotters/qqplotter.py
@@ -39,8 +39,8 @@ def main(input_dir, cell_types):
         )
 
     # Create QQ plot
-    plt.figure(figsize=(10, 6))
-    fig, ax = plt.subplots(figsize=(10, 6))
+    plt.figure(figsize=(10, 8))
+    fig, ax = plt.subplots(figsize=(10, 8))
 
     # Define a list of colors
     colors = [
