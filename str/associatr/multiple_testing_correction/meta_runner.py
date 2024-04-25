2e6d9fdce809e8958b71d20b61bf140c568d37d7
hopedisastro <40783500+hopedisastro@users.noreply.github.com>
2024-04-25 11:31:31 +1000

    first attempt

--- str/associatr/multiple_testing_correction/meta_runner.py
+++ str/associatr/multiple_testing_correction/meta_runner.py
@@ -3,73 +3,146 @@
 """
 This script runs R's meta package to generate pooled effect sizes for each eQTL.
 
-analysis-runner --dataset "bioheart" --description "compute qvals" --access-level "test" \
-    --output-dir "str/associatr/rna_pc_calibration/5_pcs/results" \
-    --image australia-southeast1-docker.pkg.dev/cpg-common/images-dev/r-qvalue:1.0 \
-    run_storey.py --input-dir=gs://cpg-bioheart-test/str/associatr/rna_pc_calibration/5_pcs/results/gene_level_pvals \
-    --cell-types=CD8_TEM --chromosomes=1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22 \
-    --gene-level-correction=bonferroni
+analysis-runner --dataset "bioheart" --description "meta results runner" --access-level "test" \
+    --output-dir "str/associatr/tob_n1055_and_bioheart_n990" \
+    --image australia-southeast1-docker.pkg.dev/cpg-common/images-dev/r-meta \
+    meta_runner.py --results-dir-1=gs://cpg-bioheart-test/str/associatr/tob_n1055/results/v1 \
+    --results-dir-2=gs://cpg-bioheart-test/str/associatr/bioheart_n990/results/v1 \
+    --gene-list-dir-1=gs://cpg-bioheart-test/str/associatr/tob_n1055/input_files/scRNA_gene_lists/1_min_pct_cells_expressed \
+    --gene-list-dir-2=gs://cpg-bioheart-test/str/associatr/bioheart_n990/input_files/scRNA_gene_lists/1_min_pct_cells_expressed \
+    --cell-types=CD8_TEM --chromosomes=1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22
 """
-
-import pandas as pd
+import json
 
 import click
-from cpg_utils.hail_batch import get_batch, output_path
-from cpg_utils import to_path
+import pandas as pd
 import rpy2.robjects as ro
+from rpy2.robjects import pandas2ri
+
+from cpg_utils import to_path
+from cpg_utils.hail_batch import get_batch, output_path
 
 
-def compute_storey(input_dir, cell_type, chromosomes, gene_level_correction):
+def run_meta_gen(input_dir_1, input_dir_2, cell_type, chr, gene):
     """
-    Compute pooled effect sizes
+    Run meta-analysis using R's meta package
     """
     ro.r('library(meta)')
-    cohort_1_path = to_path(f'{input_dir}/{gene_level_correction}/{cell_type}/chr1').copy('here1.tsv')
-    cohort_2_path = to_path(f'{input_dir}/{gene_level_correction}/{cell_type}/chr2').copy('here2.tsv')
+    ro.r('library(tidyverse)')
+    d1 = pd.read_csv(f'{input_dir_1}/{cell_type}/{chr}/{gene}_100000bp.tsv')
+    d2 = pd.read_csv(f'{input_dir_2}/{cell_type}/{chr}/{gene}_100000bp.tsv')
 
+    # remove loci that failed to be tested in either dataset
+    d1 = d1[d1['locus_filtered'] == 'False']
+    d2 = d2[d2['locus_filtered'] == 'False']
 
-    pvals = pval_df['gene_level_pval']
-    if gene_level_correction == 'bonferroni':
-        pvals = list(pvals)
-        # set p-values > 1 to 1
-        pvals = [1 if num > 1 else num for num in pvals]
-    ro.globalenv['pvals'] = ro.FloatVector(list(pvals))
-    pval_df['qval'] = ro.r('qvalue(pvals)$qvalues')
-    pi_o = ro.r('qvalue(pvals)$pi0')
-    print(f'pi0: {pi_o}')
+    # convert to R dataframe
+    with (ro.default_converter + pandas2ri.converter).context():
+        r_from_pd_df1 = ro.conversion.get_conversion().py2rpy(d1)
 
-    # write to output
-    gcs_output = output_path(
-        f'fdr_qvals/using_{gene_level_correction}/{cell_type}_qval.tsv',
-        'analysis',
+    with (ro.default_converter + pandas2ri.converter).context():
+        r_from_pd_df2 = ro.conversion.get_conversion().py2rpy(d2)
+
+    # initialise R variable in the R environment
+    ro.globalenv['r_df1'] = r_from_pd_df1
+    ro.globalenv['r_df2'] = r_from_pd_df2
+
+    # clean up dfs and merge
+    ro.r(
+        f'r_df1 = r_df1 %>% select(chrom, pos, coeff_CD4_TCM_chr1_{gene},se_CD4_TCM_chr1_{gene}, motif, period, ref_len)',
     )
-    # arrange by ascending q-value
-    pval_df = pval_df.sort_values(by='qval', ascending=True)
-    pval_df.to_csv(gcs_output, sep='\t', index=False, header=True)
+    ro.r(
+        f'r_df2 = r_df2 %>% select(chrom, pos, coeff_CD4_TCM_chr1_{gene},se_CD4_TCM_chr1_{gene}, motif, period, ref_len)',
+    )
+    ro.r(f'r_df1 = r_df1 %>% rename(coeff_1 =coeff_CD4_TCM_chr1_{gene}, se_1 =se_CD4_TCM_chr1_{gene}  )')
+    ro.r(f'r_df2 = r_df2 %>% rename(coeff_2 =coeff_CD4_TCM_chr1_{gene}, se_2 =se_CD4_TCM_chr1_{gene}  )')
+    ro.r('df <- merge(r_df1, r_df2, by = c("chrom", "pos", "motif", "period", "ref_len"))')
+
+    # initialise empty df to store meta results
+    ro.r(
+        '''
+    meta_df <- data.frame(
+    chr = character(),
+    pos = numeric(),
+    coeff_meta = numeric(),
+    pval_meta = numeric(),
+    lowerCI_meta = numeric(),
+    upperCI_meta = numeric()
+    )
+    ''',
+    )
+
+    # run meta
+
+    ro.r(
+        '''
+    for (i in 1:nrow(df)) {
+    # Extract values for cohort 1
+    row_cohort1 <- data.frame(
+        cohort = 1,
+        se = df[i, "se_1"],
+        coeff = df[i, "coeff_1"]
+    )
+    # Extract values for cohort 2
+    row_cohort2 <- data.frame(
+        cohort = 2,
+        se = df[i, "se_2"],
+        coeff = df[i, "coeff_2"]
+    )
+    result_df <- rbind(row_cohort1, row_cohort2)
+    m.gen = metagen(result_df$coeff, result_df$se, method.tau = "REML", random = TRUE,method.random.ci = "HK")
 
 
-@click.option(
-    '--input-dir', help='GCS path directory to the input gene-level p-value files'
-)
-@click.option(
-    '--gene-level-correction',
-    type=click.Choice(['acat', 'bonferroni']),
-    help='Choose between "acat" or "bonferroni"',
-)
+    new_entry = data.frame(
+        chr = df[i, "chrom"],
+        pos = df[i, "pos"],
+        coeff_meta = m.gen$TE.random,
+        se_meta = m.gen$seTE.random,
+        pval_meta = m.gen$pval.random,
+        lowerCI_meta = m.gen$lower.random,
+        upperCI_meta = m.gen$upper.random
+    )
+
+    meta_df = rbind(meta_df, new_entry)
+    }
+    ''',
+    )
+
+    # convert to pandas df
+    with (ro.default_converter + pandas2ri.converter).context():
+        pd_meta_df = ro.conversion.get_conversion().rpy2py(ro.r('meta_df'))
+
+    pd_meta_df.to_csv(
+        f'{output_path(f"meta_results/{cell_type}/{chr}/{gene}_100000bp_meta_results.tsv")}', sep='\t', index=False,
+    )
+
+
+@click.option('--results-dir-1', help='GCS path directory to the raw associatr results for cohort 1')
+@click.option('--results-dir_-1', help='GCS path directory to the raw associatr results for cohort 2')
+@click.option('--gene-list-dir-1', help='GCS path directory to the gene list for cohort 1')
+@click.option('--gene-list-dir-2', help='GCS path directory to the gene list for cohort 2')
 @click.option('--cell-types', help='cell type')
 @click.option('--chromosomes', help='chromosomes')
 @click.command()
-def main(input_dir, cell_types, chromosomes, gene_level_correction):
+def main(input_dir_1, input_dir_2, gene_list_dir_1, gene_list_dir_2, cell_types, chromosomes):
     """
-    Compute Storey's q-values for gene-level p-values
+    Compute meta-analysis using R's meta package
     """
     for cell_type in cell_types.split(','):
-        j = get_batch(f'compute_storey {gene_level_correction}').new_python_job(
-            name=f'compute_storey_{cell_type}'
-        )
-        j.cpu(1)
-        j.call(compute_storey, input_dir, cell_type, chromosomes, gene_level_correction)
-    get_batch('compute_storey').run(wait=False)
+        for chromosome in chromosomes.split(','):
+            # get the intersection of genes tested in both cohorts
+            gene_file_path_1 = to_path(f'{gene_list_dir_1}/{cell_type}/{chromosome}_{cell_type}_gene_list.json')
+            gene_file_path_2 = to_path(f'{gene_list_dir_2}/{cell_type}/{chromosome}_{cell_type}_gene_list.json')
+            with open(gene_file_path_1) as f:
+                genes_1 = json.load(f)
+            with open(gene_file_path_2) as g:
+                genes_2 = json.load(g)
+            for gene in list(set(genes_1) & set(genes_2)):
+                j = get_batch('compute_meta').new_python_job(name=f'compute_meta_{cell_type}_{chromosome}_{gene}')
+                j.cpu(1)
+                j.call(run_meta_gen, input_dir_1, input_dir_2, cell_type, chromosome, gene)
+
+    get_batch().run(wait=False)
 
 
 if __name__ == '__main__':
