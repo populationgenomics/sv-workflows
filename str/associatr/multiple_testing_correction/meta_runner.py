f30977c02fb23c50596dea6ea8243ee834ff0fc6
hopedisastro <40783500+hopedisastro@users.noreply.github.com>
2024-04-25 11:48:38 +1000

    reposition get_batch()?

--- str/associatr/multiple_testing_correction/meta_runner.py
+++ str/associatr/multiple_testing_correction/meta_runner.py
@@ -140,8 +140,8 @@ def main(results_dir_1, results_dir_2, gene_list_dir_1, gene_list_dir_2, cell_ty
                 genes_1 = json.load(f)
             with open(gene_file_path_2) as g:
                 genes_2 = json.load(g)
+            j = get_batch().new_python_job(name=f'compute_meta_{cell_type}_{chromosome}')
             for gene in list(set(genes_1) & set(genes_2)):
-                j = get_batch().new_python_job(name=f'compute_meta_{cell_type}_{chromosome}_{gene}')
                 j.cpu(1)
                 j.call(run_meta_gen, results_dir_1, results_dir_2,cell_type, chromosome, gene)
 
