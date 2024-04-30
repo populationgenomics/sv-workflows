6c80c5817dabc3ee8a29015f201cb28b1ee50ae6
hopedisastro <40783500+hopedisastro@users.noreply.github.com>
2024-04-30 14:55:49 +1000

    lint

--- str/saige/bgzipper_and_indexer.py
+++ str/saige/bgzipper_and_indexer.py
@@ -12,17 +12,21 @@ Input MT should be the output of qc_filters_saige.py
     bgzipper_and_indexer.py --vcf-dir=gs://cpg-bioheart-test/str/saige-qtl/input_files/vcf/v1-chr-specific
 
 """
-import click
 import logging
+
+import click
+
 from cpg_utils import to_path
-from cpg_utils.hail_batch import get_batch
 from cpg_utils.config import get_config
+from cpg_utils.hail_batch import get_batch
 
 
 def add_remove_chr_and_index_job(vcf_path):
     """
     Reads a VCF file, it creates an .csi index file and
     and removes "chr" from the chromosome values
+
+    copies and pasted from https://github.com/populationgenomics/saige-tenk10k/blob/main/get_genotype_vcf.py
 
     Args:
     vcf_path: input path
@@ -37,21 +41,19 @@ def add_remove_chr_and_index_job(vcf_path):
         output={
             'vcf.bgz': '{root}.vcf.bgz',
             'vcf.bgz.csi': '{root}.vcf.bgz.csi',
-        }
+        },
     )
     bcftools_job.image(get_config()['images']['bcftools'])
     bcftools_job.cpu(4)
     bcftools_job.storage(f'{storage_required}Gi')
     # now remove "chr" from chromosome names using bcftools
-    bcftools_job.command(
-        'for num in {1..22} ; do echo "chr${num} ${num}" >> chr_update.txt; done'
-    )
+    bcftools_job.command('for num in {1..22} ; do echo "chr${num} ${num}" >> chr_update.txt; done')
     bcftools_job.command(
         f"""
         bcftools annotate --rename-chrs chr_update.txt {vcf_input} | \\
         bgzip -c > {bcftools_job.output['vcf.bgz']}
         bcftools index -c {bcftools_job.output['vcf.bgz']}
-    """
+    """,
     )
     logging.info('CV VCF rename/index jobs scheduled!')
 
@@ -61,17 +63,17 @@ def add_remove_chr_and_index_job(vcf_path):
 
 @click.option('--vcf-dir', help='GCS file path to vcf files', type=str)
 @click.command()
-
 def main(vcf_dir):
     """
     Runner to bgzip and tabix
     """
 
-    for chr_index in [21]:  # iterate over chr1-22
+    for chr_index in range(22):  # iterate over chr1-22
         vcf_path = f'{vcf_dir}/hail_filtered_chr{chr_index+1}.vcf.bgz'
         add_remove_chr_and_index_job(vcf_path)
 
     get_batch().run(wait=False)
+
 
 if __name__ == '__main__':
     main()
