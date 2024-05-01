9bb48c235cf04ce5eb757b15b5d0e6fb09d11e78
Michael Harper <michael.harper@populationgenomics.org.au>
2024-05-02 09:42:49 +1000

    Adding script to compare if subsetting combined VDS (tenk10k vds) to just bioheart samples creates the same as if producing bioheart-only vds

---
+++ str/helper/vds_checker.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+"""
+This Hail Query script prints the dimensions of a VDS
+ analysis-runner --dataset "bioheart" \
+    --description "mt_extractor" \
+    --access-level "test" \
+    --output-dir "str/polymorphic_run/mt/v1" \
+    vds_counter.py --file-path=gs://cpg-bioheart-test/vds/bioheart1-0.vds
+
+"""
+
+import click
+
+import hail as hl
+
+from cpg_utils.hail_batch import init_batch
+
+
+@click.option(
+    '--tenk10k-filepath',
+    help='GCS file path to VDS.',
+)
+@click.option(
+    '--bioheart-filepath',
+    help='GCS file path to VDS.',
+)
+@click.command()
+def main(tenk10k_filepath, bioheart_filepath):
+    """Check if subsetting VDS is the same as producing VDS from scratch using only subsetted samples"""
+
+    init_batch(worker_memory='highmem')
+
+    tenk10k_vds = hl.vds.read_vds(tenk10k_filepath)
+    bioheart_vds = hl.vds.read_vds(bioheart_filepath)
+
+    tenk10k_reference_data_mt = tenk10k_vds.reference_data
+    bioheart_reference_data_mt = bioheart_vds.reference_data
+    print(f' VDS reference data dimensions: {tenk10k_reference_data_mt.count()}')
+    print(f' VDS reference data dimensions: {bioheart_reference_data_mt.count()}')
+
+    tenk10k_variant_data_mt = tenk10k_vds.variant_data
+    bioheart_variant_data_mt = bioheart_vds.variant_data
+    print(f' VDS variant data dimensions: {tenk10k_variant_data_mt.count()}')
+    print(f' VDS variant data dimensions: {bioheart_variant_data_mt.count()}')
+
+    bioheart_only_sgids = bioheart_vds.variant_data.s.collect()
+    # filter to just test subset
+    filtered_tenk10k_vds = hl.vds.filter_samples(tenk10k_vds, bioheart_only_sgids)
+
+    # densify both
+    filtered_tenk10k_dense = hl.vds.to_dense_mt(filtered_tenk10k_vds)
+    bioheart_dense = hl.vds.to_dense_mt(bioheart_vds)
+    print(f' Filtered tenk10K dense_mt data dimensions: {filtered_tenk10k_dense.count()}')
+    print(f' BioHEART dense_mt data dimensions: {bioheart_dense.count()}')
+
+    # compare vds represenations
+    print(filtered_tenk10k_vds.variant_data._same(bioheart_vds.variant_data))
+
+    # compare dense_mt's
+    print(filtered_tenk10k_dense._same(bioheart_dense))
+
+
+if __name__ == '__main__':
+    main()  # pylint: disable=no-value-for-parameter
