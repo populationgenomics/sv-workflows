fba5c3118008fff09e24167d0cb53cb0b0ef0a51
Hope Tanudisastro <40783500+hopedisastro@users.noreply.github.com>
2024-05-15 10:57:10 +1000

    Speed up gwas ld (#203)
    * trying to speed up
    
    * test works; so remvoe test cases
    
    * lint
    
    * write directly to gcp
    
    * move everything into the python job
    
    * debug
    
    * copy cyvcf local file
    
    * debug
    
    * remove test cases, works
    
    * Update gwas_ld_runner.py

--- str/coloc/gwas_ld_runner.py
+++ str/coloc/gwas_ld_runner.py
@@ -13,14 +13,15 @@ Workflow:
 
 analysis-runner --dataset "bioheart" \
     --description "Calculate LD between STR and SNPs" \
-    --access-level "full" \
+    --access-level "test" \
     --cpu=1 \
     --output-dir "str/associatr/freeze_1/gwas_ld/bioheart-only-snps" \
-    gwas_ld_runner.py --snp-vcf-dir=gs://cpg-bioheart-main/saige-qtl/bioheart_n990/input_files/genotypes/vds-bioheart1-0 \
+    gwas_ld_runner.py --snp-vcf-dir=gs://cpg-bioheart-test/str/dummy_snp_vcf \
     --str-vcf-dir=gs://cpg-bioheart-test/str/saige-qtl/input_files/vcf/v1-chr-specific \
     --gwas-file=gs://cpg-bioheart-test/str/gwas_catalog/hg38.EUR.IBD.gwas_info03_filtered.assoc_for_gwas_ld.csv \
     --celltypes=CD4_TCM \
-    --phenotype=ibd
+    --phenotype=ibd \
+    --max-parallel-jobs 250
 
 """
 
@@ -30,74 +31,151 @@ import click
 import pandas as pd
 
 import hailtop.batch as hb
-from hailtop.batch import ResourceGroup
 
-from cpg_utils import to_path
 from cpg_utils.config import output_path
 from cpg_utils.hail_batch import get_batch
 
 
 def ld_parser(
-    snp_vcf_path: ResourceGroup,
-    str_vcf_path: ResourceGroup,
-    str_locus: str,
-    lead_snp_locus: str,
-    gene: str,
-    celltype: str,
-) -> str:
+    snp_vcf_dir,
+    str_vcf_dir,
+    phenotype,
+    celltype,
+    gene_annotation_file,
+    str_fdr_dir,
+    gwas_file,
+    gene,
+):
     import pandas as pd
     from cyvcf2 import VCF
 
-    # create empty DF to store the relevant GTs (SNPs)
-    df = pd.DataFrame(columns=['individual'])
+    from cpg_utils import to_path
 
-    # cyVCF2 reads the SNP VCF
-    vcf = VCF(snp_vcf_path['vcf'])
-    df['individual'] = vcf.samples
-    print('Reading SNP VCF with VCF()')
+    # read in gwas catalog file
+    gwas_catalog_orig = pd.read_csv(gwas_file)
+    # read in gene_annotation_table
+    gene_annotation_table = pd.read_csv(gene_annotation_file)
 
-    print(f'Starting to subset VCF for the lead SNP {lead_snp_locus}')
-    for variant in vcf(lead_snp_locus):
-        geno = variant.gt_types  # extracts GTs as a numpy array
-        locus = variant.CHROM + ':' + str(variant.POS)
-        df_to_append = pd.DataFrame(geno, columns=[locus])  # creates a temp df to store the GTs for one locus
+    # obtain snp cis-window coordinates for the gene
+    gene_table = gene_annotation_table[gene_annotation_table['gene_ids'] == gene]  # subset to particular ENSG ID
+    start_snp_window = float(gene_table['start'].astype(float)) - 100000  # +-100kB window around gene
+    end_snp_window = float(gene_table['end'].astype(float)) + 100000  # +-100kB window around gene
+    chrom = gene_table['chr'].iloc[0][3:]
+    print('Obtained SNP window coordinates')
 
-        # concatenate results to the main df
-        df = pd.concat([df, df_to_append], axis=1)
-        break  # take the first SNP locus
-    print("Finished subsetting VCF for lead SNP")
+    # subset the gwas catalog to the snp_window
+    gwas_catalog = gwas_catalog_orig[gwas_catalog_orig['CHR'] == int(chrom)]
+    gwas_catalog = gwas_catalog[gwas_catalog['BP'] >= start_snp_window]
+    gwas_catalog = gwas_catalog[gwas_catalog['BP'] <= end_snp_window]
 
-    # extract GTs for the one STR
-    str_vcf = VCF(str_vcf_path['vcf'])
-    for variant in str_vcf(str_locus):
-        print(f'Captured STR with POS:{variant.POS}')
-        ds = variant.format('DS')
-        ds_list = []
-        for i in range(len(ds)):
-            ds_list.append(ds[i][0])
-        target_data = {'individual': str_vcf.samples, str_locus: ds_list}
-        target_df = pd.DataFrame(target_data)
-        break  # take the first STR locus
+    if gwas_catalog.empty:
+        print('No SNP GWAS data for ' + gene + ' in the cis-window: skipping....')
+        return
 
-    # merge the two dataframes
-    merged_df = df.merge(target_df, on='individual')
+    # obtain lead SNP (lowest p-value) in the snp_window of the GWAS
+    lowest_p_row = gwas_catalog.loc[gwas_catalog['P'].idxmin()]
+    lead_snp_chr = lowest_p_row['CHR']
+    lead_snp_bp = lowest_p_row['BP']
+    lead_snp_end = lowest_p_row['BP'] + 1
+    lead_snp_locus = f'{lead_snp_chr}:{lead_snp_bp}-{lead_snp_end}'
 
-    # calculate pairwise correlation of every SNP locus with target STR locus
-    correlation_series = merged_df.drop(columns='individual').corrwith(merged_df[str_locus])
+    # load in the str fdr file
+    str_fdr_file = f'{str_fdr_dir}/{celltype}_qval.tsv'
+    str_fdr = pd.read_csv(str_fdr_file, sep='\t')
 
-    correlation_df = pd.DataFrame(correlation_series, columns=['correlation'])
-    correlation_df['locus'] = correlation_df.index
+    snp_vcf_path = f'{snp_vcf_dir}/chr{chrom}_common_variants.vcf.bgz'
+    str_vcf_path = f'{str_vcf_dir}/hail_filtered_chr{chrom}.vcf.bgz'
 
-    # drop the STR locus from the list of SNPs (it will automatically have a correlation of 1)
-    correlation_df = correlation_df[correlation_df['locus'] != str_locus]
+    # copy SNP VCF local because cyVCF2 can only read from a local file
+    local_file = 'local.vcf.bgz'
+    gcp_file = to_path(snp_vcf_path)
+    gcp_file_index = to_path(snp_vcf_path + '.csi')
+    gcp_file.copy(local_file)
+    print('Copied SNP VCF to local file')
+    gcp_file_index.copy(local_file + '.csi')
+    print('Copied SNP VCF index to local file')
 
-    # add some attributes
-    correlation_df['gene'] = gene
-    correlation_df['str_locus'] = str_locus
-    correlation_df['celltype'] = celltype
+    # start by copying STR VCF to local
+    local_str_file = 'local_str.vcf.bgz'
+    gcp_str_file = to_path(str_vcf_path)
+    gcp_str_file_index = to_path(str_vcf_path + '.csi')
+    gcp_str_file.copy(local_str_file)
+    print('Copied STR VCF to local file')
 
-    # return the df as a String
-    return correlation_df.to_csv(index=False)
+    gcp_str_file_index.copy(local_str_file + '.csi')
+    print('Copied STR VCF index to local file')
+
+    # obtain top STR locus for the gene (if multiple are tied - iterate over each)
+    str_fdr_gene = str_fdr[str_fdr['gene_name'] == gene]
+    for estr in zip(
+        ast.literal_eval(str_fdr_gene['chr'].iloc[0]),
+        ast.literal_eval(str_fdr_gene['pos'].iloc[0]),
+    ):
+        chr_num = estr[0][3:]
+        pos = estr[1]
+        end = str(int(pos) + 1)
+        str_locus = f'{chr_num}:{pos}-{end}'
+        write_path = output_path(
+            f'gwas_ld/{phenotype}/{celltype}/{gene}_chr{chr_num}_{pos}_gwas_ld_results.csv',
+            'analysis',
+        )
+
+        if to_path(write_path).exists():
+            print(f'GWAS LD for {gene} and {str_locus} already exists. Skipping...')
+            continue
+
+        print(f'Running LD for {gene} and {str_locus}')
+
+        # create empty DF to store the relevant GTs (SNPs)
+        df = pd.DataFrame(columns=['individual'])
+
+        # cyVCF2 reads the SNP VCF
+        vcf = VCF(local_file)
+        df['individual'] = vcf.samples
+        print('Reading SNP VCF with VCF()')
+
+        print(f'Starting to subset VCF for the lead SNP {lead_snp_locus}')
+        for variant in vcf(lead_snp_locus):
+            geno = variant.gt_types  # extracts GTs as a numpy array
+            locus = variant.CHROM + ':' + str(variant.POS)
+            df_to_append = pd.DataFrame(geno, columns=[locus])  # creates a temp df to store the GTs for one locus
+
+            # concatenate results to the main df
+            df = pd.concat([df, df_to_append], axis=1)
+            break  # take the first SNP locus
+        print("Finished subsetting VCF for lead SNP")
+
+        # extract GTs for the one STR
+        str_vcf = VCF(local_str_file)
+        for variant in str_vcf(str_locus):
+            print(f'Captured STR with POS:{variant.POS}')
+            ds = variant.format('DS')
+            ds_list = []
+            for i in range(len(ds)):
+                ds_list.append(ds[i][0])
+            target_data = {'individual': str_vcf.samples, str_locus: ds_list}
+            target_df = pd.DataFrame(target_data)
+            break  # take the first STR locus
+
+        # merge the two dataframes
+        merged_df = df.merge(target_df, on='individual')
+
+        # calculate pairwise correlation of every SNP locus with target STR locus
+        correlation_series = merged_df.drop(columns='individual').corrwith(merged_df[str_locus])
+
+        correlation_df = pd.DataFrame(correlation_series, columns=['correlation'])
+        correlation_df['locus'] = correlation_df.index
+
+        # drop the STR locus from the list of SNPs (it will automatically have a correlation of 1)
+        correlation_df = correlation_df[correlation_df['locus'] != str_locus]
+
+        # add some attributes
+        correlation_df['gene'] = gene
+        correlation_df['str_locus'] = str_locus
+        correlation_df['celltype'] = celltype
+
+        # write to output_path
+        correlation_df.to_csv(write_path, index=False)
 
 
 @click.option(
@@ -128,7 +206,7 @@ def ld_parser(
 )
 @click.option('--job-cpu', default=1)
 @click.option('--job-storage', default='20G')
-@click.option('--max-parallel-jobs', default=500)
+@click.option('--max-parallel-jobs', default=100)
 @click.command()
 def main(
     snp_vcf_dir: str,
@@ -154,8 +232,6 @@ def main(
         _dependent_jobs.append(job)
 
     b = get_batch()
-    # read in gwas catalog file
-    gwas_catalog = pd.read_csv(gwas_file)
 
     for celltype in celltypes.split(','):
         # read in STR eGene annotation file
@@ -166,74 +242,26 @@ def main(
         # obtain inputs for LD parsing for each entry in `str_fdr`:
         for index, row in str_fdr.iterrows():
             gene = row['gene_name']
-            # obtain snp cis-window coordinates for the gene
-            gene_annotation_table = pd.read_csv(gene_annotation_file)
-            gene_table = gene_annotation_table[
-                gene_annotation_table['gene_ids'] == gene
-            ]  # subset to particular ENSG ID
-            start_snp_window = float(gene_table['start'].astype(float)) - 100000  # +-100kB window around gene
-            end_snp_window = float(gene_table['end'].astype(float)) + 100000  # +-100kB window around gene
-            chr = gene_table['chr'].iloc[0][3:]
-            print('Obtained SNP window coordinates')
 
-            # subset the gwas catalog to the snp_window
-            gwas_catalog = gwas_catalog[gwas_catalog['CHR'] == int(chr)]
-            gwas_catalog = gwas_catalog[gwas_catalog['BP'] >= start_snp_window]
-            gwas_catalog = gwas_catalog[gwas_catalog['BP'] <= end_snp_window]
-            if gwas_catalog.empty:
-                print('No SNP GWAS data for ' + gene + ' in the cis-window: skipping....')
-                continue
+            # run ld
+            ld_job = b.new_python_job(
+                f'LD calc for {gene}; {celltype}',
+            )
+            ld_job.cpu(job_cpu)
+            ld_job.storage(job_storage)
 
-            # obtain lead SNP (lowest p-value) in the snp_window
-            lowest_p_row = gwas_catalog.loc[gwas_catalog['P'].idxmin()]
-            lead_snp_chr = lowest_p_row['CHR']
-            lead_snp_bp = lowest_p_row['BP']
-            lead_snp_end = lowest_p_row['BP'] + 1
-            lead_snp_locus = f'{lead_snp_chr}:{lead_snp_bp}-{lead_snp_end}'
-
-            # obtain top STR locus for the gene
-            str_fdr_gene = str_fdr[str_fdr['gene_name'] == gene]
-            for estr in zip(
-                ast.literal_eval(str_fdr_gene['chr'].iloc[0]),
-                ast.literal_eval(str_fdr_gene['pos'].iloc[0]),
-            ):
-                chr_num = estr[0][3:]
-                pos = estr[1]
-                end = str(int(pos) + 1)
-                str_locus = f'{chr_num}:{pos}-{end}'
-                write_path = output_path(
-                    f'gwas_ld/{phenotype}/{celltype}/{gene}_chr{chr_num}_{pos}_gwas_ld_results.csv',
-                    'analysis',
-                )
-
-                if to_path(write_path).exists():
-                    print(f'GWAS LD for {gene} and {str_locus} already exists. Skipping...')
-                    continue
-
-                print(f'Running LD for {gene} and {str_locus}')
-                snp_vcf_path = f'{snp_vcf_dir}/chr{chr}_common_variants.vcf.bgz'
-                str_vcf_path = f'{str_vcf_dir}/hail_filtered_chr{chr_num}.vcf.bgz'
-                # run ld
-                ld_job = b.new_python_job(
-                    f'LD calc for {gene} and STR: {str_locus}; {celltype}',
-                )
-                ld_job.cpu(job_cpu)
-                ld_job.storage(job_storage)
-                snp_input = get_batch().read_input_group(**{'vcf': snp_vcf_path, 'csi': snp_vcf_path + '.csi'})
-                str_input = get_batch().read_input_group(**{'vcf': str_vcf_path, 'csi': str_vcf_path + '.csi'})
-
-                result = ld_job.call(
-                    ld_parser,
-                    snp_input,
-                    str_input,
-                    str_locus,
-                    lead_snp_locus,
-                    gene,
-                    celltype,
-                )
-
-                b.write_output(result.as_str(), write_path)
-                manage_concurrency_for_job(ld_job)
+            ld_job.call(
+                ld_parser,
+                snp_vcf_dir,
+                str_vcf_dir,
+                phenotype,
+                celltype,
+                gene_annotation_file,
+                str_fdr_dir,
+                gwas_file,
+                gene,
+            )
+            manage_concurrency_for_job(ld_job)
 
     b.run(wait=False)
 
