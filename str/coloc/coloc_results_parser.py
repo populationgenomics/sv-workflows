880f49598a21ff1ad65c016b873c83bc6f99fda2
hopedisastro <40783500+hopedisastro@users.noreply.github.com>
2024-05-16 11:57:14 +1000

    works with csv

--- str/coloc/coloc_results_parser.py
+++ str/coloc/coloc_results_parser.py
@@ -5,10 +5,12 @@ This script concatenates the results of running `coloc_runner.py` (output is per
 analysis-runner --dataset "bioheart" \
     --description "Parse coloc results" \
     --access-level "test" \
-    --output-dir "str/associatr/coloc" \
+    --memory='16G' \
+    --storage='10G' \
+    --output-dir "str/associatr" \
     coloc_results_parser.py \
-    --coloc-dir=gs://cpg-bioheart-test/str/associatr/coloc \
-    --celltypes=CD4_TCM \
+    --coloc-dir=gs://cpg-bioheart-test/str/associatr/freeze_1/gwas_ld/bioheart-only-snps/gwas_ld \
+    --celltypes=gdT,B_intermediate,ILC,Plasmablast,dnT,ASDC,cDC1,pDC,NK_CD56bright,MAIT,B_memory,CD4_CTL,CD4_Proliferating,CD8_Proliferating,HSPC,NK_Proliferating,cDC2,CD16_Mono,Treg,CD14_Mono,CD8_TCM,CD4_TEM,CD8_Naive,CD4_TCM,NK,CD8_TEM,CD4_Naive,B_naive \
     --phenos=ibd
 
 
@@ -22,22 +24,23 @@ def coloc_results_combiner(coloc_dir, pheno, celltype):
     import pandas as pd
 
     from cpg_utils import to_path
+    from cpg_utils.config import output_path
 
-    files = list(to_path(f'{coloc_dir}/{pheno}/{celltype}').glob('*.tsv'))
+    files = list(to_path(f'{coloc_dir}/{pheno}/{celltype}').glob('*.csv'))
     # List to store DataFrames
     dfs = []
 
     # Iterate over each file path
     for file_path in files:
         # Read file into a DataFrame
-        df = pd.read_csv(file_path, sep='\t')
+        df = pd.read_csv(file_path)
         # Append DataFrame to the list
         dfs.append(df)
 
     # Concatenate DataFrames row-wise
     result_df = pd.concat(dfs, ignore_index=True)
     # Write the result to a CSV file
-    result_df.to_csv(f'{coloc_dir}/{pheno}/{celltype}/gene_summary_result.csv', index=False)
+    result_df.to_csv(output_path(f'{coloc_dir}/{pheno}/{celltype}/gene_summary_result.csv'), index=False)
 
 
 @click.command()
