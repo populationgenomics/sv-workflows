eb75902242ec360ecbd8ea80e1b14d88af62764b
Hope Tanudisastro <40783500+hopedisastro@users.noreply.github.com>
2024-05-17 17:33:42 +1000

    Adds to_path()  (#213)
    * Update associatr_runner.py
    
    * lint

--- str/associatr/associatr_runner.py
+++ str/associatr/associatr_runner.py
@@ -11,7 +11,7 @@ Ensure prior scripts have been run to generate dependent files, particularly:
  analysis-runner --dataset "bioheart" --config associatr_runner.toml \
     --description "run associatr" \
     --access-level "full" \
-    --memory "16G" \
+    --memory "8G" \
     --output-dir "str/associatr/common_variants_snps/tob_n1055" \
      python3 associatr_runner.py
 
@@ -70,8 +70,11 @@ def main():
                 cis_window_size = get_config()['associatr']['cis_window_size']
                 version = get_config()['associatr']['version']
 
-                if output_path(
-                    f'results/{version}/{celltype}/{chromosome}/{gene}_{cis_window_size}bp', 'analysis',
+                if to_path(
+                    output_path(
+                        f'results/{version}/{celltype}/{chromosome}/{gene}_{cis_window_size}bp',
+                        'analysis',
+                    ),
                 ).exists():
                     continue
 
