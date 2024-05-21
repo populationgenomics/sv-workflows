c570f69ae67ec97ee0f0b41849229e9bd8414c44
hopedisastro <40783500+hopedisastro@users.noreply.github.com>
2024-05-21 14:35:33 +1000

    to_path

--- str/associatr/meta_analysis/run_gene_level_pvals_meta.py
+++ str/associatr/meta_analysis/run_gene_level_pvals_meta.py
@@ -199,10 +199,10 @@ def bonferroni_compute(gene_files, cell_type, chromosome):
         pvals, gene_name, row_dict = process_single_file(gene_file)
 
         # write to output
-        gcs_output = output_path(
+        gcs_output = to_path(output_path(
             f'gene_level_pvals/bonferroni/{cell_type}/chr{chromosome}/{gene_name}_gene_level_pval.tsv',
             'analysis',
-        )
+        ))
         if gcs_output.exists():
             print(f'{gene_file} already processed. Skipping...')
             continue
