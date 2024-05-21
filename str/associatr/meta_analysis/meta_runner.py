7be8d7e1903d17c220faa2bfec1ceae063911a83
hopedisastro <40783500+hopedisastro@users.noreply.github.com>
2024-05-21 10:05:30 +1000

    Update meta_runner.py

--- str/associatr/meta_analysis/meta_runner.py
+++ str/associatr/meta_analysis/meta_runner.py
@@ -199,12 +199,12 @@ def main(
 
             # run meta-analysis for each gene
             for gene in intersected_genes:
-                j = get_batch(name='compute_meta').new_python_job(name=f'compute_meta_{cell_type}_{chromosome}_{gene}')
-                j.cpu(0.25)
                 if to_path(output_path(
                     f"meta_results/{cell_type}/{chromosome}/{gene}_100000bp_meta_results.tsv", "analysis",
                 )).exists():
                     continue
+                j = get_batch(name='compute_meta').new_python_job(name=f'compute_meta_{cell_type}_{chromosome}_{gene}')
+                j.cpu(0.25)
                 if always_run:
                     j.always_run()
                 j.image(image_path('r-meta'))
