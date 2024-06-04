21781ee61499937c75ce4def85fc719cbada1413
Hope Tanudisastro <40783500+hopedisastro@users.noreply.github.com>
2024-06-05 07:17:15 +1000

    Use unmasked ref fasta if specify 'hgdp' in dataset param (#224)
    * try unmasked fasta for hgdp
    
    * Update str_iterative_eh_runner.py
    
    * cheeck if 'hgdp' in
    
    * Update get_cis_numpy_files.py
    
    * remove \n from 'sex' variable

--- str/runners/str_iterative_eh_runner.py
+++ str/runners/str_iterative_eh_runner.py
@@ -86,7 +86,10 @@ def main(
     b = get_batch()
 
     # Reference fasta
-    ref_fasta = str(reference_path('broad/ref_fasta'))
+    if 'hgdp' in dataset:
+        ref_fasta = 'gs://cpg-common-main/references/hg38/v0/Homo_sapiens_assembly38.fasta'
+    else:
+        ref_fasta = str(reference_path('broad/ref_fasta'))
     ref = b.read_input_group(
         **dict(
             base=ref_fasta,
@@ -112,6 +115,7 @@ def main(
             split_line = line.split(',')
             cpg_id = split_line[0]
             sex = split_line[2]
+            sex = sex.replace('\n', '')
             if cpg_id == 's':  # header line
                 continue
             if sex == 'XY':
