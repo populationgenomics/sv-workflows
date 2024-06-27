6a86f2a803c7a665f5f04e61a0cb70f38669bcd7
hopedisastro <40783500+hopedisastro@users.noreply.github.com>
2024-06-27 13:42:07 +1000

    Update coloc_results_parser.py

--- str/coloc/coloc_results_parser.py
+++ str/coloc/coloc_results_parser.py
@@ -8,9 +8,9 @@ analysis-runner --dataset "bioheart" \
     --image "australia-southeast1-docker.pkg.dev/analysis-runner/images/driver:d4922e3062565ff160ac2ed62dcdf2fba576b75a-hail-8f6797b033d2e102575c40166cf0c977e91f834e" \
     --output-dir "str/associatr" \
     coloc_results_parser.py \
-    --coloc-dir=gs://cpg-bioheart-test-analysis/str/associatr/coloc-snp-only \
+    --coloc-dir=gs://cpg-bioheart-test-analysis/str/associatr/coloc-snp-only/sig_str_filter_only \
     --celltypes=gdT,B_intermediate,ILC,Plasmablast,dnT,ASDC,cDC1,pDC,NK_CD56bright,MAIT,B_memory,CD4_CTL,CD4_Proliferating,CD8_Proliferating,HSPC,NK_Proliferating,cDC2,CD16_Mono,Treg,CD14_Mono,CD8_TCM,CD4_TEM,CD8_Naive,CD4_TCM,NK,CD8_TEM,CD4_Naive,B_naive \
-    --phenos=covid_GCST011071
+    --phenos=ibd_liu2023
 
 
 """
@@ -39,7 +39,7 @@ def coloc_results_combiner(coloc_dir, pheno, celltype):
     # Concatenate DataFrames row-wise
     result_df = pd.concat(dfs, ignore_index=True)
     # Write the result to a CSV file
-    result_df.to_csv(output_path(f'coloc-snp-only/{pheno}/{celltype}/gene_summary_result.csv', 'analysis'), index=False)
+    result_df.to_csv(output_path(f'coloc-snp-only/sig_str_filter_only{pheno}/{celltype}/gene_summary_result.csv', 'analysis'), index=False)
 
 
 @click.command()
