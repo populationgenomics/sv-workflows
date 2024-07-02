76ac94e9ca7242dd5177b4bb01765e8c8220f910
hopedisastro <40783500+hopedisastro@users.noreply.github.com>
2024-07-02 16:05:57 +1000

    do it for 44 files

--- str/coloc/liftover.py
+++ str/coloc/liftover.py
@@ -9,42 +9,99 @@ New as of 2024 with support for multi-allelic variants.
 
 analysis-runner --dataset "bioheart" --description "Liftover variants from hg19 to hg38" --access-level "test" \
     --output-dir "str/associatr/liftover" \
-    --memory "64G" \
-    --storage "20G" \
-    liftover.py --variants-file=gs://cpg-bioheart-test-upload/str/ukbb-snp-catalogs/white_british_albumin_snp_gwas_results.tab.gz
+    --memory "4G" \
+    --storage "5G" \
+    liftover.py
 """
 
-import click
 import pandas as pd
-import logging
-import requests
-import time
+
 from cpg_utils import to_path
+from cpg_utils.hail_batch import get_batch
 
-@click.command()
-@click.option('--variants-file', required=True)
-def main(variants_file: str):
-    """
-    Opens the variants_file json and read the liftover variant id for each variant.
-    If the liftover variant id is null, then get the liftover variant id
-    from the Broad institiute API. Save the variants json with the new liftover ids.
-    """
-    file_path = to_path(variants_file)
 
-    df = pd.read_csv(file_path, sep='\t', compression='gzip', usecols=['ID','REF','ALT','BETA','SE','P'])
-    liftover_df = pd.read_csv('gs://cpg-bioheart-test/str/gymrek-ukbb-snp-gwas-catalogs/ukbb_snp_chr_pos_hg38_liftover.bed', sep = '\t', header=None, names=['chromosome', 'position', 'end38', 'rsid'])
+def liftover(phenotype):
+    file_path = to_path(
+        f'gs://cpg-bioheart-test-upload/str/ukbb-snp-catalogs/white_british_{phenotype}_snp_gwas_results.tab.gz',
+    )
 
-    df = pd.merge(df, liftover_df, left_on = 'ID', right_on = 'rsid')
-    df['varbeta'] = df['SE']**2
+    df = pd.read_csv(file_path, sep='\t', compression='gzip', usecols=['ID', 'REF', 'ALT', 'BETA', 'SE', 'P'])
+    liftover_df = pd.read_csv(
+        'gs://cpg-bioheart-test/str/gymrek-ukbb-snp-gwas-catalogs/ukbb_snp_chr_pos_hg38_liftover.bed',
+        sep='\t',
+        header=None,
+        names=['chromosome', 'position', 'end38', 'rsid'],
+    )
+
+    df = pd.merge(df, liftover_df, left_on='ID', right_on='rsid')
+    df['varbeta'] = df['SE'] ** 2
     df['beta'] = df['BETA']
     df['snp'] = df['chromosome'] + '_' + df['position'].astype(str) + '_' + df['REF'] + '_' + df['ALT']
     df['p_value'] = df['P']
 
-
     # Write out the results
-    df[['chromosome', 'position', 'varbeta', 'beta', 'snp', 'p_value']].to_csv('gs://cpg-bioheart-test/str/gyremk-ukbb-snp-gwas-catalogs/white_british_albumin_snp_gwas_results_hg38.tab.gz', sep='\t', index=False)
+    df[['chromosome', 'position', 'varbeta', 'beta', 'snp', 'p_value']].to_csv(
+        'gs://cpg-bioheart-test/str/gymrek-ukbb-snp-gwas-catalogs/white_british_albumin_snp_gwas_results_hg38.tab.gz',
+        sep='\t',
+        index=False,
+    )
+
+
+def main():
+    b = get_batch()
+    liftover_job = b.new_python_job('Liftover variants from hg19 to hg38')
+    phenotypes = [
+        "alanine_aminotransferase",
+        "albumin",
+        "alkaline_phosphatase",
+        "apolipoprotein_a",
+        "apolipoprotein_b",
+        "aspartate_aminotransferase",
+        "c_reactive_protein",
+        "calcium",
+        "cholesterol",
+        "creatinine",
+        "cystatin_c",
+        "eosinophil_count",
+        "eosinophil_percent",
+        "gamma_glutamyltransferase",
+        "glucose",
+        "glycated_haemoglobin",
+        "haematocrit",
+        "haemoglobin_concentration",
+        "hdl_cholesterol",
+        "igf_1",
+        "ldl_cholesterol_direct",
+        "lymphocyte_count",
+        "lymphocyte_percent",
+        "mean_corpuscular_haemoglobin",
+        "mean_corpuscular_haemoglobin_concentration",
+        "mean_corpuscular_volume",
+        "mean_platelet_volume",
+        "mean_sphered_cell_volume",
+        "neutrophil_count",
+        "neutrophil_percent",
+        "phosphate",
+        "platelet_count",
+        "platelet_crit",
+        "platelet_distribution_width",
+        "red_blood_cell_count",
+        "red_blood_cell_distribution_width",
+        "shbg",
+        "total_bilirubin",
+        "total_protein",
+        "triglycerides",
+        "urate",
+        "urea",
+        "vitamin_d",
+        "white_blood_cell_count",
+    ]
+    for pheno in phenotypes:
+        liftover_job.memory('64G')
+        liftover_job.storage('10G')
+        liftover_job.call(liftover, pheno)
+    b.run(wait=False)
 
 
 if __name__ == '__main__':
-
     main()  # pylint: disable=no-value-for-parameter
