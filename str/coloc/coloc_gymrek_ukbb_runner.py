62d709d6f1e93e461f821ec3d2eb8f2304857c5c
hopedisastro <40783500+hopedisastro@users.noreply.github.com>
2024-07-03 09:37:31 +1000

    trying to optimise

--- str/coloc/coloc_gymrek_ukbb_runner.py
+++ str/coloc/coloc_gymrek_ukbb_runner.py
@@ -33,131 +33,158 @@ def cyclical_shifts(s):
     return [s[i:] + s[:i] for i in range(len(s))]
 
 
-def coloc_runner(gwas_str, gwas_snp, eqtl_file_path, celltype, pheno):
+def coloc_runner(result_df_cfm_str_celltype, var_table, str_gwas, snp_gwas, eqtl_cis_dir, celltype, pheno):
     import rpy2.robjects as ro
     from rpy2.robjects import pandas2ri
 
-    from cpg_utils.hail_batch import output_path
 
-    eqtl = pd.read_csv(eqtl_file_path, sep='\t')
-    gene = eqtl_file_path.split('/')[-1].split('_')[0]
+    for gene in result_df_cfm_str_celltype['gene']:
+        if to_path(
+            output_path(
+                f"coloc/sig_str_and_gwas_hit/gymrek-ukbb-{pheno}/{celltype}/{gene}_100kb.tsv",
+                'analysis',
+            ),
+        ).exists():
+            print('Coloc results already processed for ' + gene + ': skipping....')
+            continue
+        # extract the coordinates for the cis-window (gene +/- 100kB)
+        gene_table = var_table[var_table['gene_ids'] == gene]
+        start = float(gene_table['start'].astype(float)) - 100000
+        end = float(gene_table['end'].astype(float)) + 100000
+        chrom = gene_table['chr'].iloc[0][3:]
 
-    # create an empty df with the following columns: 'chromosome', 'position', 'varbeta', 'beta', 'snp', 'p_value'
-    gwas_str_harmonised = pd.DataFrame(columns=['chromosome', 'position', 'varbeta', 'beta', 'snp', 'p_value'])
+        ## subset the STR GWAS data for the cis-window
+        str_gwas_subset = str_gwas[
+            (str_gwas['chromosome'] == int(chrom))
+            & (str_gwas['start_pos (hg38)'] >= start)
+            & (str_gwas['start_pos (hg38)'] <= end)
+        ]
 
-    # harmonise Gymrek STR definitons with ours
-    eqtl_str = eqtl[~eqtl['motif'].str.contains('-')]  # str entries have no '-' in motif column
-    for index, gwas_row in gwas_str.iterrows():
-        for index2, eqtl_row in eqtl_str.iterrows():
-            if eqtl_row['pos'] >= (gwas_row['start_pos (hg38)'] - 1) and eqtl_row['pos'] <= gwas_row['end_pos (hg38)']:
-                if eqtl_row['motif'] in cyclical_shifts(gwas_row['repeat_unit']):
-                    new_entry = pd.DataFrame(
-                        [
-                            {
-                                'chromosome': eqtl_row["chr"],
-                                'position': eqtl_row['pos'],
-                                'varbeta': gwas_row['standard_error'] ** 2,
-                                'beta': gwas_row['beta'],
-                                'snp': f'{eqtl_row["chr"]}_{eqtl_row["pos"]}_{eqtl_row["motif"]}',
-                                'p_value': gwas_row['p_value'],
-                            },
-                        ],
-                    )
-                    gwas_str_harmonised = pd.concat([gwas_str_harmonised, new_entry], ignore_index=True)
+        ## subset the SNP GWAS data for the cis-window
+        snp_gwas_subset = snp_gwas[
+            (snp_gwas['chromosome'] == gene_table['chr'].iloc[0])
+            & (snp_gwas['position'] >= start)
+            & (snp_gwas['position'] <= end)
+        ]
 
-                    continue
-    # for testing purposes
-    gwas_str_harmonised.to_csv(
-        output_path(
-            f"coloc/sig_str_and_gwas_hit/gymrek-ukbb-{pheno}/{celltype}/{gene}_100kb_gwas_str_harmonised.tsv",
-            'analysis',
-        ),
-        sep='\t',
-        index=False,
-    )
-    gwas_snp.to_csv(
-        output_path(f"coloc/sig_str_and_gwas_hit/gymrek-ukbb-{pheno}/{celltype}/{gene}_100kb_gwas_snp.tsv", 'analysis'),
-        sep='\t',
-        index=False,
-    )
-    # concatenate gwas_str with gwas_snp (row wise)
-    gwas = pd.concat([gwas_str_harmonised, gwas_snp], ignore_index=True)
+        if str_gwas_subset.empty and snp_gwas_subset.empty:
+            print('No GWAS data for ' + gene + ' in the cis-window: skipping....')
+            continue
 
-    gwas = gwas[~gwas['beta'].isna()]
+        if str_gwas_subset['p_value'].min() > 5e-8 and snp_gwas_subset['p_value'].min() > 5e-8:
+            print('No significant GWAS data for ' + gene + ' in the cis-window: skipping....')
+            continue
 
-    # for testing purposes
-    gwas.to_csv(
-        output_path(f"coloc/sig_str_and_gwas_hit/gymrek-ukbb-{pheno}/{celltype}/{gene}_100kb_gwas.tsv", 'analysis'),
-        sep='\t',
-        index=False,
-    )
-    ro.r('library(coloc)')
-    ro.r('library(tidyverse)')
+        print('Extracted GWAS data for ' + gene)  # proceed to coloc for this gene
 
-    with (ro.default_converter + pandas2ri.converter).context():
-        gwas_r = ro.conversion.get_conversion().py2rpy(gwas)
-    print('loaded in gwas_r')
-    ro.globalenv['gwas_r'] = gwas_r
-    ro.r(
-        '''
-    gwas_r = gwas_r %>% distinct(beta, varbeta, position,snp)
-    gwas_r = gwas_r%>% as.list()
-    gwas_r$type = 'quant'
-    gwas_r$sdY = 1
+        eqtl_file_path = f'{eqtl_cis_dir}/{celltype}/chr{chrom}/{gene}_100000bp_meta_results.tsv'
+        eqtl = pd.read_csv(eqtl_file_path, sep='\t')
+        gene = eqtl_file_path.split('/')[-1].split('_')[0]
 
-     ''',
-    )
-    eqtl['beta'] = eqtl['coeff_meta']
-    eqtl['varbeta'] = eqtl['se_meta'] ** 2
-    eqtl['position'] = eqtl['pos']
-    eqtl['snp'] = eqtl['chr'] + '_' + eqtl['position'].astype(str) + '_' + eqtl['motif']
-    eqtl['snp'] = eqtl['snp'].str.replace('-', '_', regex=False)
+        # create an empty df with the following columns: 'chromosome', 'position', 'varbeta', 'beta', 'snp', 'p_value'
+        gwas_str_harmonised = pd.DataFrame(columns=['chromosome', 'position', 'varbeta', 'beta', 'snp', 'p_value'])
 
-    eqtl = eqtl.sort_values(by=['snp', 'pval_meta'])
+        # harmonise Gymrek STR definitons with ours
+        eqtl_str = eqtl[~eqtl['motif'].str.contains('-')]  # str entries have no '-' in motif column
+        for index, gwas_row in str_gwas_subset.iterrows():
+            for index2, eqtl_row in eqtl_str.iterrows():
+                if (
+                    eqtl_row['pos'] >= (gwas_row['start_pos (hg38)'] - 1)
+                    and eqtl_row['pos'] <= gwas_row['end_pos (hg38)']
+                ):
+                    if eqtl_row['motif'] in cyclical_shifts(gwas_row['repeat_unit']):
+                        new_entry = pd.DataFrame(
+                            [
+                                {
+                                    'chromosome': eqtl_row["chr"],
+                                    'position': eqtl_row['pos'],
+                                    'varbeta': gwas_row['standard_error'] ** 2,
+                                    'beta': gwas_row['beta'],
+                                    'snp': f'{eqtl_row["chr"]}_{eqtl_row["pos"]}_{eqtl_row["motif"]}',
+                                    'p_value': gwas_row['p_value'],
+                                },
+                            ],
+                        )
+                        gwas_str_harmonised = pd.concat([gwas_str_harmonised, new_entry], ignore_index=True)
 
-    # Step 2: Drop duplicate variants (keep the one with the lowest p-value)
-    eqtl = eqtl.drop_duplicates(subset='snp', keep='first')
-    # for testing purposes
-    eqtl.to_csv(
-        output_path(f"coloc/sig_str_and_gwas_hit/gymrek-ukbb-{pheno}/{celltype}/{gene}_100kb_eqtl.tsv", 'analysis'),
-        sep='\t',
-        index=False,
-    )
-    with (ro.default_converter + pandas2ri.converter).context():
-        eqtl_r = ro.conversion.get_conversion().py2rpy(eqtl)
-    print('loaded in eqtl_r')
-    ro.globalenv['eqtl_r'] = eqtl_r
-    ro.globalenv['gene'] = gene
-    ro.r(
-        '''
-    eqtl_r = eqtl_r%>% distinct(beta, varbeta, position, snp)
-    eqtl_r = eqtl_r %>% as.list()
-    eqtl_r$type = 'quant'
-    eqtl_r$sdY = 1
+                        continue
+
+        # concatenate gwas_str with gwas_snp (row wise)
+        gwas = pd.concat([gwas_str_harmonised, snp_gwas_subset], ignore_index=True)
+
+        gwas = gwas[~gwas['beta'].isna()]
+
+        ro.r('library(coloc)')
+        ro.r('library(tidyverse)')
+
+        with (ro.default_converter + pandas2ri.converter).context():
+            gwas_r = ro.conversion.get_conversion().py2rpy(gwas)
+        print('loaded in gwas_r')
+        ro.globalenv['gwas_r'] = gwas_r
+        ro.r(
+            '''
+                gwas_r = gwas_r %>% distinct(beta, varbeta, position,snp)
+                gwas_r = gwas_r%>% as.list()
+                gwas_r$type = 'quant'
+                gwas_r$sdY = 1
+
+                ''',
+        )
+        eqtl['beta'] = eqtl['coeff_meta']
+        eqtl['varbeta'] = eqtl['se_meta'] ** 2
+        eqtl['position'] = eqtl['pos']
+        eqtl['snp'] = eqtl['chr'] + '_' + eqtl['position'].astype(str) + '_' + eqtl['motif']
+        eqtl['snp'] = eqtl['snp'].str.replace('-', '_', regex=False)
+
+        eqtl = eqtl.sort_values(by=['snp', 'pval_meta'])
+
+        # Step 2: Drop duplicate variants (keep the one with the lowest p-value)
+        eqtl = eqtl.drop_duplicates(subset='snp', keep='first')
+
+        with (ro.default_converter + pandas2ri.converter).context():
+            eqtl_r = ro.conversion.get_conversion().py2rpy(eqtl)
+        print('loaded in eqtl_r')
+        ro.globalenv['eqtl_r'] = eqtl_r
+        ro.globalenv['gene'] = gene
+        ro.r(
+            '''
+                eqtl_r = eqtl_r%>% distinct(beta, varbeta, position, snp)
+                eqtl_r = eqtl_r %>% as.list()
+                eqtl_r$type = 'quant'
+                eqtl_r$sdY = 1
+
+                p4 <- tryCatch({
+
+                  my.res <- coloc.abf(dataset1=gwas_r,
+                                dataset2=eqtl_r)
+
+                p_df <- data.frame(gene,my.res$summary[1], my.res$summary[2], my.res$summary[3], my.res$summary[4], my.res$summary[5], my.res$summary[6])
+                names(p_df) <- c('gene', 'nvariants_coloc_tested','PP.H0.abf','PP.H1.abf','PP.H2.abf','PP.H3.abf','PP.H4.abf')
 
 
-    my.res <- coloc.abf(dataset1=gwas_r,
-                    dataset2=eqtl_r)
+                }, error = function(e) {
 
-    p_df <- data.frame(gene,my.res$summary[1], my.res$summary[2], my.res$summary[3], my.res$summary[4], my.res$summary[5], my.res$summary[6])
-    names(p_df) <- c('gene', 'nvariants_coloc_tested','PP.H0.abf','PP.H1.abf','PP.H2.abf','PP.H3.abf','PP.H4.abf')
-    ''',
-    )
+                print(paste("An error occurred:", e))
+                0
+                })
 
-    # convert to pandas df
-    with (ro.default_converter + pandas2ri.converter).context():
-        pd_p4_df = ro.conversion.get_conversion().rpy2py(ro.r('p_df'))
+                ''',
+        )
 
-    # add cell type and chrom annotation to df
-    pd_p4_df['celltype'] = celltype
-    pd_p4_df['chrom'] = eqtl['chr'].iloc[0]
+        # convert to pandas df
+        with (ro.default_converter + pandas2ri.converter).context():
+            pd_p4_df = ro.conversion.get_conversion().rpy2py(ro.r('p_df'))
 
-    # write to GCS
-    pd_p4_df.to_csv(
-        output_path(f"coloc/sig_str_and_gwas_hit/gymrek-ukbb-{pheno}/{celltype}/{gene}_100kb.tsv", 'analysis'),
-        sep='\t',
-        index=False,
-    )
+        # add cell type and chrom annotation to df
+        pd_p4_df['celltype'] = celltype
+        pd_p4_df['chrom'] = eqtl['chr'].iloc[0]
+
+        # write to GCS
+        pd_p4_df.to_csv(
+            output_path(f"coloc/sig_str_and_gwas_hit/gymrek-ukbb-{pheno}/{celltype}/{gene}_100kb.tsv", 'analysis'),
+            sep='\t',
+            index=False,
+        )
 
 
 @click.option(
@@ -243,66 +270,30 @@ def main(eqtl_cis_dir, egenes_dir, celltypes, var_annotation_file, pheno, max_pa
             regex=False,
         )  # remove .tsv from gene names (artefact of the data file)
 
+        b = get_batch(name='Run coloc')
+
         for celltype in celltypes.split(','):
             result_df_cfm_str_celltype = result_df_cfm_str[
                 result_df_cfm_str['celltype'] == celltype
             ]  # filter for the celltype of interest
-            # for gene in result_df_cfm_str_celltype['gene']:
-            for gene in ['ENSG00000198502']:
-                if to_path(
-                    output_path(
-                        f"coloc/sig_str_and_gwas_hit/gymrek-ukbb-{pheno}/{celltype}/{gene}_100kb.tsv",
-                        'analysis',
-                    ),
-                ).exists():
-                    print('Coloc results already processed for ' + gene + ': skipping....')
-                    continue
-                # extract the coordinates for the cis-window (gene +/- 100kB)
-                gene_table = var_table[var_table['gene_ids'] == gene]
-                start = float(gene_table['start'].astype(float)) - 100000
-                end = float(gene_table['end'].astype(float)) + 100000
-                chrom = gene_table['chr'].iloc[0][3:]
 
-                ## subset the STR GWAS data for the cis-window
-                str_gwas_subset = str_gwas[
-                    (str_gwas['chromosome'] == int(chrom))
-                    & (str_gwas['start_pos (hg38)'] >= start)
-                    & (str_gwas['start_pos (hg38)'] <= end)
-                ]
-
-                ## subset the SNP GWAS data for the cis-window
-                snp_gwas_subset = snp_gwas[
-                    (snp_gwas['chromosome'] == gene_table['chr'].iloc[0])
-                    & (snp_gwas['position'] >= start)
-                    & (snp_gwas['position'] <= end)
-                ]
-
-                if str_gwas_subset.empty and snp_gwas_subset.empty:
-                    print('No GWAS data for ' + gene + ' in the cis-window: skipping....')
-                    continue
-
-                if str_gwas_subset['p_value'].min() > 5e-8 and snp_gwas_subset['p_value'].min() > 5e-8:
-                    print('No significant GWAS data for ' + gene + ' in the cis-window: skipping....')
-                    continue
-
-                print('Extracted GWAS data for ' + gene)
-
-                # run coloc
-                b = get_batch(name='Run coloc')
-                coloc_job = b.new_python_job(
-                    f'Coloc for {gene}: {celltype}',
-                )
-                coloc_job.cpu(0.25)
-                coloc_job.image(image_path('r-meta'))
-                coloc_job.call(
-                    coloc_runner,
-                    str_gwas_subset,
-                    snp_gwas_subset,
-                    f'{eqtl_cis_dir}/{celltype}/chr{chrom}/{gene}_100000bp_meta_results.tsv',
-                    celltype,
-                    phenotype,
-                )
-                manage_concurrency_for_job(coloc_job)
+            # run one coloc job for every celltype-phenotype combination
+            coloc_job = b.new_python_job(
+                f'Coloc for {celltype} and {phenotype}',
+            )
+            coloc_job.cpu(3)
+            coloc_job.image(image_path('r-meta'))
+            coloc_job.call(
+                coloc_runner,
+                result_df_cfm_str_celltype,
+                var_table,
+                str_gwas,
+                snp_gwas,
+                eqtl_cis_dir,
+                celltype,
+                phenotype,
+            )
+            manage_concurrency_for_job(coloc_job)
 
     get_batch().run(wait=False)
 
