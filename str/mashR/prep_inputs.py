02c0598ec4805cf2d7dcf1c8c8a98b3e96448730
hopedisastro <40783500+hopedisastro@users.noreply.github.com>
2024-07-15 13:31:13 +1000

    add a to exists check

--- str/mashR/prep_inputs.py
+++ str/mashR/prep_inputs.py
@@ -9,6 +9,7 @@ This script prepares two files:
 
 analysis-runner --dataset "bioheart" \
     --description "Prepare inputs for mashr" \
+    --image "australia-southeast1-docker.pkg.dev/analysis-runner/images/driver:d4922e3062565ff160ac2ed62dcdf2fba576b75a-hail-8f6797b033d2e102575c40166cf0c977e91f834e" \
     --access-level "test" \
     --output-dir "potato" \
     prep_inputs.py
@@ -21,6 +22,7 @@ import numpy as np
 import pandas as pd
 
 from cpg_utils.hail_batch import get_batch
+from cpg_utils import to_path
 
 
 def cell_chrom_parser(cell, chrom, estrs_coord_chrom):
@@ -37,25 +39,25 @@ def cell_chrom_parser(cell, chrom, estrs_coord_chrom):
                 sep='\t',
             )
             df = df[(df['pos'] == pos) & (df['motif'] == motif) & (df['ref_len'] == ref_len)]
+            beta = df['coeff_meta'].iloc[0]
+            se = df['se_meta'].iloc[0]
+            # save the beta and se into a row:
+            beta_se = pd.DataFrame(
+                {
+                    'chrom': chrom,
+                    'pos': pos,
+                    'motif': motif,
+                    'ref_len': ref_len,
+                    'gene': gene_name,
+                    f'{cell}_beta': beta,
+                    f'{cell}_se': se,
+                },
+                index=[0],
+            )
+            cell_df = pd.concat([cell_df, beta_se], axis=0)
         except:
             continue
 
-        beta = df['coeff_meta'].iloc[0]
-        se = df['se_meta'].iloc[0]
-        # save the beta and se into a row:
-        beta_se = pd.DataFrame(
-            {
-                'chrom': chrom,
-                'pos': pos,
-                'motif': motif,
-                'ref_len': ref_len,
-                'gene': gene_name,
-                f'{cell}_beta': beta,
-                f'{cell}_se': se,
-            },
-            index=[0],
-        )
-        cell_df = pd.concat([cell_df, beta_se], axis=0)
     cell_df.to_csv(
         f'gs://cpg-bioheart-test/str/associatr/tob_n1055_and_bioheart_n990/mashr/estrs_beta_se/{cell}/{chrom}/beta_se.tsv',
         sep='\t',
@@ -75,6 +77,8 @@ def main():
     for cell in celltypes:
         for chrom in range(1, 23):
             estrs_coord_chrom = estrs_coord[estrs_coord['chr'] == f'chr{chrom}']
+            if to_path(f'gs://cpg-bioheart-test/str/associatr/tob_n1055_and_bioheart_n990/mashr/estrs_beta_se/{cell}/{chrom}/beta_se.tsv').exists():
+                continue
             job = b.new_python_job(f'Prep eSTRs for mashr {cell} {chrom}')
             job.cpu(0.25)
             job.call(cell_chrom_parser, cell, chrom, estrs_coord_chrom)
