1a52a0c0cc2b423ea0448cddc5c6f1a64eca5bcb
Hope Tanudisastro <40783500+hopedisastro@users.noreply.github.com>
2024-07-22 10:10:24 +1000

    Colocalisation runner for GWAS IBD (#197)
    * Create coloc_ibd_runner.py
    
    * add celltype to df
    
    * update to run with pseudobulk SNP results
    
    * ;omt
    
    * move everything into the helper function
    
    * Update coloc_ibd_runner.py
    
    * logic error
    
    * t
    
    * Update coloc_ibd_runner.py
    
    * Update coloc_ibd_runner.py
    
    * Update coloc_ibd_runner.py
    
    * bump up resource
    
    * remove redundant code
    
    * debug
    
    * Update coloc_ibd_runner.py
    
    * Update coloc_ibd_runner.py
    
    * Update coloc_ibd_runner.py
    
    * Update coloc_ibd_runner.py
    
    * wip all purpose coloc runner
    
    * Update coloc_runner.py
    
    * unindent
    
    * add logging
    
    * change var name
    
    * add celltype and chrom annotations
    
    * parsing issue
    
    * fdebug
    
    * Update coloc_runner.py
    
    * key error
    
    * update gene name!
    
    * remove entries where beta == 0 and se == 0
    
    * lint and add 5e-8 check of gwas data
    
    * Update coloc_runner.py
    
    * sigstr and gwas filter
    
    * Update coloc_runner.py
    
    * sig str filter only
    
    * Create coloc_ukbb_runner.py
    
    * debug
    
    * looks ok to run
    
    * Update coloc_ukbb_runner.py
    
    * changing to chr-specific gwas files
    
    * lint
    
    * logic error
    
    * add sh script
    
    * add in more docu
    
    * Update generate_commands.sh
    
    * Create generate_commands_1.sh
    
    * Update generate_commands.sh
    
    * update annotations

---
+++ str/coloc/coloc_ibd_runner.py
@@ -0,0 +1,187 @@
+#!/usr/bin/env python3
+
+"""
+This script performs colocalisation analysis betweeen eGenes identified by pseudobulk STR analysis and GWAS signals from IBD meta-analysis.
+
+1) Identify eGenes (FDR <5%) from the STR analysis
+2) Extract the SNP GWAS data for the cis-window (gene +/- 100kB)
+3) Run coloc for each eGene - only do this if STR is still the lead signal in the joint SNP+STR eQTL analysis.
+4) Write the results to a TSV file
+
+analysis-runner --dataset "bioheart" \
+    --description "Run coloc for eGenes identified by STR analysis" \
+    --access-level "test" \
+    --output-dir "str/associatr" \
+    coloc_ibd_runner.py \
+    --snp-cis-dir "gs://cpg-bioheart-test/str/associatr/common_variants_snps/tob_n1055_and_bioheart_n990/meta_results/meta_results" \
+    --celltypes "cDC2"
+
+    ,NK,CD8_TEM,CD4_Naive,B_naive
+"
+,,CD4_Naive,B_naive
+
+"""
+
+import click
+import pandas as pd
+
+from cpg_utils import to_path
+from cpg_utils.hail_batch import get_batch, output_path
+
+def coloc_runner(gene, snp_cis_dir, var_annotation_file, gwas_file, celltype):
+    import rpy2.robjects as ro
+    from rpy2.robjects import pandas2ri
+
+    from cpg_utils.hail_batch import output_path
+
+    # read in gene annotation file
+    var_table = pd.read_csv(var_annotation_file)
+
+    hg38_map = pd.read_csv(
+        gwas_file,
+        sep='\t',
+        header=None,
+        names=['CHR', 'BP', 'END', 'FRQ_A_12882', 'FRQ_U_21770', 'OR', 'SE', 'P'],
+    )
+
+    gene_table = var_table[var_table['gene_ids'] == gene]
+    chr = gene_table['chr'].iloc[0][3:]
+    if to_path(f'{snp_cis_dir}/{celltype}/chr{chr}/{gene}_100000bp_meta_results.tsv').exists():
+        print('Cis results for ' + gene + ' exist: proceed with coloc')
+
+        # extract the coordinates for the cis-window (gene +/- 100kB)
+        start = float(gene_table['start'].astype(float)) - 100000
+        end = float(gene_table['end'].astype(float)) + 100000
+        hg38_map_chr = hg38_map[hg38_map['CHR'] == int(chr)]
+        hg38_map_chr_start = hg38_map_chr[hg38_map_chr['BP'] >= start]
+        hg38_map_chr_start_end = hg38_map_chr_start[hg38_map_chr_start['BP'] <= end]
+        hg38_map_chr_start_end['locus'] = (
+            hg38_map_chr_start_end['CHR'].astype(str) + ':' + hg38_map_chr_start_end['BP'].astype(str)
+        )
+        hg38_map_chr_start_end[['locus']].to_csv(
+            output_path(f'coloc-snp-only/lead-str-signal/ibd/{celltype}/{gene}_snp_gwas_list.csv'),
+            index=False,
+        )
+        if hg38_map_chr_start_end.empty:
+            print('No SNP GWAS data for ' + gene + ' in the cis-window: skipping....')
+            return
+        print('Extracted SNP GWAS data for ' + gene)
+    else:
+        print('No cis results for ' + gene + ' exist: skipping....')
+        return
+
+    ro.r('library(coloc)')
+    ro.r('library(tidyverse)')
+
+    with (ro.default_converter + pandas2ri.converter).context():
+        gwas_r = ro.conversion.get_conversion().py2rpy(hg38_map_chr_start_end)
+    ro.globalenv['gwas_r'] = gwas_r
+    ro.r(
+        '''
+    gwas_r$MAF = (gwas_r$FRQ_A_12882*12882 + gwas_r$FRQ_U_21770*21770)/(12882+21770)
+    gwas_r$pvalues = gwas_r$P
+    gwas_r$or = gwas_r$OR
+    gwas_r$beta = log(gwas_r$or)
+    gwas_r$varbeta = (gwas_r$SE/gwas_r$or)**2
+    gwas_r$position = gwas_r$BP
+    gwas_r$snp = paste('s', gwas_r$BP, sep = '')
+
+    gwas_r = gwas_r %>% select(beta, varbeta, position,snp,MAF)
+    gwas_r = gwas_r %>% distinct(position, .keep_all = TRUE)
+    gwas_r = gwas_r%>% as.list()
+    gwas_r$type = 'cc'
+    gwas_r$N = 34652
+    gwas_r$s = 0.33
+
+     ''',
+    )
+    eqtl = pd.read_csv(f'{snp_cis_dir}/{celltype}/chr{chr}/{gene}_100000bp_meta_results.tsv', sep='\t')
+    with (ro.default_converter + pandas2ri.converter).context():
+        eqtl_r = ro.conversion.get_conversion().py2rpy(eqtl)
+    ro.globalenv['eqtl_r'] = eqtl_r
+    ro.globalenv['gene'] = gene
+    ro.r(
+        '''
+    eqtl_r = eqtl_r %>% filter(!is.na(coeff_meta))
+    eqtl_r = eqtl_r %>% distinct(pos, .keep_all = TRUE)
+    eqtl_r$beta = eqtl_r$coeff_meta
+    eqtl_r$varbeta = eqtl_r$se_meta**2
+    eqtl_r$position = eqtl_r$pos
+    eqtl_r$snp = paste('s', eqtl_r$position, sep = '')
+    eqtl_r = eqtl_r %>% select(beta, varbeta, position, snp)
+
+    eqtl_r = eqtl_r %>% as.list()
+    eqtl_r$type = 'quant'
+    eqtl_r$sdY = 1
+
+
+    my.res <- coloc.abf(dataset1=gwas_r,
+                    dataset2=eqtl_r)
+
+    p_df <- data.frame(gene, my.res$summary[6])
+    names(p_df) <- c('gene', 'PP.H4.abf')
+    ''',
+    )
+
+    # convert to pandas df
+    with (ro.default_converter + pandas2ri.converter).context():
+        pd_p4_df = ro.conversion.get_conversion().rpy2py(ro.r('p_df'))
+
+    # add cell type annotation to df
+    pd_p4_df['celltype'] = celltype
+
+    # write to GCS
+    pd_p4_df.to_csv(
+        f'{output_path(f"coloc-snp-only/lead-str-signal/ibd/{celltype}/{gene}_100kb.tsv")}',
+        sep='\t',
+        index=False,
+    )
+
+@click.option(
+    '--snp-cis-dir',
+    help='Path to the directory containing the SNP cis results',
+    default='gs://cpg-bioheart-test/saige-qtl/bioheart_n990/v2/output_files/output_files',
+)
+@click.option('--celltypes', help='Cell type for which the eGenes were identified', default='CD4_TCM')
+@click.option(
+    '--var-annotation-file',
+    help='Gene annotation file path',
+    default='gs://cpg-bioheart-test/str/240_libraries_tenk10kp1_v2/concatenated_gene_info_donor_info_var.csv',
+)
+@click.option(
+    '--gwas-file',
+    help='Path to the GWAS file',
+    default='gs://cpg-bioheart-test/str/gwas_catalog/g38.EUR.IBD.gwas_info03_filtered.assoc',
+)
+@click.command()
+def main(snp_cis_dir, celltypes, var_annotation_file, gwas_file):
+    b = get_batch()
+
+    for celltype in celltypes.split(','):
+
+        # read in significant eSTRs file (joint analysis
+        estrs = pd.read_csv('gs://cpg-bioheart-test/str/associatr/eSTRs_from_joint_analysis.csv')
+        #subset to cell type
+        estrs = estrs[estrs['cell_type']==celltype]
+        #for gene in estrs['gene_name']:
+        for gene in ['ENSG00000121764']:
+            # run coloc
+            coloc_job = b.new_python_job(
+                f'Coloc for {gene}: {celltype}',
+            )
+            coloc_job.cpu(2)
+            coloc_job.image('australia-southeast1-docker.pkg.dev/cpg-common/images/r-meta:7.0.0')
+            coloc_job.call(
+                coloc_runner,
+                gene,
+                snp_cis_dir,
+                var_annotation_file,
+                gwas_file,
+                celltype,
+            )
+
+    b.run(wait=False)
+
+
+if __name__ == '__main__':
+    main()
