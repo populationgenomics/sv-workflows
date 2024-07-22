c13a737f86659bc8fc15366c764f27ea5179d483
Hope Tanudisastro <40783500+hopedisastro@users.noreply.github.com>
2024-07-22 10:27:59 +1000

    Merge results of SUSIE and FINEMAP together  (#233)
    * first try
    
    * space delimited not tab
    
    * Update str/fine-mapping/finemap_susie_merger.py
    
    Co-authored-by: michael-harper <109899932+michael-harper@users.noreply.github.com>
    
    * Update str/fine-mapping/finemap_susie_merger.py
    
    Co-authored-by: michael-harper <109899932+michael-harper@users.noreply.github.com>
    
    * Add helper files (#236)
    
    * wip
    
    * Update liftover.py
    
    * Update liftover.py
    
    * Update liftover.py
    
    * topath
    
    * read in less columns to sped things up
    
    * try with albumin
    
    * do it for 44 files
    
    * Update liftover.py
    
    * Update liftover.py
    
    * oom
    
    * wip
    
    * lint
    
    * test gene
    
    * fix append
    
    * write intermediate files for testing purpsoes
    
    * debug
    
    * drop duplicate STRs
    
    * debug
    
    * debug
    
    * Update coloc_gymrek_ukbb_runner.py
    
    * dbug
    
    * change eqtl cis dir
    
    * debg
    
    * trying to optimise
    
    * move imports into method
    
    * cpu
    
    * dbug
    
    * try iwth 8cpu
    
    * Update coloc_gymrek_ukbb_runner.py
    
    * optimise driver mem usage
    
    * bug
    
    * Create gwas_hit_genes.py
    
    * add click options
    
    * Update gwas_hit_genes.py
    
    * Update gwas_hit_genes.py
    
    * Qqplot patch - isoalte permuted control on the legend (#237)
    
    * add cell type mapping
    
    * Update qqplotter.py
    
    * rejig legend
    
    * reposiiton
    
    * Update qqplotter.py
    
    * Catalog sharder - hlper file (#238)
    
    * Create catalog_sharder.py
    
    * bump memory
    
    * G!!!
    
    * do it for all phenos
    
    * Colocalisation runner for GWAS IBD (#197)
    
    * Create coloc_ibd_runner.py
    
    * add celltype to df
    
    * update to run with pseudobulk SNP results
    
    * ;omt
    
    * move everything into the helper function
    
    * Update coloc_ibd_runner.py
    
    * logic error
    
    * t
    
    * Update coloc_ibd_runner.py
    
    * Update coloc_ibd_runner.py
    
    * Update coloc_ibd_runner.py
    
    * bump up resource
    
    * remove redundant code
    
    * debug
    
    * Update coloc_ibd_runner.py
    
    * Update coloc_ibd_runner.py
    
    * Update coloc_ibd_runner.py
    
    * Update coloc_ibd_runner.py
    
    * wip all purpose coloc runner
    
    * Update coloc_runner.py
    
    * unindent
    
    * add logging
    
    * change var name
    
    * add celltype and chrom annotations
    
    * parsing issue
    
    * fdebug
    
    * Update coloc_runner.py
    
    * key error
    
    * update gene name!
    
    * remove entries where beta == 0 and se == 0
    
    * lint and add 5e-8 check of gwas data
    
    * Update coloc_runner.py
    
    * sigstr and gwas filter
    
    * Update coloc_runner.py
    
    * sig str filter only
    
    * Create coloc_ukbb_runner.py
    
    * debug
    
    * looks ok to run
    
    * Update coloc_ukbb_runner.py
    
    * changing to chr-specific gwas files
    
    * lint
    
    * logic error
    
    * add sh script
    
    * add in more docu
    
    * Update generate_commands.sh
    
    * Create generate_commands_1.sh
    
    * Update generate_commands.sh
    
    * update annotations
    
    * Ukbb str parser (#239)
    
    * Create ukbb_str_snp_maker.py
    
    * Update ukbb_str_snp_maker.py
    
    * Update ukbb_str_snp_maker.py
    
    * Update ukbb_str_snp_maker.py
    
    * lint
    
    * delete redundant files
    
    * update annotation
    
    * Delete liftover.py
    
    * Liftover hg19 to hg38 (#234)
    
    * wip
    
    * Update liftover.py
    
    * Update liftover.py
    
    * Update liftover.py
    
    * topath
    
    * read in less columns to sped things up
    
    * try with albumin
    
    * do it for 44 files
    
    * Update liftover.py
    
    * Update liftover.py
    
    * oom
    
    * logic error
    
    ---------
    
    Co-authored-by: michael-harper <109899932+michael-harper@users.noreply.github.com>

--- str/associatr/plotters/qqplotter.py
+++ str/associatr/plotters/qqplotter.py
@@ -3,11 +3,11 @@
 This script plots a QQ plot of observed vs expected -log10(p-values) for each cell type.
 
 analysis-runner --dataset "bioheart" --description "plot qq plot" --access-level "test" \
-    --output-dir "str/associatr/bioheart_n990" --memory=32G \
+    --output-dir "str/associatr/tob_n1055_and_bioheart_n990" --memory=8G \
     qqplotter.py \
-    --input-dir=gs://cpg-bioheart-test/str/associatr/bioheart_n990/results/raw_pval_extractor \
+    --input-dir=gs://cpg-bioheart-test/str/associatr/tob_n1055_and_bioheart_n990/DL_random_model/raw_pval_extractor \
     --cell-types=CD4_TCM,CD4_Naive,CD4_TEM,CD4_CTL,CD4_Proliferating,CD4_TCM_permuted,NK,NK_CD56bright,NK_Proliferating,CD8_TEM,CD8_TCM,CD8_Proliferating,CD8_Naive,Treg,B_naive,B_memory,B_intermediate,Plasmablast,CD14_Mono,CD16_Mono,cDC1,cDC2,pDC,dnT,gdT,MAIT,ASDC,HSPC,ILC \
-    --title='associaTR BioHEART' --ylim=200
+    --title='associaTR BioHEART' --ylim=330
 
 """
 import click
@@ -44,6 +44,38 @@ def main(input_dir, cell_types, title, ylim):
         globals()[f'expected_log_pvals_{cell_type}'] = -np.log10(
             np.arange(1, globals()[f'n_{cell_type}'] + 1) / globals()[f'n_{cell_type}'],
         )
+
+    cell_type_mapping = {
+        'ASDC': 'ASDC',
+        'B_intermediate': 'B intermediate',
+        'B_memory': 'B memory',
+        'B_naive': 'B naive',
+        'CD14_Mono': 'CD14+ Monocyte',
+        'CD16_Mono': 'CD16+ Monocyte',
+        'CD4_CTL': 'CD4+ CTL',
+        'CD4_Naive': 'CD4+ Naive',
+        'CD4_Proliferating': 'CD4+ Proliferating',
+        'CD4_TCM': 'CD4+ TCM',
+        'CD4_TCM_permuted': 'Permuted control',
+        'CD4_TEM': 'CD4+ TEM',
+        'CD8_Naive': 'CD8+ Naive',
+        'CD8_Proliferating': 'CD8+ Proliferating',
+        'CD8_TCM': 'CD8+ TCM',
+        'CD8_TEM': 'CD8+ TEM',
+        'cDC1': 'cDC1',
+        'cDC2': 'cDC2',
+        'dnT': 'dnT',
+        'gdT': 'gdT',
+        'HSPC': 'HSPC',
+        'ILC': 'ILC',
+        'MAIT': 'MAIT',
+        'NK': 'NK',
+        'NK_CD56bright': 'NK CD56bright',
+        'NK_Proliferating': 'NK Proliferating',
+        'pDC': 'pDC',
+        'Plasmablast': 'Plasmablast',
+        'Treg': 'Treg',
+    }
 
     # Create QQ plot
     plt.figure(figsize=(10, 8))
@@ -92,25 +124,43 @@ def main(input_dir, cell_types, title, ylim):
 
     # Loop through each cell type and plot the scatter plot
     for i, cell_type in enumerate(cell_type_list):
+        output_label = cell_type_mapping.get(cell_type, cell_type)
         color_index = i % len(colors)  # Get the index of the color to use for the current cell type
         ax.scatter(
             expected_sorted_values[cell_type],
             observed_sorted_values[cell_type],
             color=colors[color_index],
-            label=cell_type,
+            label=output_label,
             s=9,
         )
 
-    ax.set_xlabel('Expected -log10(p-value)')
-    ax.set_ylabel('Observed -log10(p-value)')
-    ax.set_title(f'QQ Plot - {title}')
+    # Create a legend for all items except "permuted control"
+    handles, labels = ax.get_legend_handles_labels()
+    permuted_control_handle = [h for h, l in zip(handles, labels) if l == "Permuted control"]
+    other_handles = [h for h, l in zip(handles, labels) if l != "Permuted control"]
+    other_labels = [l for l in labels if l != "Permuted control"]
+    ax.add_artist(
+        ax.legend(
+            permuted_control_handle,
+            ['Permuted control'],
+            bbox_to_anchor=(1.05, 0),
+            loc='upper left',
+            fontsize=11,
+        ),
+    )
+
+    # Create the main legend with other items
+    ax.legend(other_handles, other_labels, bbox_to_anchor=(1.05, 1), loc='upper left', fontsize=11)
+
+    ax.set_xlabel('Expected -log10(p-value)', fontsize=14)
+    ax.set_ylabel('Observed -log10(p-value)', fontsize=14)
+    plt.xticks(fontsize=10)
+    plt.yticks(fontsize=10)
     ax.set_ylim(0, ylim)
 
-    ax.grid(True)
-    ax.legend(bbox_to_anchor=(1.05, 1), loc='upper left')
     ax.plot([0, 7], [0, 7], color='grey', linestyle='--')  # Add a reference line
 
-    gcs_output_path = output_path('summary_plots/qq_plot.png', 'analysis')
+    gcs_output_path = output_path('summary_plots/publish/v1/qq_plot.png', 'analysis')
     fig.tight_layout()
     fig.savefig('qqplot.png')
     hl.hadoop_copy('qqplot.png', gcs_output_path)
