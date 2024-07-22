c7f6d06571e94726f47bd0cfdcd9e8d4cfd3e2d1
hopedisastro <40783500+hopedisastro@users.noreply.github.com>
2024-07-23 08:14:45 +1000

    Create df_maker2.py

---
+++ str/fine-mapping/df_maker2.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+import click
+
+import hailtop.batch as hb
+
+from cpg_utils import to_path
+from cpg_utils.hail_batch import get_batch, output_path
+import pandas as pd
+
+"""
+analysis-runner --dataset "bioheart" --description "Create a dataframe from all cell types" --access-level "test" \
+    --output-dir "str/associatr/fine_mapping/susie_finemap" \
+    df_maker2.py
+
+"""
+
+def run_concatenator(cell):
+    """
+    Concatenate two dataframes together.
+    """
+    import pandas as pd
+
+    # read input files
+    dfs =[]
+    for chrom in range(1,23):
+        try:
+            files_list = list(to_path(f'gs://cpg-bioheart-test-analysis/str/associatr/fine_mapping/v3-ocv/susie/{cell}/chr{chrom}').glob('*.tsv'))
+        except:
+            continue
+        for file in files_list:
+            gene_name = str(file).split('/')[-1].split('_')[0]
+            data = pd.read_csv(file, sep = '\t')
+            data['celltype'] = cell
+            data['gene'] = gene_name
+            data = data[~data['motif'].str.contains('-')] # remove SNP rows
+            dfs.append(data)
+        print (f'Processed {cell} and chr{chrom}')
+    result_df = pd.concat(dfs, ignore_index=True)
+    result_df.to_csv(f'gs://cpg-bioheart-test-analysis/str/associatr/fine_mapping/v3-ocv/susie/{cell}/all_genes_strs.tsv', sep = '\t', index = False)
+
+def main():
+
+    cell_types = 'gdT,B_intermediate,ILC,Plasmablast,dnT,ASDC,cDC1,pDC,NK_CD56bright,MAIT,B_memory,CD4_CTL,CD4_Proliferating,CD8_Proliferating,HSPC,NK_Proliferating,cDC2,CD16_Mono,Treg,CD14_Mono,CD8_TCM,CD4_TEM,CD8_Naive,CD4_TCM,NK,CD8_TEM,CD4_Naive,B_naive'
+    b = get_batch()
+    # Split the string into a list of characters
+    cell_type_list = cell_types.split(',')
+    for cell in cell_type_list:
+        combiner_job = b.new_python_job(
+            f'Concatenate all genes for {cell}',
+        )
+        combiner_job.cpu(2)
+        combiner_job.call(run_concatenator, cell)
+    b.run(wait=False)
+
+
+
+if __name__ == '__main__':
+    main()
