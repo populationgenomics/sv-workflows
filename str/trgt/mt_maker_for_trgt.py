6dcd7a272319b8a40d42bb958a12c169e2c60df6
hopedisastro <40783500+hopedisastro@users.noreply.github.com>
2024-08-01 14:59:08 +1000

    Update mt_maker_for_trgt.py

--- str/trgt/mt_maker_for_trgt.py
+++ str/trgt/mt_maker_for_trgt.py
@@ -19,19 +19,29 @@ from cpg_utils.hail_batch import get_batch, init_batch, output_path
 
 
 def main():
-    init_batch(worker_memory = 'highmem')
+    init_batch(worker_memory='highmem')
 
-    mt = hl.read_matrix_table('gs://cpg-bioheart-test/str/wgs_genotyping/polymorphic_run_n2045/annotated_mt/v2/str_annotated.mt')
+    mt = hl.read_matrix_table(
+        'gs://cpg-bioheart-test/str/wgs_genotyping/polymorphic_run_n2045/annotated_mt/v2/str_annotated.mt',
+    )
 
     # Pull out the 25 samples with LR PacBio data
-    samples = hl.import_table('gs://cpg-bioheart-test/str/wgs_genotyping/trgt/mapper_trgt.csv', delimiter=',',key='FS_ID',quote=None,)
+    samples = hl.import_table(
+        'gs://cpg-bioheart-test/str/wgs_genotyping/trgt/mapper_trgt.csv',
+        delimiter=',',
+        key='FS_ID',
+        quote=None,
+    )
     cpg_ids = samples.s.collect()
     mt = mt.filter_cols(hl.literal(cpg_ids).contains(mt.s))
 
     # Pull out the rep lengths stored as allele_1_rep_length and allele_2_rep_length
-    mt = mt.annotate_entries(sr_summed_rep_length = mt.allele_1_rep_length + mt.allele_2_rep_length)
-    #extract as a table
-    mt.select_entries(mt.sr_summed_rep_length).write('gs://cpg-bioheart-test/str/wgs_genotyping/trgt/sr_rep_lengths.mt', overwrite=True)
+    mt = mt.annotate_entries(sr_summed_rep_length=mt.allele_1_rep_length + mt.allele_2_rep_length)
+    # extract as a table
+    mt.select_entries(mt.sr_summed_rep_length).write(
+        'gs://cpg-bioheart-test/str/wgs_genotyping/trgt/sr_rep_lengths.mt', overwrite=True,
+    )
+
 
 if __name__ == '__main__':
     main()
