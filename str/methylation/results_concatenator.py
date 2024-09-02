36b1ed50c5c5e76758fb43c7649b37597ce3024a
hopedisastro <40783500+hopedisastro@users.noreply.github.com>
2024-09-02 10:46:02 -0400

    try speeding up

--- str/methylation/results_concatenator.py
+++ str/methylation/results_concatenator.py
@@ -3,38 +3,41 @@
 This script concatenates the results from running associatr-methylation on a per-chromosome basis.
 The output is a single CSV file containing per chromosome results
 
-analysis-runner --dataset bioheart --access-level test --output-dir potato --description "Concatenate associatr-methylation results" python3 results_concatenator.py results_concatenator.py
+analysis-runner --dataset bioheart --access-level test --output-dir potato --description "Concatenate associatr-methylation results" python3 results_concatenator.py
 
 
 """
 
 import click
 import pandas as pd
-
+from concurrent.futures import ThreadPoolExecutor
 from cpg_utils import to_path
 from cpg_utils.hail_batch import get_batch
 
+def process_file(file):
+    df = pd.read_csv(file, sep='\t')
+    site_chr = str(file).split('/')[-1].split('_')[0]
+    site_pos = str(file).split('/')[-1].split('_')[1]
+    site = f'{site_chr}_{site_pos}'
+    old_p = f'p_{site}'
+    old_coeff = f'coeff_{site}'
+    old_se = f'se_{site}'
+
+    # Rename the columns to be more generic
+    df = df.rename(columns={old_p: 'p', old_coeff: 'coeff', old_se: 'se'})
+    df['methyl_site'] = site  # add a column for the Cpg site coordinate
+    return df
 
 def concatenator(input_dir, chrom):
     files = list(to_path(f'{input_dir}/chr{chrom}').glob('*.tsv'))
     master_df = pd.DataFrame()
-    for file in files:
-        df = pd.read_csv(file, sep='\t')
-        site_chr = str(file).split('/')[-1].split('_')[0]
-        site_pos = str(file).split('/')[-1].split('_')[1]
-        site = f'{site_chr}_{site_pos}'
-        old_p = f'p_{site}'
-        old_coeff = f'coeff_{site}'
-        old_se = f'se_{site}'
 
-        # Rename the columns to be more generic
-        df = df.rename(columns={old_p: 'p', old_coeff: 'coeff', old_se: 'se'})
+    # Use ThreadPoolExecutor to process files in parallel
+    with ThreadPoolExecutor() as executor:
+        results = list(executor.map(process_file, files))
 
-        df['methyl_site'] = site  # add a column for the Cpg site coordinate
-        if master_df.empty:
-            master_df = df
-        else:
-            master_df = pd.concat([master_df, df], ignore_index=True)
+    # Concatenate all DataFrames
+    master_df = pd.concat(results, ignore_index=True)
     output_gcs = to_path(f'{input_dir}/chr{chrom}_concatenated_results.tsv')
     master_df.to_csv(output_gcs, sep='\t', index=False, header=True)
 
