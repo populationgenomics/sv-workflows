5402b3ce9c9d88a3fc96c2b134427aa378be237d
hopedisastro <40783500+hopedisastro@users.noreply.github.com>
2024-09-02 14:25:28 -0400

    bump t0 500 jobs/batch

--- str/methylation/associatr_runner.py
+++ str/methylation/associatr_runner.py
@@ -45,11 +45,11 @@ def main():
 
         methyl_dir = get_config()['associatr']['pheno_cov_numpy_dir']
         site_numpy_list = list(to_path(f'{methyl_dir}/{chromosome}').glob('*.npy'))
-        for i in range(0, len(site_numpy_list), 150):
+        for i in range(0, len(site_numpy_list), 500):
             _dependent_jobs = []
             reset_batch()
-            batch_gene_files = site_numpy_list[i : i + 150]
-            b = get_batch(name='Run associatr-methylation chr' + chromosome + ' sites ' + str(i) + '-' + str(i + 150))
+            batch_gene_files = site_numpy_list[i : i + 500]
+            b = get_batch(name='Run associatr-methylation chr' + chromosome + ' sites ' + str(i) + '-' + str(i + 500))
             variant_vcf = b.read_input_group(
                 **dict(
                     vcf=vcf_file_path,
