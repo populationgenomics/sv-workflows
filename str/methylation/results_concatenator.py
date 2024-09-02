38caab6a80566b4633ba4b5fffcb34e151118ecb
hopedisastro <40783500+hopedisastro@users.noreply.github.com>
2024-09-02 10:28:26 -0400

    Create results_concatenator.py

---
+++ str/methylation/results_concatenator.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+"""
+This script concatenates the results from running associatr-methylation on a per-chromosome basis.
+The output is a single CSV file containing per chromosome results
+
+analysis-runner --dataset bioheart --access-level test --output-dir potato --description "Concatenate associatr-methylation results" python3 results_concatenator.py results_concatenator.py
+
+
+"""
+
+import click
+import pandas as pd
+
+from cpg_utils import to_path
+from cpg_utils.hail_batch import get_batch
+
+
+def concatenator(input_dir, chrom):
+    files = list(to_path(f'{input_dir}/chr{chrom}').glob('*.tsv'))
+    master_df = pd.DataFrame()
+    for file in files:
+        df = pd.read_csv(file, sep='\t')
+        site_chr = str(file).split('/')[-1].split('_')[0]
+        site_pos = str(file).split('/')[-1].split('_')[1]
+        site = f'{site_chr}_{site_pos}'
+        old_p = f'p_{site}'
+        old_coeff = f'coeff_{site}'
+        old_se = f'se_{site}'
+
+        # Rename the columns to be more generic
+        df = df.rename(columns={old_p: 'p', old_coeff: 'coeff', old_se: 'se'})
+
+        df['methyl_site'] = site  # add a column for the Cpg site coordinate
+        if master_df.empty:
+            master_df = df
+        else:
+            master_df = pd.concat([master_df, df], ignore_index=True)
+    output_gcs = to_path(f'{input_dir}/chr{chrom}_concatenated_results.tsv')
+    master_df.to_csv(output_gcs, sep='\t', index=False, header=True)
+
+
+@click.option(
+    '--input-dir',
+    help='GCS path to the directory containing the associatr-methylation results',
+    default='gs://cpg-bioheart-test-analysis/str/associatr-methylation/bioheart_n25/5kb/results',
+)
+@click.option('--chromosomes', help='Comma-separated list of chromosomes to concatenate', default='22')
+@click.command()
+def main(input_dir, chromosomes):
+    b = get_batch(name='Concatenate associatr-methylation results')
+    for chrom in chromosomes.split(','):
+        j = b.new_python_job(
+            name=f'Concatenate associatr-methylation results for chr{chrom}',
+        )
+        j.cpu(1)
+        j.storage('5G')
+
+        j.call(
+            concatenator,
+            input_dir,
+            chrom,
+        )
+
+    b.run(wait=False)
+
+
+if __name__ == '__main__':
+    main()  # pylint: disable=no-value-for-parameter
