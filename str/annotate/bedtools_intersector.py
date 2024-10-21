5431fe56e52a9d29be240113c1815837bca7c93c
hopedisastro <40783500+hopedisastro@users.noreply.github.com>
2024-10-21 10:37:47 -0400

    catalog

--- str/annotate/bedtools_intersector.py
+++ str/annotate/bedtools_intersector.py
@@ -13,7 +13,7 @@ from cpg_utils.config import get_config
 from cpg_utils.hail_batch import get_batch, output_path
 
 CATALOG_PATH = 'gs://cpg-bioheart-test/str/ncAnnot.v0.14.jul2024.bed'
-TR_PATH = 'gs://cpg-bioheart-test/str/arthur/eTRs.bed'
+TR_PATH = 'gs://cpg-bioheart-test/str/arthur/catalog.bed'
 
 
 # Initializing Batch
@@ -33,6 +33,6 @@ tr = b.read_input(TR_PATH)
 bedtools_job.command(f'bedtools intersect -a {tr} -b {catalog} -wo > {bedtools_job.ofile}')
 
 # write output to GCP bucket for this dataset
-b.write_output(bedtools_job.ofile, 'gs://cpg-bioheart-test/str/arthur/eTRs_intersect.bed')
+b.write_output(bedtools_job.ofile, 'gs://cpg-bioheart-test/str/arthur/catalog_intersect.bed')
 
 b.run(wait=False)
