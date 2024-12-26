4bee5d4ab86589b9233da471583118a7bfba5b23
hopedisastro <40783500+hopedisastro@users.noreply.github.com>
2024-12-26 15:23:53 +1100

    Update lead_tr_snv_debug.py

--- str/helper/lead_tr_snv_debug.py
+++ str/helper/lead_tr_snv_debug.py
@@ -37,132 +37,116 @@ def genes_parser(
 
     genes_with_lead_tr = []
     chromosome = f'chr{chromosome}'
-    for gene_file in ['gs://cpg-bioheart-test-analysis/tenk10k/str/associatr/final_freeze/snps_and_strs/bioheart_n975_and_tob_n950/rm_str_indels_dup_strs/meta_results/gdT/chr16/ENSG00000155592_100000bp_meta_results.tsv']:
-        eqtl_results = pd.read_csv(
-            gene_file,
-            sep='\t',)
-        gene_name = str(gene_file).split('/')[-1].split('_')[0]
+    gene_file = 'gs://cpg-bioheart-test-analysis/tenk10k/str/associatr/final_freeze/snps_and_strs/bioheart_n975_and_tob_n950/rm_str_indels_dup_strs/meta_results/gdT/chr16/ENSG00000155592_100000bp_meta_results.tsv'
+    eqtl_results = pd.read_csv(
+        gene_file,
+        sep='\t',)
+    gene_name = str(gene_file).split('/')[-1].split('_')[0]
 
-        # get row(s) with minimum p-value
-        min_pval = eqtl_results['pval_meta'].min()
-        smallest_pval_rows = eqtl_results[eqtl_results['pval_meta'] == min_pval]
-        # check if all rows are SNPs:
-        at_least_one_lead_SNV = smallest_pval_rows['motif'].str.contains('-').any()
-        if at_least_one_lead_SNV:
-            snv_meta_results = eqtl_results[eqtl_results['motif'].str.contains('-')]  # filter for SNVs
-            lead_snv = snv_meta_results[
-                snv_meta_results['pval_meta'] == snv_meta_results['pval_meta'].min()
-            ]  # get the lead SNV
-            lead_snv_coord = chromosome + ':' + str(lead_snv.iloc[0]['pos'])
-            lead_snv_motif = lead_snv.iloc[0]['motif']
+    # get row(s) with minimum p-value
+    min_pval = eqtl_results['pval_meta'].min()
+    smallest_pval_rows = eqtl_results[eqtl_results['pval_meta'] == min_pval]
+    # check if all rows are SNPs:
+    at_least_one_lead_SNV = smallest_pval_rows['motif'].str.contains('-').any()
+    if at_least_one_lead_SNV:
+        snv_meta_results = eqtl_results[eqtl_results['motif'].str.contains('-')]  # filter for SNVs
+        lead_snv = snv_meta_results[
+            snv_meta_results['pval_meta'] == snv_meta_results['pval_meta'].min()
+        ]  # get the lead SNV
+        lead_snv_coord = chromosome + ':' + str(lead_snv.iloc[0]['pos'])
+        lead_snv_motif = lead_snv.iloc[0]['motif']
 
-            proxy_snv_results = snv_meta_results[snv_meta_results.index != lead_snv.index[0]]
-            proxy_snv = proxy_snv_results[proxy_snv_results['pval_meta'] == proxy_snv_results['pval_meta'].min()]
-            proxy_snv_coord = f"{chromosome}:{proxy_snv.iloc[0]['pos']}"
-            proxy_snv_motif = proxy_snv.iloc[0]['motif']
-            distance = abs(int(lead_snv.iloc[0]['pos']) - int(proxy_snv.iloc[0]['pos']))
+        proxy_snv_results = snv_meta_results[snv_meta_results.index != lead_snv.index[0]]
+        proxy_snv = proxy_snv_results[proxy_snv_results['pval_meta'] == proxy_snv_results['pval_meta'].min()]
+        proxy_snv_coord = f"{chromosome}:{proxy_snv.iloc[0]['pos']}"
+        proxy_snv_motif = proxy_snv.iloc[0]['motif']
+        distance = abs(int(lead_snv.iloc[0]['pos']) - int(proxy_snv.iloc[0]['pos']))
 
-            # Extract the genotypes for lead SNV in the cis window
-            lead_df = pd.DataFrame(columns=['individual'])
-            snp_vcf_1 = VCF(snp_input['vcf'])
-            lead_df['individual'] = snp_vcf_1.samples
+        # Extract the genotypes for lead SNV in the cis window
+        lead_df = pd.DataFrame(columns=['individual'])
+        snp_vcf_1 = VCF(snp_input['vcf'])
+        lead_df['individual'] = snp_vcf_1.samples
 
-            for variant in snp_vcf_1(lead_snv_coord):
-                if str(variant.INFO.get('RU')) == lead_snv_motif:
-                    gt = variant.gt_types  # extracts GTs as a numpy array
-                    gt[gt == 3] = 2
-                    snp = variant.CHROM + '_' + str(variant.POS) + '_' + lead_snv_motif
-                    df_to_append = pd.DataFrame(gt, columns=[snp])  # creates a temp df to store the GTs for one locus
-                    lead_df = pd.concat([lead_df, df_to_append], axis=1)
-                    break
-                print('Finished reading SNP VCF')
-
-        else:
-            lead_tr = eqtl_results[eqtl_results['pval_meta'] == min_pval]
-            lead_tr_coord = chromosome + ':' + str(lead_tr.iloc[0]['pos'])
-            lead_tr_motif = lead_tr.iloc[0]['motif']
-
-            proxy_snv_results = eqtl_results[eqtl_results.index != lead_tr.index[0]]  # filter out lead tr
-            proxy_snv_results = proxy_snv_results[proxy_snv_results['motif'].str.contains('-')]  # filter for SNVs
-            proxy_snv = proxy_snv_results[proxy_snv_results['pval_meta'] == proxy_snv_results['pval_meta'].min()]
-            proxy_snv_coord = f"{chromosome}:{proxy_snv.iloc[0]['pos']}"
-            proxy_snv_motif = proxy_snv.iloc[0]['motif']
-            distance = abs(int(lead_tr.iloc[0]['pos']) - int(proxy_snv.iloc[0]['pos']))
-
-            # Extract the genotypes for STRs in the cis window
-            lead_df = pd.DataFrame(columns=['individual'])
-            str_vcf = VCF(str_input['vcf'])
-            lead_df['individual'] = str_vcf.samples
-            print('Starting to subset STR VCF for window...')
-            for variant in str_vcf(lead_tr_coord):
-                if str(variant.INFO.get('RU')) == lead_tr_motif:
-                    genotypes = variant.format('REPCN')
-                    # Replace '.' with '-99/-99' to handle missing values
-                    genotypes = np.where(genotypes == '.', '-99/-99', genotypes)
-
-                    # Split each element by '/'
-                    split_genotypes = [genotype.split('/') for genotype in genotypes]
-
-                    # Convert split_genotypes into a numpy array for easier manipulation
-                    split_genotypes_array = np.array(split_genotypes)
-
-                    # Convert the strings to integers and sum them row-wise
-                    sums = np.sum(split_genotypes_array.astype(int), axis=1)
-                    # set dummy -198 value to np.nan
-                    sums = np.where(sums == -198, np.nan, sums)
-                    snp = variant.CHROM + '_' + str(variant.POS) + '_' + str(variant.INFO.get('RU'))
-
-                    lead_df[snp] = sums
-                    break
-
-        # Extract the genotypes for proxy SNV in the cis window
-        proxy_df = pd.DataFrame(columns=['individual'])
-        snp_vcf_2 = VCF(snp_input['vcf'])
-        proxy_df['individual'] = snp_vcf_2.samples
-
-        for variant in snp_vcf_2(proxy_snv_coord):
-            if str(variant.INFO.get('RU')) == proxy_snv_motif:
-                gt = variant.gt_types
+        for variant in snp_vcf_1(lead_snv_coord):
+            if str(variant.INFO.get('RU')) == lead_snv_motif:
+                gt = variant.gt_types  # extracts GTs as a numpy array
                 gt[gt == 3] = 2
-                snp = variant.CHROM + '_' + str(variant.POS) + '_' + proxy_snv_motif
-                df_to_append = pd.DataFrame(gt, columns=[snp])
-                proxy_df = pd.concat([proxy_df, df_to_append], axis=1)
+                snp = variant.CHROM + '_' + str(variant.POS) + '_' + lead_snv_motif
+                df_to_append = pd.DataFrame(gt, columns=[snp])  # creates a temp df to store the GTs for one locus
+                lead_df = pd.concat([lead_df, df_to_append], axis=1)
                 break
             print('Finished reading SNP VCF')
 
-        # calculate LD
-        # merge the STR and SNP GT dfs together
-        print("Lead df:")
-        print(lead_df)
-        print("Proxy df:")
-        print(proxy_df)
-        merged_df = lead_df.merge(proxy_df, on='individual')
-        print("Merged df:")
-        print(merged_df)
+    else:
+        lead_tr = eqtl_results[eqtl_results['pval_meta'] == min_pval]
+        lead_tr_coord = chromosome + ':' + str(lead_tr.iloc[0]['pos'])
+        lead_tr_motif = lead_tr.iloc[0]['motif']
 
-        # merged_df has only two columns - calculate the correlation
-        try:
-            correlation = merged_df.drop(columns='individual').corr().iloc[0, 1]
-        except:
-            print(correlation)
-            print(gene_name)
+        proxy_snv_results = eqtl_results[eqtl_results.index != lead_tr.index[0]]  # filter out lead tr
+        proxy_snv_results = proxy_snv_results[proxy_snv_results['motif'].str.contains('-')]  # filter for SNVs
+        proxy_snv = proxy_snv_results[proxy_snv_results['pval_meta'] == proxy_snv_results['pval_meta'].min()]
+        proxy_snv_coord = f"{chromosome}:{proxy_snv.iloc[0]['pos']}"
+        proxy_snv_motif = proxy_snv.iloc[0]['motif']
+        distance = abs(int(lead_tr.iloc[0]['pos']) - int(proxy_snv.iloc[0]['pos']))
+
+        # Extract the genotypes for STRs in the cis window
+        lead_df = pd.DataFrame(columns=['individual'])
+        str_vcf = VCF(str_input['vcf'])
+        lead_df['individual'] = str_vcf.samples
+        print('Starting to subset STR VCF for window...')
+        for variant in str_vcf(lead_tr_coord):
+            if str(variant.INFO.get('RU')) == lead_tr_motif:
+                genotypes = variant.format('REPCN')
+                # Replace '.' with '-99/-99' to handle missing values
+                genotypes = np.where(genotypes == '.', '-99/-99', genotypes)
+
+                # Split each element by '/'
+                split_genotypes = [genotype.split('/') for genotype in genotypes]
+
+                # Convert split_genotypes into a numpy array for easier manipulation
+                split_genotypes_array = np.array(split_genotypes)
+
+                # Convert the strings to integers and sum them row-wise
+                sums = np.sum(split_genotypes_array.astype(int), axis=1)
+                # set dummy -198 value to np.nan
+                sums = np.where(sums == -198, np.nan, sums)
+                snp = variant.CHROM + '_' + str(variant.POS) + '_' + str(variant.INFO.get('RU'))
+
+                lead_df[snp] = sums
+                break
+
+    # Extract the genotypes for proxy SNV in the cis window
+    proxy_df = pd.DataFrame(columns=['individual'])
+    snp_vcf_2 = VCF(snp_input['vcf'])
+    proxy_df['individual'] = snp_vcf_2.samples
+
+    for variant in snp_vcf_2(proxy_snv_coord):
+        if str(variant.INFO.get('RU')) == proxy_snv_motif:
+            gt = variant.gt_types
+            gt[gt == 3] = 2
+            snp = variant.CHROM + '_' + str(variant.POS) + '_' + proxy_snv_motif
+            df_to_append = pd.DataFrame(gt, columns=[snp])
+            proxy_df = pd.concat([proxy_df, df_to_append], axis=1)
             break
+        print('Finished reading SNP VCF')
 
+    # calculate LD
+    # merge the STR and SNP GT dfs together
+    print("Lead df:")
+    print(lead_df)
+    print("Proxy df:")
+    print(proxy_df)
+    merged_df = lead_df.merge(proxy_df, on='individual')
+    print("Merged df:")
+    print(merged_df)
 
-        # save correlation and pval ratio to a df
-        results_df = pd.DataFrame(
-            {
-                'correlation': [correlation],
-                'proxy_pval': [proxy_snv.iloc[0]['pval_meta']],
-                'lead_pval': [min_pval],
-                'lead_snv_boolean': [at_least_one_lead_SNV],
-                'cell_type': [cell_type],
-                'gene': [gene_name],
-                'distance': [distance],
-            },
-        )
+    # merged_df has only two columns - calculate the correlation
+    try:
+        correlation = merged_df.drop(columns='individual').corr().iloc[0, 1]
+    except:
+        print(merged_df)
+        print(gene_name)
 
-        max_corr_master_df = pd.concat([max_corr_master_df, results_df], axis=0)
 
 
 
