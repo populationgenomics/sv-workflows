d962b607e64ff3fbe1cc82f75af87f11271dd74a
hopedisastro <40783500+hopedisastro@users.noreply.github.com>
2024-12-27 13:24:13 +1100

    Update susie_parser.py

--- str/helper/susie_parser.py
+++ str/helper/susie_parser.py
@@ -13,6 +13,9 @@ for cell in cell_type_list:
     data = pd.read_csv(files_list)
     data['celltype'] = cell
     data['gene'] = gene_name
+    data = data[data['susie_pip']>=0.7]
     dfs.append(data)
 result_df = pd.concat(dfs, ignore_index=True)
-result_df.to_csv('gs://cpg-bioheart-test-analysis/tenk10k/str/associatr/final_freeze/fine_mapping/susie/all_cells_all_genes.csv', index = False)
+result_df.to_csv('gs://cpg-bioheart-test-analysis/tenk10k/str/associatr/final_freeze/fine_mapping/susie/all_cells_all_genes_pip_0.7x.csv', index = False)
+
+
