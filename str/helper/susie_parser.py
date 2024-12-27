1123d4536168b66c4333b6f24fe303b1aae83bb9
hopedisastro <40783500+hopedisastro@users.noreply.github.com>
2024-12-27 12:25:03 +1100

    Update susie_parser.py

--- str/helper/susie_parser.py
+++ str/helper/susie_parser.py
@@ -8,12 +8,11 @@ cell_types = 'B_intermediate,ILC,Plasmablast,ASDC,cDC1,pDC,NK_CD56bright,MAIT,B_
 cell_type_list = cell_types.split(',')
 dfs =[]
 for cell in cell_type_list:
-    files_list = list(to_path(f'gs://cpg-bioheart-test-analysis/tenk10k/str/associatr/final_freeze/fine_mapping/susie/{cell}').rglob('*.tsv'))
-    for file in files_list:
-        gene_name = str(file).split('/')[-1].split('_')[0]
-        data = pd.read_csv(file, sep = '\t')
-        data['celltype'] = cell
-        data['gene'] = gene_name
-        dfs.append(data)
+    files_list = f'gs://cpg-bioheart-test-analysis/tenk10k/str/associatr/final_freeze/fine_mapping/susie/{cell}/all_genes.csv'
+    gene_name = str(files_list).split('/')[-1].split('_')[0]
+    data = pd.read_csv(files_list)
+    data['celltype'] = cell
+    data['gene'] = gene_name
+    dfs.append(data)
 result_df = pd.concat(dfs, ignore_index=True)
 result_df.to_csv('gs://cpg-bioheart-test-analysis/tenk10k/str/associatr/final_freeze/fine_mapping/susie/all_cells_all_genes.csv', index = False)
