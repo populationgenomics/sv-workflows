d79d6e3d36cf3035f636ebdfac62336c6a6318ff
hopedisastro <40783500+hopedisastro@users.noreply.github.com>
2024-12-28 09:03:37 +1100

    Update bgzip_tabix.py

--- str/helper/bgzip_tabix.py
+++ str/helper/bgzip_tabix.py
@@ -4,10 +4,10 @@
 This script receives a VCF file and performs BGZIP and TABIX
 
 analysis-runner --access-level test --dataset bioheart --description  \
-    'VCF combiner'  --output-dir 'str/associatr/common_variant_snps' \
+    'VCF combiner'  --output-dir 'tenk10k/str/associatr/final_freeze/methyl_eqtl/input_files' \
     bgzip_tabix.py \
-    --input-dir=gs://cpg-bioheart-test/str/associatr/common_variants_snps \
-    --chromosomes=20
+    --input-vcf=gs://gs://cpg-bioheart-test/tenk10k/str/associatr/final_freeze/methyl_eqtl/input_files/finemapped_etrs.vcf
+
 """
 
 import click
@@ -21,48 +21,44 @@ BCFTOOLS_IMAGE = config['images']['bcftools']
 
 
 @click.command()
-@click.option('--input-dir', help='Input directory for VCF files')
-@click.option(
-    '--chromosomes',
-    default='1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22',
-    help='Chromosome number',
-)
+@click.option('--input-vcf', help='Input directory for VCF files')
+
 @click.option('--job-memory', default='4G', help='Job memory')
 @click.option('--job-storage', default='10G', help='Job storage')
 @click.option('--job-cpu', default=1, help='Job CPU')
-def main(input_dir, chromosomes, job_memory, job_storage, job_cpu):
+def main(input_vcf, chromosomes, job_memory, job_storage, job_cpu):
     """
     BGZIPs and TABIX a GZIPPED/unzipped VCF input file and writes it to a GCS bucket
 
     """
     b = get_batch()
-    for chrom in chromosomes.split(','):
-        input_file = f'{input_dir}/hail_filtered_chr{chrom}.vcf'
-        vcf_input = b.read_input(input_file)
-        input_file_name = (input_file.split('/')[-1]).split('.')[0]
 
-        bcftools_job = b.new_job(name=f'{input_file_name} BGZIPPING')
-        bcftools_job.image(BCFTOOLS_IMAGE)
-        bcftools_job.memory(job_memory)
-        bcftools_job.storage(job_storage)
-        bcftools_job.cpu(job_cpu)
 
-        bcftools_job.declare_resource_group(
-            vcf_output={
-                'vcf.bgz': '{root}.vcf.bgz',
-                'vcf.bgz.tbi': '{root}.vcf.bgz.tbi',
-            },
-        )
+    vcf_input = b.read_input(input_vcf)
+    input_file_name = (input_vcf.split('/')[-1]).split('.')[0]
 
-        bcftools_job.command(
-            f"""
+    bcftools_job = b.new_job(name=f'{input_file_name} BGZIPPING')
+    bcftools_job.image(BCFTOOLS_IMAGE)
+    bcftools_job.memory(job_memory)
+    bcftools_job.storage(job_storage)
+    bcftools_job.cpu(job_cpu)
 
-            bcftools view {vcf_input} | bgzip -c >  {bcftools_job.vcf_output['vcf.bgz']}
-            tabix -f -p vcf {bcftools_job.vcf_output['vcf.bgz']}  > {bcftools_job.vcf_output['vcf.bgz.tbi']}
+    bcftools_job.declare_resource_group(
+        vcf_output={
+            'vcf.bgz': '{root}.vcf.bgz',
+            'vcf.bgz.tbi': '{root}.vcf.bgz.tbi',
+        },
+    )
 
-            """,
-        )
-        b.write_output(bcftools_job.vcf_output, (output_path(f'{input_file_name}')))
+    bcftools_job.command(
+        f"""
+
+        bcftools view {vcf_input} | bgzip -c >  {bcftools_job.vcf_output['vcf.bgz']}
+        tabix -f -p vcf {bcftools_job.vcf_output['vcf.bgz']}  > {bcftools_job.vcf_output['vcf.bgz.tbi']}
+
+        """,
+    )
+    b.write_output(bcftools_job.vcf_output, (output_path(f'{input_file_name}')))
 
     b.run(wait=False)
 
