0b5147d6d3d00bde89eb83d83012c0595f4836c5
hopedisastro <40783500+hopedisastro@users.noreply.github.com>
2025-05-20 17:00:34 +1000

    Update results_concatenator.py

--- str/atac/results_concatenator.py
+++ str/atac/results_concatenator.py
@@ -4,7 +4,7 @@ This script concatenates the results from running associatr-atac on a cell type 
 The output is a single CSV file containing per cell type results
 
 analysis-runner --dataset bioheart --access-level test --output-dir str/associatr-atac/tob/input_files/10kb_estrs/v1 --description "Concatenate associatr-atac results" python3 results_concatenator.py \
-    --celltypes "ASDC"
+    --celltypes "CD14_Mono_permuted,CD14_Mono,ASDC"
 
 
 """
@@ -47,7 +47,7 @@ def concatenator(input_dir, cell_type):
 @click.option(
     '--input-dir',
     help='GCS path to the directory containing the associatr-methylation results',
-    default='gs://cpg-bioheart-test-analysis/str/associatr-atac/tob/input_files/10kb_estrs/v1/results',
+    default='gs://cpg-bioheart-test-analysis/str/associatr-atac/tob/input_files/10kb_estrs/v1-mac-5/results',
 )
 @click.option('--celltypes', help='Comma-separated list of cell types to concatenate')
 @click.command()
