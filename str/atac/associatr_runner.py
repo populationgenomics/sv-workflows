251c3f050dd574901f10d223e6f0fd1485eb124b
hopedisastro <40783500+hopedisastro@users.noreply.github.com>
2025-05-20 19:07:39 +1000

    Update associatr_runner.py

--- str/atac/associatr_runner.py
+++ str/atac/associatr_runner.py
@@ -6,7 +6,7 @@ This script runs associaTR on atac data.
  analysis-runner --dataset "bioheart" --config associatr_runner.toml \
     --description "run associatr" \
     --access-level "test" \
-    --output-dir "str/associatr-atac/tob/input_files/10kb_estrs/v1-mac-5" \
+    --output-dir "str/associatr-atac/tob/input_files/10kb_estrs/v1-mac-20" \
      python3 associatr_runner.py
 
 
