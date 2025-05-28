4342ad79051cf0f679a6c5c9f9353ac950efe2b5
hopedisastro <40783500+hopedisastro@users.noreply.github.com>
2025-05-28 14:30:12 +1000

    going to try it on a gene level instead

--- str/fine-mapping/mcv/files_prep_dosages.py
+++ str/fine-mapping/mcv/files_prep_dosages.py
@@ -13,6 +13,7 @@ from cpg_utils.hail_batch import get_batch, output_path
 import pandas as pd
 from cpg_utils import to_path
 
+
 def tr_extract_genotype_matrix(vcf_path, chrom, start, end):
     """
     Extracts the genotype matrix for tandem repeats from a VCF file.
@@ -94,60 +95,57 @@ def snp_extract_genotype_matrix(vcf_path, chrom, start, end):
     return df
 
 
-def dosages(df, snp_input, tr_input):
+def dosages(gene, snp_input, tr_input):
     import pandas as pd
     import numpy as np
     from cpg_utils.hail_batch import output_path
 
     """
     Extracts the dosage files for each gene in the df DataFrame.
-    """""
+    """ ""
 
     # === LOAD metadata ===
     gene_info = pd.read_csv('gs://cpg-bioheart-test/tenk10k/saige-qtl/300libraries_n1925_adata_raw_var.csv')
-    for row in df.itertuples(): #for every row ie for every gene - pull out the variants GT in the cis window.
 
-        gene_name = row.gene_name
+    gene_ensg = gene
+    gene_info_filtered = gene_info[gene_info['gene_ids'] == gene_ensg]
+    chrom = gene_info_filtered.iloc[0]['chr']
+    start = int(gene_info_filtered.iloc[0]['start'])
+    end = int(gene_info_filtered.iloc[0]['end']) + 100_000
+    start_body = max(0, start - 100_000)
 
-        gene_ensg = gene_name
-        gene_info_filtered = gene_info[gene_info['gene_ids'] == gene_ensg]
-        chrom = gene_info_filtered.iloc[0]['chr']
-        start = int(gene_info_filtered.iloc[0]['start'])
-        end = int(gene_info_filtered.iloc[0]['end']) + 100_000
-        start_body = max(0, start - 100_000)
+    tr_df = tr_extract_genotype_matrix(tr_input['vcf'], chrom, start_body, end)
+    snp_df = snp_extract_genotype_matrix(snp_input['vcf'], chrom, start_body, end)
+    variant_df = tr_df.merge(snp_df)
+    variant_df['sample'] = variant_df['sample'].astype(float)
 
-        tr_df = tr_extract_genotype_matrix(tr_input['vcf'], chrom, start_body, end)
-        snp_df = snp_extract_genotype_matrix(snp_input['vcf'], chrom, start_body, end)
-        variant_df = tr_df.merge(snp_df)
-        variant_df['sample'] = variant_df['sample'].astype(float)
-
-        # Save files
-        variant_df.to_csv(output_path(f"dosages/{gene_ensg}_dosages.csv"), header=True)
+    # Save files
+    variant_df.to_csv(output_path(f"dosages/{gene_ensg}_dosages.csv"), header=True)
 
 
 @click.option('--estrs-path', type=str, required=True, help='Path to the estrs file.')
 @click.command()
 def main(estrs_path):
-
-
     b = get_batch(name='Residualized files prep for SuSie MCV')
     df = pd.read_csv(estrs_path)
     df = df.drop_duplicates(subset=['gene_name', 'chr'])
     # sort by chromosome
     for chrom in df['chr'].unique():
         df_chr = df[df['chr'] == chrom]
+        for gene in df_chr['gene_name'].unique():
+            tr_vcf_file = f'gs://cpg-bioheart-test/tenk10k/str/associatr/final-freeze/input_files/tr_vcf/v1-chr-specific/hail_filtered_{chrom}.vcf.bgz'
+            snp_vcf_file = (
+                f'gs://cpg-bioheart-test/tenk10k/str/associatr/common_variant_snps/hail_filtered_{chrom}.vcf.bgz'
+            )
 
-        tr_vcf_file = f'gs://cpg-bioheart-test/tenk10k/str/associatr/final-freeze/input_files/tr_vcf/v1-chr-specific/hail_filtered_{chrom}.vcf.bgz'
-        snp_vcf_file = f'gs://cpg-bioheart-test/tenk10k/str/associatr/common_variant_snps/hail_filtered_{chrom}.vcf.bgz'
+            snp_input = get_batch().read_input_group(**{'vcf': snp_vcf_file, 'tbi': snp_vcf_file + '.tbi'})
+            tr_input = get_batch().read_input_group(**{'vcf': tr_vcf_file, 'tbi': tr_vcf_file + '.tbi'})
 
-        snp_input = get_batch().read_input_group(**{'vcf': snp_vcf_file, 'tbi': snp_vcf_file + '.tbi'})
-        tr_input = get_batch().read_input_group(**{'vcf': tr_vcf_file, 'tbi': tr_vcf_file + '.tbi'})
-
-        j = b.new_python_job(f'Prepare for {chr}')
-        j.cpu(0.25)
-        j.storage('5G')
-        j.call(dosages, df_chr, snp_input, tr_input)
-        break # try only one chromosome for now
+            j = b.new_python_job(name=f'Prepare for {chr} and {gene}')
+            j.cpu(0.25)
+            j.storage('5G')
+            j.call(dosages, gene, snp_input, tr_input)
+        break  # try only one chromosome for now
     b.run(wait=False)
 
 
