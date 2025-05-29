e0a1f055afea4dad48645c2b079356751e772ae1
hopedisastro <40783500+hopedisastro@users.noreply.github.com>
2025-05-29 10:16:08 +1000

    debug

--- str/fine-mapping/mcv/susie_mcv_runner.py
+++ str/fine-mapping/mcv/susie_mcv_runner.py
@@ -63,66 +63,11 @@ def susie_runner(input_dir, gene, cell_type, num_causal_variants, num_iterations
     susie_fit <- susie(X, y_r, L = num_causal_variants, max_iter = num_iterations)
     print(susie_fit)
 
-    # extract CS membership and purity
-    susie_cs <- susie_get_cs(susie_fit, X = X)
-
-
-    # initialize annotation vectors
-    n_variants <- length(variant_ids)
-    cs_id <- rep(NA_integer_, n_variants)
-    cs_size <- rep(NA_integer_, n_variants)
-    max_pip_in_cs <- rep(NA_real_, n_variants)
-
-    # Loop over credible sets
-    for (i in seq_along(susie_cs$cs)) {
-    cs_variants <- susie_cs$cs[[i]]
-    purity <- susie_cs$purity[[i]]
-    max_pip <- max(susie_fit$pip[cs_variants], na.rm = TRUE)
-
-    cs_id[cs_variants] <- i
-    cs_size[cs_variants] <- length(cs_variants)
-    max_pip_in_cs[cs_variants] <- max_pip
-    }
-
-
-    # Create a data frame with PIP and CS information
-    pip_df <- data.frame(
-    variant_id = variant_ids,
-    pip = susie_fit$pip,
-    pip_prune = susie_get_pip(susie_fit, prune_by_cs = TRUE),
-    cs_id = cs_id,
-    cs_size = cs_size,
-    max_pip_in_cs = max_pip_in_cs
-    )
-
-     # Capture the summary outputs (try catch, in case it is empty)
-    p4 <- tryCatch({
-
-    final_df <- merge(pip_df, coord_df, by = "variant_id")
-    final_df <- final_df[order(final_df$chr, final_df$pos), ]
-
-    }, error = function(e) {
-
-    print(paste("An error occurred:", e))
-    0
-    })
-
-
 
     ''',
     )
 
-    # convert to pandas df
-    with (ro.default_converter + pandas2ri.converter).context():
-        susie_output_df = ro.conversion.get_conversion().rpy2py(ro.r('final_df'))
-    print('converted back to pandas df')
 
-    # write dataframe to GCS
-    susie_output_df.to_csv(
-        output_path(f"susie/{cell_type}/{gene}_100kb.tsv", 'analysis'),
-        sep='\t',
-        index=False,
-    )
 
 
 @click.option('--table-s1-path', help='Table S1 with eGenes to run SusieR on')
