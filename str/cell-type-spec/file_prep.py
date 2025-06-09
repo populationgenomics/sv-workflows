8fdc6f99d7fbd170be47cef37df61226a6292575
hopedisastro <40783500+hopedisastro@users.noreply.github.com>
2025-06-09 13:22:06 +1000

    Update file_prep.py

--- str/cell-type-spec/file_prep.py
+++ str/cell-type-spec/file_prep.py
@@ -85,7 +85,7 @@ def meta_eqt_file_prep(cell_type_eqtls, cell_type, associatr_dir):
                     continue
                 except IndexError:
                     logging.info(f'Index error for {gene} and {cell_type2}')
-                    continue
+                    break
 
     o_file_path = output_path(f'prep_files/{cell_type}/meta_input_df.csv')
     o_file_path_opposite = output_path(f'prep_files/{cell_type}/opposite_signed_betas.csv')
