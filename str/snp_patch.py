72646ace9abe07e0dfc07304005bbe2301ac25c8
hopedisastro <40783500+hopedisastro@users.noreply.github.com>
2025-06-17 11:10:32 +1000

    Update snp_patch.py

--- str/snp_patch.py
+++ str/snp_patch.py
@@ -2,8 +2,8 @@
 
 """
 
-analysis-runner --dataset "bioheart" --access-level "test" --description "Patch SNP files" --output-dir "tenk10k/str/associatr/final_freeze/common_variant_snps/tob_n950/results/v2-patch" snp_patch.py \
---chromosomes=chr21 --cell-types=CD4_CTL
+analysis-runner --dataset "bioheart" --access-level "test" --description "Patch SNP files" --output-dir "tenk10k/str/associatr/final_freeze/common_variant_snps/tob_n950/results/v2-patch" --memory 16G snp_patch.py \
+--chromosomes=chr19 --cell-types=CD4_CTL
 """
 
 import click
@@ -18,32 +18,29 @@ def correct_csv_format(file_path, cell, chrom):
     from cpg_utils.hail_batch import output_path
     import pandas as pd
 
-    try:
-        df = pd.read_csv(file_path, sep='\t')
 
-    except pd.errors.ParserError:
-        corrected_lines = []
-        with open(to_path(file_path), "r") as f:
-            for i, line in enumerate(f):
-                fields = line.strip().split("\t")
-                if i == 0:
-                    # Assume first line is header
-                    header = fields
-                    corrected_lines.append(fields)
-                elif len(fields) == 13:
-                    corrected_lines.append(fields)
-                elif len(fields) > 13 and len(fields) >= 18:
-                    # Malformed line: ignore first 5 fields, keep the rest to make up 13 total
-                    fixed_fields = fields[5:]
-                    corrected_lines.append(fixed_fields)
+    corrected_lines = []
+    with open(to_path(file_path), "r") as f:
+        for i, line in enumerate(f):
+            fields = line.strip().split("\t")
+            if i == 0:
+                # Assume first line is header
+                header = fields
+                corrected_lines.append(fields)
+            elif len(fields) == 13:
+                corrected_lines.append(fields)
+            elif len(fields) > 13 and len(fields) >= 18:
+                # Malformed line: ignore first 5 fields, keep the rest to make up 13 total
+                fixed_fields = fields[5:]
+                corrected_lines.append(fixed_fields)
 
-        # Convert to DataFrame using the fixed data
-        df = pd.DataFrame(corrected_lines[1:], columns=corrected_lines[0])
-        df.to_csv(
-            output_path(f'{cell}/{chrom}/{to_path(file_path).name}', 'analysis'),
-            sep='\t',
-            index=False,
-        )
+    # Convert to DataFrame using the fixed data
+    df = pd.DataFrame(corrected_lines[1:], columns=corrected_lines[0])
+    df.to_csv(
+        output_path(f'{cell}/{chrom}/{to_path(file_path).name}', 'analysis'),
+        sep='\t',
+        index=False,
+    )
 
 
 @click.option(
@@ -60,12 +57,12 @@ def main(input_dir, cell_types, chromosomes):
         for chromosome in chromosomes.split(','):
             files = list(to_path(f'{input_dir}/{cell_type}/{chromosome}').glob('*.tsv'))
             for file in files:
-                if to_path(output_path(f'{cell_type}/{chromosome}/{to_path(file.name)}', 'analysis')).exists():
-                    print(f"File {file} already processed, skipping.")
-                    continue
-                job = b.new_python_job(f'Patch SNPs for {file.name}')
-                job.cpu(0.25)
-                job.call(correct_csv_format, str(file), cell_type, chromosome)
+                try:
+                    df = pd.read_csv(file, sep='\t')
+                except:
+                    job = b.new_python_job(f'Patch SNPs for {file.name}')
+                    job.cpu(0.25)
+                    job.call(correct_csv_format, str(file), cell_type, chromosome)
     b.run(wait=False)
 
 
