315f3bfad749893948937b9038206276e5bfb7d1
hopedisastro <40783500+hopedisastro@users.noreply.github.com>
2025-06-19 18:20:26 +1000

    batch it by 30 susie runs per batch to reduce costs

--- str/fine-mapping/mcv/susie_mcv_runner.py
+++ str/fine-mapping/mcv/susie_mcv_runner.py
@@ -8,12 +8,13 @@ Required inputs:
 - output from`files_prep_residualizer.py` (residualized Y and X files) (which requires files_prep_dosages.py to run first).
 - List of eGenes to run SusieR on (ie Table S1 from the manuscript).
 
-analysis-runner --dataset tenk10k --access-level test --memory 4G  --image "australia-southeast1-docker.pkg.dev/cpg-common/images/r-meta:susie" --description "Run SusiE MCV" --output-dir str/associatr/final_freeze/fine_mapping/susie_mcv/output_files \
-susie_mcv_runner.py --table-s1-path=gs://cpg-tenk10k-test/str/associatr/final_freeze/meta_fixed/cell-type-spec/estrs.csv --residualized-dir=gs://cpg-tenk10k-test/str/associatr/final_freeze/fine_mapping/susie_mcv/prep_files/residualized  --max-parallel-jobs=1000
+analysis-runner --dataset tenk10k --access-level test --memory 4G  --image "australia-southeast1-docker.pkg.dev/cpg-common/images/r-meta:r-fast" --description "Run SusiE MCV" --output-dir str/associatr/final_freeze/fine_mapping/susie_mcv/output_files \
+susie_mcv_runner.py --table-s1-path=gs://cpg-tenk10k-test/str/associatr/final_freeze/meta_fixed/cell-type-spec/estrs.csv --residualized-dir=gs://cpg-tenk10k-test/str/associatr/final_freeze/fine_mapping/susie_mcv/prep_files/residualized  --max-parallel-jobs=1000 \
+--chromosomes=chr2
 """
 
 import click
-
+import math
 import hailtop.batch as hb
 import pandas as pd
 
@@ -21,7 +22,7 @@ from cpg_utils import to_path
 from cpg_utils.hail_batch import get_batch, output_path
 
 
-def susie_runner(input_dir, gene, cell_type, chrom, num_causal_variants, num_iterations):
+def susie_runner(input_dir, df_batch, cell_type, chrom, num_causal_variants, num_iterations):
     import pandas as pd
     import rpy2.robjects as ro
     from rpy2.robjects import pandas2ri
@@ -31,104 +32,111 @@ def susie_runner(input_dir, gene, cell_type, chrom, num_causal_variants, num_ite
     ro.r('library(susieR)')
     ro.r('library(tidyverse)')
 
-    # load in X and y file paths
-    X_df = pd.read_csv(f'{input_dir}/{cell_type}/{chrom}/{gene}_{cell_type}_meta_cleaned_X_resid.csv')
-    y_df = pd.read_csv(f'{input_dir}/{cell_type}/{chrom}/{gene}_{cell_type}_meta_cleaned_y_resid.csv')
+    for gene in df_batch['gene_name'].unique():
+        if to_path(
+                output_path(f'{cell_type}/{chrom}/{gene}_100kb.tsv', 'analysis'),
+            ).exists():
+            print(f'SusieR file for {gene} in {cell_type} already exists, skipping.')
+            continue
+        print(f'Running SuSiE for {gene} in {cell_type} on {chrom}')
 
-    with (ro.default_converter + pandas2ri.converter).context():
-        X = ro.conversion.get_conversion().py2rpy(X_df)
+        # load in X and y file paths
+        X_df = pd.read_csv(f'{input_dir}/{cell_type}/{chrom}/{gene}_{cell_type}_meta_cleaned_X_resid.csv')
+        y_df = pd.read_csv(f'{input_dir}/{cell_type}/{chrom}/{gene}_{cell_type}_meta_cleaned_y_resid.csv')
 
-    with (ro.default_converter + pandas2ri.converter).context():
-        y = ro.conversion.get_conversion().py2rpy(y_df)
+        with (ro.default_converter + pandas2ri.converter).context():
+            X = ro.conversion.get_conversion().py2rpy(X_df)
 
-    # === 4. Assign to R environment ===
-    ro.globalenv['X'] = X
-    ro.globalenv['y'] = y
-    ro.globalenv['num_causal_variants'] = num_causal_variants
-    ro.globalenv['num_iterations'] = num_iterations
+        with (ro.default_converter + pandas2ri.converter).context():
+            y = ro.conversion.get_conversion().py2rpy(y_df)
 
-    # === 5. Run SuSiE ===
-    ro.r(
+        # === 4. Assign to R environment ===
+        ro.globalenv['X'] = X
+        ro.globalenv['y'] = y
+        ro.globalenv['num_causal_variants'] = num_causal_variants
+        ro.globalenv['num_iterations'] = num_iterations
+
+        # === 5. Run SuSiE ===
+        ro.r(
+            '''
+        X <- subset(X, select = -sample)
+        x_input = as.matrix(X)
+        variant_ids <- colnames(x_input)
+        y = subset(y, select=-sample)
+        y_input = y[,1]
+        # run susieR
+        susie_fit <- susie(x_input, y_input, L = num_causal_variants, max_iter = num_iterations)
+
+        #capture susie output results to save later
+        raw_output = capture.output(summary(susie_fit))
+            '''
+        )
+
+        # convert raw output to python
+        raw_output_python = ro.r('raw_output')
+
+        # write raw output to GCS
+        with to_path(output_path(f"{cell_type}/{chrom}/{gene}_100kb_output.txt", 'analysis')).open('w') as file:
+            file.write(str(raw_output_python))
+
+        ro.r('coord_df <- data.frame(variant_id = variant_ids)')
+        ro.r('coord_df$chr <- sub("\\\\..*", "", coord_df$variant_id)')
+        print('extracted coordinates from variant IDs')
+
+        ro.r(
+            '''
+        # Get credible sets from SuSiE fit
+        susie_cs <- susie_get_cs(susie_fit, X = x_input)
+
+        # Initialize annotation vectors
+        n_variants <- length(variant_ids)
+        cs_id <- rep(NA_real_, n_variants)
+        cs_size <- rep(NA_real_, n_variants)
+        max_pip_in_cs <- rep(NA_real_, n_variants) '''
+        )
+        print('obtained credible sets and initialized vectors')
+
+        ro.r(
+            '''
+        # Annotate variants with CS membership
+        for (i in seq_along(susie_cs$cs)) {
+        idx <- susie_cs$cs[[i]]
+        cs_id[idx] <- i
+        cs_size[idx] <- length(idx)
+        max_pip_in_cs[idx] <- max(susie_fit$pip[idx], na.rm = TRUE)
+        }
         '''
-    library(susieR)
-    X <- subset(X, select = -sample)
-    x_input = as.matrix(X)
-    variant_ids <- colnames(x_input)
-    y = subset(y, select=-sample)
-    y_input = y[,1]
-    # run susieR
-    susie_fit <- susie(x_input, y_input, L = num_causal_variants, max_iter = num_iterations)
+        )
+        print('annotated variants with CS membership')
 
-    #capture susie output results to save later
-    raw_output = capture.output(summary(susie_fit))
-         '''
-    )
+        ro.r(
+            '''
+        # Combine results into final dataframe
+        final_df <- data.frame(
+        variant_id = variant_ids,
+        pip = susie_fit$pip,
+        pip_prune = susie_get_pip(susie_fit, prune_by_cs = TRUE),
+        cs_id = cs_id,
+        cs_size = cs_size,
+        max_pip_in_cs = max_pip_in_cs
+        ) |>
+        merge(coord_df, by = "variant_id") |>
+        dplyr::arrange(pip)
 
-    # convert raw output to python
-    raw_output_python = ro.r('raw_output')
-
-    # write raw output to GCS
-    with to_path(output_path(f"{cell_type}/{chrom}/{gene}_100kb_output.txt", 'analysis')).open('w') as file:
-        file.write(str(raw_output_python))
-
-    ro.r('coord_df <- data.frame(variant_id = variant_ids)')
-    ro.r('coord_df$chr <- sub("\\\\..*", "", coord_df$variant_id)')
-    print('extracted coordinates from variant IDs')
-
-    ro.r(
         '''
-    # Get credible sets from SuSiE fit
-    susie_cs <- susie_get_cs(susie_fit, X = x_input)
+        )
+        print('combined results into final dataframe')
 
-    # Initialize annotation vectors
-    n_variants <- length(variant_ids)
-    cs_id <- rep(NA_real_, n_variants)
-    cs_size <- rep(NA_real_, n_variants)
-    max_pip_in_cs <- rep(NA_real_, n_variants) '''
-    )
-    print('obtained credible sets and initialized vectors')
+        with (ro.default_converter + pandas2ri.converter).context():
+            susie_output_df = ro.conversion.get_conversion().rpy2py(ro.r('final_df'))
+        print('converted back to pandas df')
 
-    ro.r(
-        '''
-    # Annotate variants with CS membership
-    for (i in seq_along(susie_cs$cs)) {
-    idx <- susie_cs$cs[[i]]
-    cs_id[idx] <- i
-    cs_size[idx] <- length(idx)
-    max_pip_in_cs[idx] <- max(susie_fit$pip[idx], na.rm = TRUE)
-    }
-    '''
-    )
-    print('annotated variants with CS membership')
-
-    ro.r(
-        '''
-    # Combine results into final dataframe
-    final_df <- data.frame(
-    variant_id = variant_ids,
-    pip = susie_fit$pip,
-    pip_prune = susie_get_pip(susie_fit, prune_by_cs = TRUE),
-    cs_id = cs_id,
-    cs_size = cs_size,
-    max_pip_in_cs = max_pip_in_cs
-    ) |>
-    merge(coord_df, by = "variant_id") |>
-    dplyr::arrange(pip)
-
-    '''
-    )
-    print('combined results into final dataframe')
-
-    with (ro.default_converter + pandas2ri.converter).context():
-        susie_output_df = ro.conversion.get_conversion().rpy2py(ro.r('final_df'))
-    print('converted back to pandas df')
-
-    # write dataframe to GCS
-    susie_output_df.to_csv(
-        output_path(f"{cell_type}/{chrom}/{gene}_100kb.tsv", 'analysis'),
-        sep='\t',
-        index=False,
-    )
+        # write dataframe to GCS
+        susie_output_df.to_csv(
+            output_path(f"{cell_type}/{chrom}/{gene}_100kb.tsv", 'analysis'),
+            sep='\t',
+            index=False,
+        )
 
 
 @click.option('--table-s1-path', help='Table S1 with eGenes to run SusieR on')
@@ -167,31 +175,28 @@ def main(
     df = df.drop_duplicates(subset=['cell_type', 'gene_name', 'chr'])
     for chrom in chromosomes.split(','):
         df_chrom = df[df['chr'] == chrom]
-        for row in df_chrom.itertuples():
-            cell_type = row.cell_type
-            gene = row.gene_name
-            if to_path(
-                output_path(f'{cell_type}/{chrom}/{gene}_100kb.tsv', 'analysis'),
-            ).exists():
-                print(f'SusieR file for {gene} in {cell_type} already exists, skipping.')
-                continue
-            susie_job = b.new_python_job(
-                f'SusieR for {gene}: {cell_type}',
-            )
-            susie_job.cpu(susie_cpu)
-            if always_run:
-                susie_job.always_run()
-            susie_job.call(
-                susie_runner,
-                residualized_dir,
-                gene,
-                cell_type,
-                chrom,
-                num_causal_variants,
-                num_iterations,
-            )
-
-            manage_concurrency_for_job(susie_job)
+        for cell_type in df_chrom['cell_type'].unique():
+            df_cell = df_chrom[df_chrom['cell_type'] == cell_type]
+            # Split df_cell into batches of 30 rows
+            num_batches = math.ceil(len(df_cell) / 30)
+            for i in range(num_batches):
+                df_batch = df_cell.iloc[i*30 : (i+1)*30]
+                susie_job = b.new_python_job(
+                f'SusieR {cell_type} {chrom} batch {i}',
+                )
+                susie_job.cpu(susie_cpu)
+                if always_run:
+                    susie_job.always_run()
+                susie_job.call(
+                    susie_runner,
+                    residualized_dir,
+                    df_batch,
+                    cell_type,
+                    chrom,
+                    num_causal_variants,
+                    num_iterations,
+                )
+                manage_concurrency_for_job(susie_job)
     b.run(wait=False)
 
 
