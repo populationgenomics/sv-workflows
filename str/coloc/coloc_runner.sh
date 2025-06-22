26a1540deed713161d888fad46ace5b0aa1c6f52
hopedisastro <40783500+hopedisastro@users.noreply.github.com>
2025-06-23 08:18:29 +1000

    Update coloc_runner.sh

--- str/coloc/coloc_runner.sh
+++ str/coloc/coloc_runner.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Define the cell types as a comma-separated string
-#celltypes='ILC,Plasmablast,ASDC,cDC1,pDC,NK_CD56bright,MAIT,B_memory,CD4_CTL,CD4_Proliferating,CD8_Proliferating,HSPC,NK_Proliferating,cDC2,CD16_Mono,Treg,CD14_Mono,CD8_TCM,CD4_TEM,CD8_Naive,NK,CD8_TEM,CD4_Naive,B_naive,gdT,dnT,CD4_TCM'
-celltypes='B_intermediate'
+celltypes='B_intermediate,ILC,Plasmablast,ASDC,cDC1,pDC,NK_CD56bright,MAIT,B_memory,CD4_CTL,CD4_Proliferating,CD8_Proliferating,HSPC,NK_Proliferating,cDC2,CD16_Mono,Treg,CD14_Mono,CD8_TCM,CD4_TEM,CD8_Naive,NK,CD8_TEM,CD4_Naive,B_naive,gdT,dnT,CD4_TCM'
+#celltypes='B_intermediate'
 
 # Convert the cell types into an array
 IFS=',' read -ra celltype_array <<< "$celltypes"
@@ -58,12 +58,12 @@ phenotypes=(
 # Loop through each cell type and phenotype
 for celltype in "${celltype_array[@]}"; do
     for pheno in "${phenotypes[@]}"; do
-        analysis-runner --dataset "bioheart" \
+        analysis-runner --dataset "tenk10k" \
         --description "Run coloc for eGenes identified by STR analysis" \
         --access-level "test" \
         --memory='4G' \
         --image "australia-southeast1-docker.pkg.dev/analysis-runner/images/driver:d4922e3062565ff160ac2ed62dcdf2fba576b75a-hail-8f6797b033d2e102575c40166cf0c977e91f834e" \
-        --output-dir "tenk10k/str/associatr/final_freeze" \
+        --output-dir "str/associatr/final_freeze/meta_fixed" \
         coloc_ukbb_runner.py \
         --pheno-output-name="$pheno" \
         --celltypes "$celltype" \
