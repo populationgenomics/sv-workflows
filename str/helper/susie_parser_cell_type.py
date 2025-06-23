c515ee665e8b4c4400bd6c420f9626fe914928e1
hopedisastro <40783500+hopedisastro@users.noreply.github.com>
2025-06-24 09:34:28 +1000

    Update susie_parser_cell_type.py

--- str/helper/susie_parser_cell_type.py
+++ str/helper/susie_parser_cell_type.py
@@ -4,8 +4,8 @@ import pandas as pd
 from cpg_utils import to_path
 from cpg_utils.hail_batch import get_batch
 
-cell_types = 'B_intermediate,ILC,Plasmablast,ASDC,cDC1,pDC,NK_CD56bright,MAIT,B_memory,CD4_CTL,CD4_Proliferating,CD8_Proliferating,HSPC,NK_Proliferating,cDC2,CD16_Mono,Treg,CD14_Mono,CD8_TCM,CD4_TEM,CD8_Naive,NK,CD8_TEM,CD4_Naive,B_naive,CD4_TCM,gdT,dnT'
-
+#cell_types = 'B_intermediate,ILC,Plasmablast,ASDC,cDC1,pDC,NK_CD56bright,MAIT,B_memory,CD4_CTL,CD4_Proliferating,CD8_Proliferating,HSPC,NK_Proliferating,cDC2,CD16_Mono,Treg,CD14_Mono,CD8_TCM,CD4_TEM,CD8_Naive,NK,CD8_TEM,CD4_Naive,B_naive,CD4_TCM,gdT,dnT'
+cell_types = 'CD4_TCM'
 # Split the string into a list of cell types
 cell_type_list = cell_types.split(',')
 
@@ -20,7 +20,8 @@ for cell in cell_type_list:
 
     # Define the function to be called by the job
     def parse_susie(cell):
-        files_list = list(to_path(f'gs://cpg-bioheart-test-analysis/tenk10k/str/associatr/final_freeze/fine_mapping/susie/{cell}').rglob('*.tsv'))
+        #files_list = list(to_path(f'gs://cpg-bioheart-test-analysis/tenk10k/str/associatr/final_freeze/fine_mapping/susie/{cell}').rglob('*.tsv'))
+        files_list = list(to_path(f'gs://cpg-tenk10k-test-analysis/str/associatr/final_freeze/fine_mapping/susie_summstats/{cell}').rglob('*.tsv'))
         dfs = []
         for file in files_list:
             gene_name = str(file).split('/')[-1].split('_')[0]
@@ -29,7 +30,7 @@ for cell in cell_type_list:
             data['gene'] = gene_name
             dfs.append(data)
         result_df = pd.concat(dfs, ignore_index=True)
-        result_df.to_csv(f'gs://cpg-bioheart-test-analysis/tenk10k/str/associatr/final_freeze/fine_mapping/susie/{cell}/all_genes.csv', index=False)
+        result_df.to_csv(f'gs://cpg-tenk10k-test-analysis/str/associatr/final_freeze/fine_mapping/susie_summstats/{cell}/all_genes.csv', index=False)
 
     # Call the function within the job
     susie_job.call(parse_susie, cell)
