dd58b15bf5bd0150c1ef1fb2b1fc1d9e6c8c0083
hopedisastro <40783500+hopedisastro@users.noreply.github.com>
2025-06-26 08:52:12 +1000

    Update ukbb_str_snp_maker.py

--- str/coloc/ukbb_str_snp_maker.py
+++ str/coloc/ukbb_str_snp_maker.py
@@ -112,7 +112,7 @@ def main():
         "vitamin_d",
         "white_blood_cell_count",
     ]
-    for pheno in phenotypes:
+    for pheno in ['white_blood_cell_count']:
         liftover_job = b.new_python_job('Parse STR UKBB and combine with SNP ' + pheno)
         liftover_job.memory('32G')
         liftover_job.storage('10G')
