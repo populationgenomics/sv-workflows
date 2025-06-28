0aef0fcf7302d63c21d69fce5c4428ee60b798ff
hopedisastro <40783500+hopedisastro@users.noreply.github.com>
2025-06-28 13:48:44 +1000

    remove threadpool

--- str/methylation/results_concatenator.py
+++ str/methylation/results_concatenator.py
@@ -4,7 +4,7 @@ This script concatenates the results from running associatr-methylation on a per
 The output is a single CSV file containing per chromosome results
 
 analysis-runner --dataset tenk10k --access-level test --output-dir potato --description "Concatenate associatr-methylation results" python3 results_concatenator.py \
-    --chromosomes 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,21,22
+    --chromosomes 22
 
 
 """
@@ -16,31 +16,41 @@ from cpg_utils import to_path
 from cpg_utils.hail_batch import get_batch
 
 def process_file(file):
-    df = pd.read_csv(file, sep='\t')
-    site_chr = str(file).split('/')[-1].split('_')[0]
-    site_pos = str(file).split('/')[-1].split('_')[1]
-    site = f'{site_chr}_{site_pos}'
-    old_p = f'p_{site}'
-    old_coeff = f'coeff_{site}'
-    old_se = f'se_{site}'
+    try:
+        df = pd.read_csv(file, sep='\t')
+        site_chr = str(file).split('/')[-1].split('_')[0]
+        site_pos = str(file).split('/')[-1].split('_')[1]
+        site = f'{site_chr}_{site_pos}'
+        old_p = f'p_{site}'
+        old_coeff = f'coeff_{site}'
+        old_se = f'se_{site}'
 
-    # Rename the columns to be more generic
-    df = df.rename(columns={old_p: 'p', old_coeff: 'coeff', old_se: 'se'})
-    df['methyl_site'] = site  # add a column for the Cpg site coordinate
-    return df
+        # Rename the columns to be more generic
+        df = df.rename(columns={old_p: 'p', old_coeff: 'coeff', old_se: 'se'})
+        df['methyl_site'] = site
+        return df
+    except Exception as e:
+        print(f"Failed processing {file}: {e}")
+        return None
 
 def concatenator(input_dir, chrom):
     files = list(to_path(f'{input_dir}/chr{chrom}').glob('*.tsv'))
-    master_df = pd.DataFrame()
+    dataframes = []
 
-    # Use ThreadPoolExecutor to process files in parallel
-    with ThreadPoolExecutor() as executor:
-        results = list(executor.map(process_file, files))
+    for file in files:
+        df = process_file(file)
+        if df is not None:
+            dataframes.append(df)
 
-    # Concatenate all DataFrames
-    master_df = pd.concat(results, ignore_index=True)
-    output_gcs = f'gs://cpg-tenk10k-test-analysis/str/associatr-methylation/bioheart_n25/5kb/results/chr{chrom}_concatenated_results.tsv'
-    master_df.to_csv(output_gcs, sep='\t', index=False, header=True)
+    if dataframes:
+        master_df = pd.concat(dataframes, ignore_index=True)
+        output_gcs = f'gs://cpg-tenk10k-test-analysis/str/associatr-methylation/bioheart_n25/5kb/results/chr{chrom}_concatenated_results.tsv'
+        master_df.to_csv(output_gcs, sep='\t', index=False, header=True)
+        print(f"Wrote {len(master_df)} rows to {output_gcs}")
+    else:
+        print("No valid dataframes to concatenate.")
+
+
 
 
 @click.option(
