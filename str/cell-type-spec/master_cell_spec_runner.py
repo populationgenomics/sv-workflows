2d39600c2bc09a034a4b52136acb5117e7c7e1a3
hopedisastro <40783500+hopedisastro@users.noreply.github.com>
2025-07-02 13:13:39 +1000

    try to remove file check

--- str/cell-type-spec/master_cell_spec_runner.py
+++ str/cell-type-spec/master_cell_spec_runner.py
@@ -287,9 +287,9 @@ def main(estrs_path, meta_scen2_path, ld_path, associatr_path):
             if estrs_celltype_chrom.empty:
                 print(f"No data for {cell_type} on chromosome {chrom}, skipping.")
                 continue
-            if to_path(output_path(f'{cell_type}/chr{chrom}/cell_type_specificity_analysis.csv', 'analysis')).exists():
-                print(f"Cell type specificity analysis for {cell_type} on chromosome {chrom} already exists, skipping.")
-                continue
+            #if to_path(output_path(f'{cell_type}/chr{chrom}/cell_type_specificity_analysis.csv', 'analysis')).exists():
+                #print(f"Cell type specificity analysis for {cell_type} on chromosome {chrom} already exists, skipping.")
+                #continue
             j = b.new_python_job(
                 name=f'Cell spec for {cell_type} and chromosome {chrom}',
             )
