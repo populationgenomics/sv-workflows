03205f6b65330bdd8f58c3bb8340382b0d1bcdcb
hopedisastro <40783500+hopedisastro@users.noreply.github.com>
2025-07-08 17:13:27 +1000

    Update qc_filters_associatr.py

--- str/qc/qc_filters_associatr.py
+++ str/qc/qc_filters_associatr.py
@@ -27,7 +27,7 @@ import click
 import hail as hl
 
 from cpg_utils.config import get_config
-from cpg_utils.hail_batch import get_batch, init_batch, output_path
+from cpg_utils.hail_batch import get_batch
 
 config = get_config()
 
@@ -38,6 +38,8 @@ def qc_filter(mt_path, version):
     """
     Applies QC filters to the input MT
     """
+    from cpg_utils.hail_batch import init_batch, output_path
+
     init_batch(worker_memory='highmem')
 
     # read in mt
