039f0cbee64c1cbd32f8fbd27278084b9eb56006
Hope Tanudisastro <40783500+htanudisastro@users.noreply.github.com>
2025-07-16 14:22:07 +1000

    STEP 1 of Finemap MCV workflow - get genotypes (dosages)  (#278)
    * Update files_prep_dosages.py
    
    * try to make it more efficient
    
    * try on one chr only
    
    * Update files_prep_dosages.py
    
    * Update files_prep_dosages.py
    
    * make it cheaper
    
    * Update files_prep_dosages.py
    
    * Update files_prep_dosages.py
    
    * Update files_prep_dosages.py
    
    * all chromosomes
    
    * Update files_prep_dosages.py
    
    ---------
    
    Co-authored-by: hopedisastro <40783500+hopedisastro@users.noreply.github.com>

---
+++ str/fine-mapping/mcv/files_prep_dosages.py
@@ -0,0 +1,182 @@
+#!/usr/bin/env python3
+
+"""
+This script prepares the GT dosages files for input into files_prep_residualizer.py
+
+analysis-runner --dataset tenk10k --access-level test --memory 8G --description "Dosages file prep" --output-dir str/associatr/final_freeze/fine_mapping/susie_mcv/prep_files \
+files_prep_dosages.py --estrs-path=gs://cpg-tenk10k-test/str/associatr/final_freeze/meta_fixed/cell-type-spec/estrs.csv
+
+"""
+
+import click
+from cpg_utils.hail_batch import get_batch, output_path
+import pandas as pd
+from cpg_utils import to_path
+
+
+def tr_extract_genotype_matrix(chrom, start, end,vcf):
+    """
+    Extracts the genotype matrix for tandem repeats from a VCF file.
+    (summed repeats)
+    """
+
+    import pandas as pd
+
+    region = f"{chrom}:{start}-{end}"
+    samples = vcf.samples
+    genotype_dict = {sample: {} for sample in samples}
+
+    for variant in vcf(region):
+        coord = f"{variant.CHROM}:{variant.POS}:{variant.INFO.get('RU')}"
+        repcn_data = variant.format('REPCN')  # array of strings like "10/12"
+
+        if repcn_data is None:
+            continue  # skip if field missing
+
+        for i, sample in enumerate(samples):
+            val = repcn_data[i]
+            try:
+                if isinstance(val, bytes):
+                    val = val.decode()
+
+                if val in {"./.", ".", ""}:
+                    summed = None
+                else:
+                    allele_1, allele_2 = map(int, val.split('/'))
+                    summed = allele_1 + allele_2
+            except Exception:
+                summed = None
+
+            genotype_dict[sample][coord] = summed
+
+    df = pd.DataFrame.from_dict(genotype_dict, orient='index')
+    df.index.name = "sample"
+    df.sort_index(axis=1, inplace=True)
+    df.reset_index(inplace=True)
+    return df
+
+
+def snp_extract_genotype_matrix(chrom, start, end,vcf):
+    """
+    Extracts the genotype matrix for SNPs from a VCF file.
+
+    """
+
+    import pandas as pd
+
+    region = f"{chrom}:{start}-{end}"
+    samples = vcf.samples
+    genotype_dict = {sample: {} for sample in samples}
+
+    for variant in vcf(region):
+        coord = f"{variant.CHROM}:{variant.POS}:{variant.INFO.get('RU')}" # e.g., "chr1:123456:A-T"
+        gt_data = variant.genotypes  # shape: (n_samples, 3)
+
+        for i, sample in enumerate(samples):
+            try:
+                allele_1, allele_2, phased = gt_data[i]  # example: [0, 1, True]
+                # Ignore phased for dosage purposes
+                if allele_1 is None or allele_2 is None:
+                    summed = None
+                else:
+                    summed = allele_1 + allele_2 #summed GT in SNP context is number of non-REF alleles
+            except Exception:
+                summed = None
+
+            genotype_dict[sample][coord] = summed
+
+    df = pd.DataFrame.from_dict(genotype_dict, orient='index')
+    df.index.name = "sample"
+    df.sort_index(axis=1, inplace=True)
+    df.reset_index(inplace=True)
+    return df
+
+
+def dosages(chrom,df_chr):
+    import pandas as pd
+    import numpy as np
+    from cpg_utils.hail_batch import output_path
+    from cyvcf2 import VCF
+    """
+    Extracts the dosage files for each gene in the df DataFrame.
+    """ ""
+
+    # Load metadata to extract the window around the gene
+    gene_info = pd.read_csv('gs://cpg-bioheart-test/tenk10k/saige-qtl/300libraries_n1925_adata_raw_var.csv')
+
+    # Load in the SNP
+
+    local_snp_file = 'local.snp.vcf.bgz'
+    local_snp_file_index = 'local.snp.vcf.bgz.tbi'
+
+    snp_file = to_path(
+        f'gs://cpg-bioheart-test/tenk10k/str/associatr/common_variant_snps/hail_filtered_{chrom}.vcf.bgz'
+    )
+    snp_file_index = to_path(
+        f'gs://cpg-bioheart-test/tenk10k/str/associatr/common_variant_snps/hail_filtered_{chrom}.vcf.bgz.tbi'
+    )
+
+    snp_file.copy(local_snp_file)
+    snp_file_index.copy(local_snp_file_index)
+    snp_vcf = VCF(local_snp_file)
+
+    # Load in the TR VCFs
+    local_tr_file = 'local.tr.vcf.bgz'
+    local_tr_file_index = 'local.tr.vcf.bgz.tbi'
+
+    tr_file = to_path(
+        f'gs://cpg-bioheart-test/tenk10k/str/associatr/final-freeze/input_files/tr_vcf/v1-chr-specific/hail_filtered_{chrom}.vcf.bgz'
+    )
+    tr_file_index = to_path(
+        f'gs://cpg-bioheart-test/tenk10k/str/associatr/final-freeze/input_files/tr_vcf/v1-chr-specific/hail_filtered_{chrom}.vcf.bgz.tbi'
+    )
+
+    tr_file.copy(local_tr_file)
+    tr_file_index.copy(local_tr_file_index)
+
+    tr_vcf = VCF(local_tr_file)
+
+    for gene in df_chr['gene_name'].unique():
+        if to_path(output_path(f'dosages/{chrom}/{gene}_dosages.csv')).exists():
+            print(f"Dosage file for {gene} already exists, skipping.")
+            continue
+
+        gene_ensg = gene
+        gene_info_filtered = gene_info[gene_info['gene_ids'] == gene_ensg]
+        chrom = gene_info_filtered.iloc[0]['chr']
+        start = int(gene_info_filtered.iloc[0]['start'])
+        end = int(gene_info_filtered.iloc[0]['end']) + 100_000
+        start_body = max(0, start - 100_000)
+
+        # Extract genotype matrices for tandem repeats and SNPs
+        tr_df = tr_extract_genotype_matrix(chrom, start_body, end,tr_vcf)
+        snp_df = snp_extract_genotype_matrix(chrom, start_body, end,snp_vcf)
+
+        # Merge the two genotype dfs together
+        variant_df = tr_df.merge(snp_df)
+        variant_df['sample'] = variant_df['sample'].astype(float)
+
+        # Save file
+        variant_df.to_csv(output_path(f"dosages/{chrom}/{gene_ensg}_dosages.csv"), header=True)
+
+
+@click.option('--estrs-path', type=str, required=True, help='Path to the estrs file.')
+@click.command()
+def main(estrs_path):
+    b = get_batch(name='Dosages files prep for SuSie MCV')
+    df = pd.read_csv(estrs_path)
+    df = df.drop_duplicates(subset=['gene_name', 'chr'])
+    # sort by chromosome
+    for chrom in df['chr'].unique():
+        df_chr = df[df['chr'] == chrom]
+
+        j = b.new_python_job(name=f'Prepare for {chrom}')
+        j.cpu(2)
+        j.storage('10G')
+        j.call(dosages, chrom, df_chr)
+
+    b.run(wait=False)
+
+
+if __name__ == '__main__':
+    main()
